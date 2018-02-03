0e895e4e684513e5f3f813c5c7830d6151edec61
Morteza Hosseini <s_morteza_h@yahoo.com>
2018-02-03 18:21:48 +0000

    Changes

--- src/par.hpp
+++ src/par.hpp
@@ -62,63 +62,104 @@ void Parameters::parse (int& argc, char**& argv)
         }
         
         // Mandatory args
-        if (std::find(vArgs.begin(), vArgs.end(), "-t") == vArgs.end()
-            || std::find(vArgs.begin(), vArgs.end(), "--tar") == vArgs.end())
-            cerr << "Please specify the target file address with "
+        if (std::find(vArgs.begin(), vArgs.end(), "-t")    == vArgs.end() ||
+            std::find(vArgs.begin(), vArgs.end(), "--tar") == vArgs.end())
+            cerr << "Please specify the target file address, with "
                  << "\"-t fileName\".";
-        else if (std::find(vArgs.begin(), vArgs.end(), "-r") == vArgs.end()
-                 || std::find(vArgs.begin(), vArgs.end(), "--ref") == vArgs.end())
-            cerr << "Please specify the reference file address with "
+        else if (std::find(vArgs.begin(), vArgs.end(), "-r")    == vArgs.end()||
+                 std::find(vArgs.begin(), vArgs.end(), "--ref") == vArgs.end())
+            cerr << "Please specify the reference file address, with "
                  << "\"-r fileName\".";
     }
 }
 
 /*
- * Usage help
+ * Usage guide
  */
 inline void Parameters::help ()
 {
     cerr                                                                 << '\n'
         << "NAME"                                                        << '\n'
-        << "      Smash++ v" << VERSION << " - rearrangements finder"    << '\n'
+        << "    Smash++ v" << VERSION << " - rearrangements finder"      << '\n'
                                                                          << '\n'
         << "AUTHORS"                                                     << '\n'
-        << "      Morteza Hosseini    seyedmorteza@ua.pt"                << '\n'
-        << "      Diogo Pratas        pratas@ua.pt"                      << '\n'
-        << "      Armando J. Pinho    ap@ua.pt"                          << '\n'
+        << "    Morteza Hosseini    seyedmorteza@ua.pt"                  << '\n'
+        << "    Diogo Pratas        pratas@ua.pt"                        << '\n'
+        << "    Armando J. Pinho    ap@ua.pt"                            << '\n'
                                                                          << '\n'
         << "SYNOPSIS"                                                    << '\n'
-//        << "      ./smashpp [OPTION]...  [-d] [IN_FILE] > [OUT_FILE]"    << '\n'
-//                                                                         << '\n'
+        << "    ./smashpp [OPTION]...  -t [TAR_FILE] -r [REF_FILE]"      << '\n'
+                                                                         << '\n'
 //        << "SAMPLE"                                                      << '\n'
 //        << "      Compress:    ./cryfa -k pass.txt in.fq > comp"         << '\n'
 //        << "      Decompress:  ./cryfa -k pass.txt -d comp > orig.fq"    << '\n'
 //                                                                         << '\n'
-//        << "DESCRIPTION"                                                 << '\n'
+        << "DESCRIPTION"                                                 << '\n'
 //        << "      Compress and encrypt FASTA/FASTQ files."               << '\n'
-//        << "      Shuffle and encrypt any other text-based files."       << '\n'
 //                                                                         << '\n'
 //        << "      The KEY_FILE specifies a file including the password." << '\n'
 //                                                                         << '\n'
+        << "    -t [FILE], --tar          target file    -- MANDATORY"   << '\n'
+        << "    -r [FILE], --ref          reference file -- MANDATORY"   << '\n'
+        << "    -h,        --help         usage guide"                   << '\n'
+        << "    -v,        --verbose      more information"              << '\n'
+        << "    -t [NUM],  --thread       number of threads"             << '\n'
+                                                                         << '\n'
+        << "COPYRIGHT"                                                   << '\n'
+        << "    Copyright (C) "<< DEV_YEARS <<", IEETA, University of Aveiro.\n"
+        << "    You may redistribute copies of this Free software"       << '\n'
+        << "    under the terms of the GNU - General Public License"     << '\n'
+        << "    v3 <http://www.gnu.org/licenses/gpl.html>. There"        << '\n'
+        << "    is NOT ANY WARRANTY, to the extent permitted by law."    <<'\n';
+    
+    
+//    cerr                                                                 << '\n'
+//        << "NAME"                                                        << '\n'
+//        << "      Smash++ v" << VERSION << " - rearrangements finder"    << '\n'
+//                                                                         << '\n'
+//        << "AUTHORS"                                                     << '\n'
+//        << "      Morteza Hosseini    seyedmorteza@ua.pt"                << '\n'
+//        << "      Diogo Pratas        pratas@ua.pt"                      << '\n'
+//        << "      Armando J. Pinho    ap@ua.pt"                          << '\n'
+//                                                                         << '\n'
+//        << "SYNOPSIS"                                                    << '\n'
+//        << "      ./smashpp [OPTION]...  -t [TAR_FILE] -r [REF_FILE]"    << '\n'
+//                                                                         << '\n'
+////        << "SAMPLE"                                                      << '\n'
+////        << "      Compress:    ./cryfa -k pass.txt in.fq > comp"         << '\n'
+////        << "      Decompress:  ./cryfa -k pass.txt -d comp > orig.fq"    << '\n'
+////                                                                         << '\n'
+//        << "DESCRIPTION"                                                 << '\n'
+////        << "      Compress and encrypt FASTA/FASTQ files."               << '\n'
+////        << "      Shuffle and encrypt any other text-based files."       << '\n'
+////                                                                         << '\n'
+////        << "      The KEY_FILE specifies a file including the password." << '\n'
+////                                                                         << '\n'
 //        << "      -h,  --help"                                           << '\n'
-//        << "           usage help"                                      << '\n'
+//        << "           usage guide"                                      << '\n'
 //                                                                         << '\n'
-//        << "      -d,  --dec"                                            << '\n'
-//        << "           decompress & decrypt"                             << '\n'
+//        << "      -t [TAR_FILE],  --tar [TAR_FILE]"                      << '\n'
+//        << "           target file address -- MANDATORY"                 << '\n'
 //                                                                         << '\n'
+//        << "      -r [REF_FILE],  --ref [REF_FILE]"                      << '\n'
+//        << "           reference file address -- MANDATORY"              << '\n'
+//                                                                         << '\n'
+////        << "      -d,  --dec"                                            << '\n'
+////        << "           decompress & decrypt"                             << '\n'
+////                                                                         << '\n'
 //        << "      -v,  --verbose"                                        << '\n'
 //        << "           verbose mode (more information)"                  << '\n'
 //                                                                         << '\n'
 //        << "      -t [NUMBER],  --thread [NUMBER]"                       << '\n'
 //        << "           number of threads"                                << '\n'
-                                                                         << '\n'
-        << "COPYRIGHT"                                                   << '\n'
-        << "      Copyright (C) " << DEV_YEARS
-                                  << ", IEETA, University of Aveiro."    << '\n'
-        << "      You may redistribute copies of this Free software"     << '\n'
-        << "      under the terms of the GNU - General Public License v3"<< '\n'
-        << "      <http://www.gnu.org/licenses/gpl.html>. There is NOT"  << '\n'
-        << "      ANY WARRANTY, to the extent permitted by law."         <<'\n';
+//                                                                         << '\n'
+//        << "COPYRIGHT"                                                   << '\n'
+//        << "      Copyright (C) " << DEV_YEARS
+//                                  << ", IEETA, University of Aveiro."    << '\n'
+//        << "      You may redistribute copies of this Free software"     << '\n'
+//        << "      under the terms of the GNU - General Public License"   << '\n'
+//        << "      v3 <http://www.gnu.org/licenses/gpl.html>. There"      << '\n'
+//        << "      is NOT ANY WARRANTY, to the extent permitted by law."  <<'\n';
 }
 
 #endif //SMASHPP_PAR_HPP
