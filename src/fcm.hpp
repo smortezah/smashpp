bbf30b833cac49ee133ca3665d73b1f88d552428
Morteza <s_morteza_h@yahoo.com>
2018-02-06 18:06:30 +0000

    CMake

--- src/fcm.hpp
+++ src/fcm.hpp
@@ -13,12 +13,15 @@ public:
     FCM () = default;
     ~FCM () { delete tbl; }
     void buildModel (const Parameters&);
+    void compress (const Parameters&);
     
     void printTable (const Parameters&); //todo. test
     
 private:
-//    u64* tbl;
-    float* tbl;
+    u64* tbl;
+//    double* tbl;
+
+    u64 countSyms (const string &);
 };
 
 #endif //SMASHPP_FCM_HPP
