c0ee6678d89027944c174e96cb1bfd0937e0741e
Morteza <s_morteza_h@yahoo.com>
2018-02-08 18:18:21 +0000

    CMake

--- src/main.cpp
+++ src/main.cpp
@@ -22,31 +22,33 @@ using std::setprecision;
 
 int main (int argc, char* argv[])
 {
-    Param p;
-    try         { p.parse(argc, argv); }
+    try {
+      Param p;
+      p.parse(argc, argv);
+      
+      auto* m = new FCM(p);
+    
+      // Build models
+      auto t0=high_resolution_clock::now();    // Start time
+      m->buildModel(p);
+      auto t1=high_resolution_clock::now();    // Finish time
+      dur_t e=t1-t0;                           // Elapsed time
+      cerr << "in " << std::fixed<<setprecision(3)<< e.count() << " seconds.\n";
+    
+    
+//      m->mode=='t' ? m->printTbl(p) : m->printHashTbl();
+    
+    
+      // Compress
+      t0=high_resolution_clock::now();
+      m->compress(p);
+      t1=high_resolution_clock::now();
+      e=t1-t0;
+      cerr << "in " << std::fixed<<setprecision(3)<< e.count() << " seconds.\n";
+    
+      delete m;
+    }
     catch (...) { return EXIT_FAILURE; }
-    
-    auto* m = new FCM(p);
-    
-    // Build models
-    auto t0 = high_resolution_clock::now();    // Start time
-    m->buildModel(p);
-    auto t1 = high_resolution_clock::now();    // Finish time
-    dur_t e = t1-t0;                           // Elapsed time
-    cerr << "in " << std::fixed<< setprecision(3) << e.count() << " seconds.\n";
-    
-    
-//    m->mode=='t' ? m->printTbl(p) : m->printHashTbl();
-
-    
-    // Compress
-    t0 = high_resolution_clock::now();
-    m->compress(p);
-    t1 = high_resolution_clock::now();
-    e = t1-t0;
-    cerr << "in " << std::fixed<< setprecision(3) << e.count() << " seconds.\n";
-    
-    delete m;
     
     return 0;
 }
