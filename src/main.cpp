b2066a41536dc5444f9bc28ee5d7375dd8158177
Morteza <s_morteza_h@yahoo.com>
2018-02-19 16:56:55 +0000

    CMake

--- src/main.cpp
+++ src/main.cpp
@@ -26,29 +26,31 @@ int main (int argc, char* argv[])
     Param p;
     p.parse(argc, argv);
     
-    auto* m = new FCM(p);
+//    auto* m = new FCM(p);
+    vector<FCM> m;
 
     //todo. multithr: arrThreadSize=std::min(nThr, nModel)
-    
-    // Build models
-    auto t0 = high_resolution_clock::now();    // Start time
-    m->buildModel(p);
-    auto t1 = high_resolution_clock::now();    // Finish time
-    dur_t e = t1-t0;                           // Elapsed time
-    cerr << "in " << std::fixed << setprecision(3)<< e.count() << " seconds.\n";
-
+for (u8 i=0; i!=p.nMdl; ++i) {
+    m.emplace_back(FCM(p));
+//    // Build models
+//    auto t0 = high_resolution_clock::now();    // Start time
+//    m[0].buildModel(p);
+//    auto t1 = high_resolution_clock::now();    // Finish time
+//    dur_t e = t1-t0;                           // Elapsed time
+//    cerr << "in " << std::fixed << setprecision(3)<< e.count() << " seconds.\n";
+}
 
 //    m->mode=='t' ? m->printTbl(p) : m->printHashTbl();
 
 
-    // Compress
-    t0=high_resolution_clock::now();
-    m->compress(p);
-    t1=high_resolution_clock::now();
-    e=t1-t0;
-    cerr << "in " << std::fixed << setprecision(3)<< e.count() << " seconds.\n";
+//    // Compress
+//    t0=high_resolution_clock::now();
+//    m->compress(p);
+//    t1=high_resolution_clock::now();
+//    e=t1-t0;
+//    cerr << "in " << std::fixed << setprecision(3)<< e.count() << " seconds.\n";
 
-    delete m;
+//    delete m;
   }
   catch (...) { return EXIT_FAILURE; }
   
