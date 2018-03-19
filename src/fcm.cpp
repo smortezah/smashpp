fa8bf80220ce07f45fa7a0012dfdb34526c315ce
Morteza <s_morteza_h@yahoo.com>
2018-03-19 14:32:45 +0000

    Minor changes

--- src/fcm.cpp
+++ src/fcm.cpp
@@ -41,26 +41,18 @@ FCM::~FCM () {
 }
 
 void FCM::buildModel (const Param& p) {
-//  u64      ctx;                // Context (integer) to slide in the file
-//  u64      maxPV=POW5[p.k[0]];    // Max Place Value//todo. change k[0]
-//  u64      ctxIR;              // Inverted repeat context (integer)
-//  u8       curr;               // Current symbol (integer)
-//  u64      ctxIRCurr;          // Concat IR context - current symbol
-//  double   a=p.alpha[0], sa=ALPH_SZ*a;//todo. change alpha[0]
-//  ifstream rf;                 // Ref file
-//  char     c;                  // To read from ref file
-//  rf.open(p.ref);
   cerr << "Building models...\n";
-  
   for (auto m : model) {
     switch (m.mode) {
       case MODE::TABLE_64:     buildTbl64(p.ref, m.k);    break;
       case MODE::TABLE_32:     buildTbl32(p.ref, m.k);    break;
       case MODE::LOG_TABLE_8:  buildLogTbl8(p.ref, m.k);  break;
-      default:                 buildSketch4(p.ref, m.k);
+      case MODE::SKETCH_8:     buildSketch4(p.ref, m.k);  break;
+      default:                 cerr << "Error.";
     }
   }
   
+  //todo. test multithreading
 //  vector<thread> thrd;  thrd.resize(4);
 //  for (auto m : model) {
 //    switch (m.mode) {
@@ -75,89 +67,10 @@ void FCM::buildModel (const Param& p) {
 //    }
 //  }
 //  for (u8 t=0; t!=4; ++t)  if (thrd[t].joinable()) thrd[t].join();
-
-    
-  //todo test
-//  tbl64->printTbl();
-//  cerr<< '\n';
-//  sketch4->printSk();
-
-  
-  
-  // Table64
-//  if (p.mode == 't') {
-//    tbl = new double[TAB_COL*maxPV];
-//    for (u64 i=0; i!=TAB_COL*maxPV; ++i) {
-//      tbl[i] = (i%TAB_COL==ALPH_SZ) ? sa : a;
-//    }
-//    ctx   = 0;
-//    ctxIR = maxPV-1;
-//    // Fill tbl by no. occurrences of symbols A,C,N,G,T
-//    while (rf.get(c)) {
-//      if (c != '\n') {
-//        curr = NUM[c];
-//        u64 rowIdx;
-//
-//        // Inverted repeats
-//        if (p.ir[0]) {//todo. change ir[0]
-//          ctxIRCurr = ctxIR + (IRMAGIC-curr)*maxPV;
-//          ctxIR     = ctxIRCurr/ALPH_SZ;      // Update ctxIR
-//          rowIdx    = ctxIR*TAB_COL;
-//          ++tbl[rowIdx+ctxIRCurr%ALPH_SZ];
-//          ++tbl[rowIdx+ALPH_SZ];              // 'sum' col
-//        }
-//
-//        rowIdx = ctx*TAB_COL;
-//        ++tbl[rowIdx+curr];
-//        ++tbl[rowIdx+ALPH_SZ];
-//        // Update ctx.  (rowIdx - k) == (k * ALPH_SIZE)
-//        ctx = (rowIdx-ctx)%maxPV + curr;             // Fastest
-////        ctx = (ctx*ALPH_SZ)%maxPV + curr;             // Fastest
-////        ctx = (rowIdx-ctx+curr)%maxPV;             // Faster
-////        ctx = (ctx%POW5[p.k-1])*ALPH_SZ + curr;    // Fast
-//      }
-//    }
-//  }
-  // Sketch
-//  else if (p.mode == 's') {
-//    ctx = 0;
-//    auto mask = static_cast<u64>((4<<(p.k[0]<<1)) - 1); // 4<<2k -1 = 4^(k+1) -1
-//    // Fill the sketch by no. occurrences of A,C,G,T
-//    while (rf.get(c)) {
-//      if (c != '\n') {
-//        ctx = ((ctx<<2) & mask) | NUM[c];    // Update ctx
-//        skch->update(ctx);
-//      }
-//    }
-//  }
-  // Hash table//todo. remove
-//  else if (p.mode == 'h') {
-//    ctx   = 0;
-//    ctxIR = maxPV-1;
-//    // Fill tbl by no. occurrences of symbols A,sk,N,G,T
-//    while (rf.get(c)) {
-//      if (c!='\n') {
-//        curr = NUM[c];
-//        // Inverted repeats
-//        if (p.ir[0]) {
-//          ctxIRCurr = ctxIR + (IR_MAGIC-curr)*maxPV;
-//          ctxIR     = ctxIRCurr/ALPH_SZ;       // Update ctxIR
-//          ++htbl[ctxIR][ctxIRCurr%ALPH_SZ];
-//        }
-//
-//        ++htbl[ctx][curr];
-//        ctx = (ctx*ALPH_SZ)%maxPV + curr;        // Update ctx
-//      }
-//    }
-//  }
-//  else
-//    cerr << "Error.\n";
-
-//  rf.close();
   cerr << "Models built ";
 }
 
-void FCM::buildTbl64 (const string& ref, u8 k) {
+inline void FCM::buildTbl64 (const string& ref, u8 k) {
   ifstream rf(ref);
   char c;
   auto mask = static_cast<u32>((4<<(k<<1)) - 1);    // 4<<2k - 1 = 4^(k+1) - 1
@@ -170,7 +83,7 @@ void FCM::buildTbl64 (const string& ref, u8 k) {
   rf.close();
 }
 
-void FCM::buildTbl32 (const string& ref, u8 k) {
+inline void FCM::buildTbl32 (const string& ref, u8 k) {
   ifstream rf(ref);
   char c;
   auto mask = static_cast<u32>((4<<(k<<1)) - 1);
@@ -183,7 +96,7 @@ void FCM::buildTbl32 (const string& ref, u8 k) {
   rf.close();
 }
 
-void FCM::buildLogTbl8 (const string& ref, u8 k) {
+inline void FCM::buildLogTbl8 (const string& ref, u8 k) {
   ifstream rf(ref);
   char c;
   auto mask = static_cast<u32>((4<<(k<<1)) - 1);
@@ -196,7 +109,7 @@ void FCM::buildLogTbl8 (const string& ref, u8 k) {
   rf.close();
 }
 
-void FCM::buildSketch4 (const string& ref, u8 k) {
+inline void FCM::buildSketch4 (const string& ref, u8 k) {
   ifstream rf(ref);
   char c;
   auto mask = static_cast<u64>((4<<(k<<1)) - 1);
@@ -210,85 +123,184 @@ void FCM::buildSketch4 (const string& ref, u8 k) {
 }
 
 void FCM::compress (const Param& p) const {
-//  double   a=p.alpha[0], sa=ALPH_SZ*a;
-//  ifstream tf;
-//  char     c;
-//  u8       curr;           // Current symbol (integer)
-//  u64      maxPV=POW5[p.k[0]];
-//  u64      ctx=0;          // Context(s) (integer) sliding through the dataset
-//  u64      ctxIR=0;        // Inverted repeat context (integer)
-//  double   sEntr=0;        // Sum of entropies = sum( log_2 P(s|c^t) )
-//  u64      symsNo=0;       // No. syms in target file, except \n
-//  tf.open(p.tar);
-//
-//  cerr << "Compressing...\n";
-//  // Table64
-//  if (p.mode == 't') {
-////      while (tf.get(c)) {
-////        if (c!='\n') {
-////          ++symsNo;
-////          curr       = NUM[c];
-////          u64 rowIdx = ctx*TAB_COL;
-////          sEntr     += log2(tbl[rowIdx+ALPH_SZ]/tbl[rowIdx+curr]);
-////          ctx        = (rowIdx-ctx)%maxPV + curr;    // Update ctx
-////        }
-////      }
-//  }
-//  // Sketch
-//  else if (p.mode == 's') {
-//    std::array<u64, 4> vN{0}; // Vector of number of elements
-//    auto shl  = static_cast<u64>(2 * p.k[0]);    // 2*k
-//    auto mask = static_cast<u64>((4<<shl) - 1); // 4<<2k -1 = 4^(k+1) -1
-//    ctxIR = mask;
-//    while (tf.get(c)) {
-//      if (c != '\n') {
-//        ++symsNo;
-//        // Inverted repeat
-//        if (p.ir[0]) {//todo. change ir[0]
-//          u64 r = ctxIR>>2;                  // Update ctx
-//          vN[0] = skch->query(r | 3<<shl); // A
-//          vN[1] = skch->query(r | 2<<shl); // C
-//          vN[2] = skch->query(r | 1<<shl); // G
-//          vN[3] = skch->query(r);          // T
-////          ctxIR  = (ctxIR>>2) | (IRMAGIC-NUM[c])<<shl;    // Update ctx
-//        }
-//
-//        u64 l  = (ctx<<2) & mask;    // Update ctx
-//        vN[0] += skch->query(l);     // A
-//        vN[1] += skch->query(l | 1); // C
-//        vN[2] += skch->query(l | 2); // G
-//        vN[3] += skch->query(l | 3); // T
-//
-//        sEntr += log2((vN[0]+vN[1]+vN[2]+vN[3]+sa) / (vN[NUM[c]]+a));
-//
-////        cout<<"nA="<<nA<<'\t'<<"nC="<<nC<<'\t'<<"nG="<<nG<<'\t'<<"nT="<<nT<<'\t'
-////            <<"sum="<<sum<<'\n'
-////            <<"sEntr="<<sEntr<<'\n';
-//      }
-//    }
-//  }
-//  // Hash table
-//  else if (p.mode == 'h') { //todo. remove
-////    while (tf.get(c)) {
-////      if (c!='\n') {
-////        ++symsNo;
-////        curr    = NUM[c];
-////        auto hi = htbl.find(ctx);
-////        if (hi!=htbl.end()) {
-////          auto ar = hi->second;
-////          u64 sum=0;    for (const auto &e : ar)  sum+=e;
-////          sEntr += log2((sum+sa)/(ar[curr]+a));
-////        }
-////        else {
-////          sEntr += log2(ALPH_SZ);
-////        }
-////        ctx = (ctx*ALPH_SZ)%maxPV + curr;    // Update ctx
-////      }
-////    }
-//  }
-//
-//  tf.close();
-//  double aveEntr = sEntr/symsNo;
-//  cerr << "Average Entropy (H) = " << aveEntr << '\n';
+  cerr << "Compressing...\n";
+  for (auto m : model) {
+    switch (m.mode) {
+      case MODE::TABLE_64:    compressTbl64(p.tar, m.ir, m.k, m.alpha);   break;
+      case MODE::TABLE_32:    compressTbl32(p.tar, m.ir, m.k, m.alpha);   break;
+      case MODE::LOG_TABLE_8: compressLogTbl8(p.tar, m.ir, m.k, m.alpha); break;
+      case MODE::SKETCH_8:    compressSketch4(p.tar, m.ir, m.k, m.alpha); break;
+      default:                cerr << "Error";
+    }
+  }
 //  cerr << "Compression finished ";
+}
+
+inline void FCM::compressTbl64 (const string& tar,
+                                u8 ir, u8 k, float alpha) const {
+  std::array<u64, 4> vN{0};// Vector of number of elements
+  auto   shl    = k<<1;    // Shift left
+  auto   mask   = static_cast<u32>((1<<shl) - 1); // 1<<2k -1 = 4^k-1 = 2^(2k)-1
+  u32    ctx    = 0;       // Context(s) (integer) sliding through the dataset
+  u32    ctxIR  = mask;    // Inverted repeat context (integer)
+  u64    symsNo = 0;       // No. syms in target file, except \n
+  double sAlpha = ALPH_SZ*alpha;                  // Sum of alphas
+  double sEntr  = 0;       // Sum of entropies = sum( log_2 P(s|c^t) )
+  ifstream tf(tar);
+  char c;
+  while (tf.get(c)) {
+    if (c != '\n') {
+      ++symsNo;
+      auto numSym = NUM[c];
+      // Inverted repeat
+      if (ir) {
+        auto r = ctxIR>>2;
+        vN[0] = tbl64->query(3<<shl | r);    // A
+        vN[1] = tbl64->query(2<<shl | r);    // C
+        vN[2] = tbl64->query(1<<shl | r);    // G
+        vN[3] = tbl64->query(r);             // T
+        ctxIR = REVNUM[c]<<shl | r;          // Update ctxIR
+      }
+      auto l = ctx<<2;
+      vN[0] += tbl64->query(l);        // A
+      vN[1] += tbl64->query(l | 1);    // C
+      vN[2] += tbl64->query(l | 2);    // G
+      vN[3] += tbl64->query(l | 3);    // T
+      ctx = (l & mask) | numSym;       // Update ctx
+      
+      sEntr += log2((vN[0]+vN[1]+vN[2]+vN[3]+sAlpha) / (vN[numSym]+alpha));
+      
+      //todo test
+//      cout<<"nA="<<nA<<'\t'<<"nC="<<nC<<'\t'<<"nG="<<nG<<'\t'<<"nT="<<nT<<'\t'
+//          <<"sum="<<sum<<'\n'
+//          <<"sEntr="<<sEntr<<'\n';
+    }
+  }
+  tf.close();
+  double aveEntr = sEntr/symsNo;
+  cerr << "Average Entropy (H) = " << aveEntr << '\n';
+  cerr << "Compression finished ";
+}
+
+inline void FCM::compressTbl32 (const string& tar,
+                                u8 ir, u8 k, float alpha) const {
+  std::array<u64, 4> vN{0};
+  auto   shl    = k<<1;
+  auto   mask   = static_cast<u32>((1<<shl) - 1);
+  u32    ctx    = 0;
+  u32    ctxIR  = mask;
+  u64    symsNo = 0;
+  double sAlpha = ALPH_SZ*alpha;
+  double sEntr  = 0;
+  ifstream tf(tar);
+  char c;
+  while (tf.get(c)) {
+    if (c != '\n') {
+      ++symsNo;
+      auto numSym = NUM[c];
+      // Inverted repeat
+      if (ir) {
+        auto r = ctxIR>>2;
+        vN[0] = tbl32->query(3<<shl | r);    // A
+        vN[1] = tbl32->query(2<<shl | r);    // C
+        vN[2] = tbl32->query(1<<shl | r);    // G
+        vN[3] = tbl32->query(r);             // T
+        ctxIR = REVNUM[c]<<shl | r;          // Update ctxIR
+      }
+      auto l = ctx<<2;
+      vN[0] += tbl32->query(l);        // A
+      vN[1] += tbl32->query(l | 1);    // C
+      vN[2] += tbl32->query(l | 2);    // G
+      vN[3] += tbl32->query(l | 3);    // T
+      ctx = (l & mask) | numSym;       // Update ctx
+      
+      sEntr += log2((vN[0]+vN[1]+vN[2]+vN[3]+sAlpha) / (vN[numSym]+alpha));
+    }
+  }
+  tf.close();
+  double aveEntr = sEntr/symsNo;
+  cerr << "Average Entropy (H) = " << aveEntr << '\n';
+  cerr << "Compression finished ";
+}
+
+inline void FCM::compressLogTbl8 (const string& tar,
+                                  u8 ir, u8 k, float alpha) const {
+  std::array<u64, 4> vN{0};
+  auto   shl    = k<<1;
+  auto   mask   = static_cast<u32>((1<<shl) - 1);
+  u32    ctx    = 0;
+  u32    ctxIR  = mask;
+  u64    symsNo = 0;
+  double sAlpha = ALPH_SZ*alpha;
+  double sEntr  = 0;
+  ifstream tf(tar);
+  char c;
+  while (tf.get(c)) {
+    if (c != '\n') {
+      ++symsNo;
+      auto numSym = NUM[c];
+      // Inverted repeat
+      if (ir) {
+        auto r = ctxIR>>2;
+        vN[0] = logtbl8->query(3<<shl | r);    // A
+        vN[1] = logtbl8->query(2<<shl | r);    // C
+        vN[2] = logtbl8->query(1<<shl | r);    // G
+        vN[3] = logtbl8->query(r);             // T
+        ctxIR = REVNUM[c]<<shl | r;          // Update ctxIR
+      }
+      auto l = ctx<<2;
+      vN[0] += logtbl8->query(l);        // A
+      vN[1] += logtbl8->query(l | 1);    // C
+      vN[2] += logtbl8->query(l | 2);    // G
+      vN[3] += logtbl8->query(l | 3);    // T
+      ctx = (l & mask) | numSym;       // Update ctx
+      
+      sEntr += log2((vN[0]+vN[1]+vN[2]+vN[3]+sAlpha) / (vN[numSym]+alpha));
+    }
+  }
+  tf.close();
+  double aveEntr = sEntr/symsNo;
+  cerr << "Average Entropy (H) = " << aveEntr << '\n';
+  cerr << "Compression finished ";
+}
+
+inline void FCM::compressSketch4 (const string& tar,
+                                  u8 ir, u8 k, float alpha) const {
+  std::array<u32, 4> vN{0};  // 2*(2^16-1) < 32 bits
+  auto   shl    = k<<1;
+  auto   mask   = static_cast<u64>((1<<shl) - 1);
+  u64    ctx    = 0;
+  u64    ctxIR  = mask;
+  u64    symsNo = 0;
+  double sAlpha = ALPH_SZ*alpha;
+  double sEntr  = 0;
+  ifstream tf(tar);
+  char c;
+  while (tf.get(c)) {
+    if (c != '\n') {
+      ++symsNo;
+      auto numSym = NUM[c];
+      // Inverted repeat
+      if (ir) {
+        auto r = ctxIR>>2;
+        vN[0] = sketch4->query(3<<shl | r);    // A
+        vN[1] = sketch4->query(2<<shl | r);    // C
+        vN[2] = sketch4->query(1<<shl | r);    // G
+        vN[3] = sketch4->query(r);             // T
+        ctxIR = REVNUM[c]<<shl | r;            // Update ctxIR
+      }
+      auto l = ctx<<2;
+      vN[0] += sketch4->query(l);        // A
+      vN[1] += sketch4->query(l | 1);    // C
+      vN[2] += sketch4->query(l | 2);    // G
+      vN[3] += sketch4->query(l | 3);    // T
+      ctx = (l & mask) | numSym;         // Update ctx
+      
+      sEntr += log2((vN[0]+vN[1]+vN[2]+vN[3]+sAlpha) / (vN[numSym]+alpha));
+    }
+  }
+  tf.close();
+  double aveEntr = sEntr/symsNo;
+  cerr << "Average Entropy (H) = " << aveEntr << '\n';
+  cerr << "Compression finished ";
 }
