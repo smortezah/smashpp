d7e2546eb55c816789427880399876c1148fe3d7
Morteza Hosseini <s_morteza_h@yahoo.com>
2018-03-25 22:50:08 +0100

    Minor Changes

--- src/main.cpp
+++ src/main.cpp
@@ -32,20 +32,22 @@ int main (int argc, char* argv[])
 //    else
 //      cerr<<"0 ";
 //  }
-  
+
+
+
   
   try {
     Param p;
     p.parse(argc, argv);
     auto* m = new FCM(p);
-    
+
     // Build models
     auto t0 = high_resolution_clock::now();    // Start time
     m->buildModel(p);
     auto t1 = high_resolution_clock::now();    // Finish time
     dur_t e = t1 - t0;                         // Elapsed time
     cerr << "in " << std::fixed << setprecision(3)<< e.count() << " seconds.\n";
-    
+
     // Compress
     t0 = high_resolution_clock::now();
     m->compress(p);
