b299971061ed57f9db8693dcd46fffeb901a271a
Morteza <s_morteza_h@yahoo.com>
2018-04-04 18:27:42 +0100

    Minor changes

--- src/main.cpp
+++ src/main.cpp
@@ -39,7 +39,7 @@ int main (int argc, char* argv[])
 
     // Compress
     t0 = high_resolution_clock::now();
-    m->compress(p);
+//    m->compress(p);
     t1 = high_resolution_clock::now();
     e  = t1 - t0;
     cerr << "in " << std::fixed << setprecision(3)<< e.count() << " seconds.\n";
