77b3df48c8253b1c7bae6e9089a80bbfd44f0ccd
Morteza <s_morteza_h@yahoo.com>
2018-04-11 15:23:19 +0100

    Minor changes
    Former-commit-id: fa82f74936fc158001210392ec4bf901a81e2647

--- src/bench.cpp
+++ src/bench.cpp
@@ -7,15 +7,17 @@
 using std::string;
 using std::array;
 using std::cerr;
+typedef unsigned int  u32;
 
-string exec (const char*cmd) {
-  array<char, 128> buffer{};
+string exec (const char* cmd) {
+  const u32 bufSize = 128;
+  array<char, bufSize> buffer{};
   string result;
   std::shared_ptr<FILE> pipe(popen(cmd, "r"), pclose);
   if (!pipe)
     throw std::runtime_error("Error: popen() failed!");
   while (!feof(pipe.get())) {
-    if (fgets(buffer.data(), 128, pipe.get())!=nullptr)
+    if (fgets(buffer.data(), bufSize, pipe.get()) != nullptr)
       result += buffer.data();
   }
   return result;
@@ -26,6 +28,8 @@ int main (int argc, char* argv[])
 //  std::system("ls -lh");
   cerr << exec("./smashpp -t A -r A -l 0");
   cerr << exec("./smashpp -t A -r A -l 1");
+  std::system("./smashpp -t A -r A -l 2");
+  popen("./smashpp -t A -r A -l 0", "r");
   
   return 0;
 }
