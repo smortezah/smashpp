8995ed22d7d4899253cb558dced76c7d1a92eede
Morteza <s_morteza_h@yahoo.com>
2018-04-27 11:53:09 +0100

    Minor changes

--- src/fcm.hpp
+++ src/fcm.hpp
@@ -18,23 +18,32 @@ using std::initializer_list;
 class ModelPar {
  public:
   u8         k;         // Context size
-  u8         ir;       // Markov models Inverted repeat
-  float      alpha;
-  float      gamma;
   Container  cner;      // Tbl 64, Tbl 32, LogTbl 8, Sketch 4
-//  ModelPar (u8, u64, u8, u8, float, float, u8, u8, float, float);
-//  ModelPar (u8, u8, float, float);
-//  ModelPar (u8, u64, u8, u8, float, float);
-//  ModelPar (u8, u8, float, float, u8, u8, float, float);
+ protected:
+//  ModelPar      () = default;
+  explicit ModelPar (u8);
+//  explicit ModelPar (Container);
+  virtual void set_cner (Container);
 };
 class MMPar : public ModelPar {
  public:
   u64        w;         // Width of count-min-log sketch
   u8         d;         // Depth of count-min-log sketch
+  u8         ir;       // Markov models Inverted repeat
+  float      alpha;
+  float      gamma;
+  MMPar (u8, u64, u8, u8, float, float);
+  MMPar (u8, u8, float, float);
+  void set_cner (Container);
 };
 class STMMPar : public ModelPar {
  public:
   u8         thresh;  // Substitutional tolerant Markov models threshold
+  u8         ir;       // Markov models Inverted repeat
+  float      alpha;
+  float      gamma;
+  STMMPar (u8, u8, u8, float, float);
+  void set_cner (Container);
 };
 
 //struct ModelPar {
@@ -77,12 +86,14 @@ class FCM    // Finite-context models
   double          aveEnt;
   
   explicit FCM (const Param&);
-  void store (const Param&);   // Build FCM (finite-context models)
-  void compress (const Param&);
+//  void store (const Param&);   // Build FCM (finite-context models)
+//  void compress (const Param&);
 //  void report     (const Param&) const;
 
  private:
-  vector<ModelPar>              models;
+//  vector<ModelPar>              models;
+  vector<MMPar>                 MMs;
+  vector<STMMPar>               STMMs;
   vector<shared_ptr<Table64>>   tbl64;
   vector<shared_ptr<Table32>>   tbl32;
   vector<shared_ptr<LogTable8>> lgtbl8;
@@ -93,21 +104,21 @@ class FCM    // Finite-context models
   void config (const Param&); // Set models parameters
   template <class InIter, class Vec>  //Split by dlim
   void split        (InIter, InIter, char, Vec&) const;
-  void set_container ();
-  void alloc_model ();             // Allocate memory to models
+//  void set_cner ();
+//  void alloc_model ();             // Allocate memory to models
 //  void setModesComb ();             // Set combination of modes of models
 //  void setIRsComb   ();             // Set combination of inv. repeats of models
-  void store_1_thr (const Param&); // Build models one thread
-  void store_n_thr (const Param&); // Build models multiple threads
-  template <class Mask, class CnerIter>
-  void store_impl (const string&, Mask, CnerIter);    // Fill data structure
+//  void store_1_thr (const Param&); // Build models one thread
+//  void store_n_thr (const Param&); // Build models multiple threads
+//  template <class Mask, class CnerIter>
+//  void store_impl (const string&, Mask, CnerIter);    // Fill data structure
   // Compress data structure
-  template <
-//    class msk_t,
-      class CnerIter>
-  void compress_1_MM (const string&,
-//                      msk_t,
-                      CnerIter);  // 1 Markov models
+//  template <
+////    class msk_t,
+//      class CnerIter>
+//  void compress_1_MM (const string&,
+////                      msk_t,
+//                      CnerIter);  // 1 Markov models
 //  template <class msk0_t, class msk1_t, class ds0_t, class ds1_t>
 //  void comp2mdl  (const string&, msk0_t, msk1_t, const ds0_t&, const ds1_t&);
 //  void comp4mdl  (const string&);   // It has all possible models
