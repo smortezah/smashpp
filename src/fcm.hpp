5b7fd6bc000f96d5efe7c34a035fb8b3e73e29a1
Morteza <s_morteza_h@yahoo.com>
2018-05-04 13:42:31 +0100

    Minor changes
    Former-commit-id: 8d7ea2fd35a37782458da3fe5b83e52c8b51208a

--- src/fcm.hpp
+++ src/fcm.hpp
@@ -14,29 +14,8 @@
 using std::unique_ptr;
 using std::shared_ptr;
 using std::make_unique;
-//using std::initializer_list;
 using std::vector;
 
-//class ModelPar {
-// public:
-//  u8        k;         // Context size
-//  u8        thresh;  // Substitutional tolerant Markov models threshold
-//  u64       w;         // Width of count-min-log sketch
-//  u8        d;         // Depth of count-min-log sketch
-//  u8        ir;       // Markov models Inverted repeat
-//  float     alpha;
-//  float     gamma;
-//  Container cont;      // Tbl 64, Tbl 32, LogTbl 8, Sketch 4
-////  u8        cnerIdx; // Index of the container
-//  Mode      mode;
-//  bool      enable; // For STMM only
-//  ModelPar (u8, u8, u64, u8, u8, float, float);
-//  ModelPar (u8, u64, u8, u8, float, float);
-//  ModelPar (u8, u8, float, float);
-//  ModelPar (u8, u8, u8, float, float);
-//};
-
-//todo
 struct STMMPar;
 
 struct MMPar {
@@ -84,12 +63,12 @@ class FCM    // Finite-context models
   double aveEnt;
   
   explicit FCM (const Param&);
-//  void store (const Param&);   // Build FCM (finite-context models)
-//  void compress (const Param&);
+  void store (const Param&);   // Build FCM (finite-context models)
+  void compress (const Param&);
 ////  void report     (const Param&) const;
 
  private:
-//  vector<ModelPar>              models;
+///  vector<ModelPar>              models;
   vector<MMPar>                 Ms;     // Markov models
   vector<STMMPar>               TMs;    // Tolerant Markov models
   vector<shared_ptr<Table64>>   tbl64;
@@ -102,29 +81,29 @@ class FCM    // Finite-context models
   void split (InIter, InIter, char, Vec&) const;
   void set_cont ();
   void alloc_model ();            // Allocate memory to models
-//  void store_1 (const Param&);    // Build models one thread
-//  void store_n (const Param&);    // Build models multiple threads
-//  template <class Mask, class CnerIter>
-//  void store_impl (const string&, Mask, CnerIter);  // Fill data structure
-//  // Compress data structure
-//  template <class CnerIter>
-//  void compress_1 (const string&, CnerIter);        // 1 Markov models
+  void store_1 (const Param&);    // Build models one thread
+  void store_n (const Param&);    // Build models multiple threads
+  template <class Mask, class CnerIter>
+  void store_impl (const string&, Mask, CnerIter);  // Fill data structure
+  // Compress data structure
+  template <class CnerIter>
+  void compress_1 (const string&, CnerIter);        // 1 Markov models
 //  void compress_n (const string&);
-//  template <class CnerIter, class ProbParIter>
-//  double prob    (CnerIter, ProbParIter) const;  // Probability
-//  template <class CnerIter, class ProbParIter>
-//  double probIr  (CnerIter, ProbParIter) const;  // Prob. IR
-//  double entropy (double) const;
+  template <class CnerIter, class ProbParIter>
+  double prob    (CnerIter, ProbParIter) const;  // Probability
+  template <class CnerIter, class ProbParIter>
+  double probIr  (CnerIter, ProbParIter) const;  // Prob. IR
+  double entropy (double) const;
 //  template <class OutIter, class InIter>
 //  double entropy (OutIter, InIter, InIter) const;
 //  template <class OutIter, class InIter>
 //  void update_weights (OutIter, InIter, InIter) const;
 //  template <class OutIter, class InIter>
 //  void normalize (OutIter, InIter, InIter) const;
-//  template <class ProbParIter>
-//  void update_ctx (u64&, ProbParIter) const;
-//  template <class ProbParIter>
-//  void update_ctx (u64&, u64&, ProbParIter) const;
+  template <class ProbParIter>
+  void update_ctx (u64&, ProbParIter) const;
+  template <class ProbParIter>
+  void update_ctx (u64&, u64&, ProbParIter) const;
 };
 
 #endif //SMASHPP_FCM_HPP
