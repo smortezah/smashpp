7e07b09a572cfcedeacf4da8031760dd193b641e
Morteza <s_morteza_h@yahoo.com>
2018-10-04 21:57:02 +0100

    Minor changes

--- src/fcm.hpp
+++ src/fcm.hpp
@@ -41,10 +41,8 @@ class FCM    // Finite-context models
   void store_impl  (const string&, Mask, ContIter);  // Fill data structure
 
   template <typename ContIter>
-  void compress_1     (const string&, const string&,
-                       ContIter);       // Compress with 1 Markov model
-  void compress_n     (const string&,
-                       const string&);  // Compress with n Markov models
+  void compress_1 (const string&, const string&, ContIter); //Compress with 1 MM
+  void compress_n     (const string&, const string&); //Compress with n Markov M
   void compress_n_ave (const string&, const string&, shared_ptr<CompressPar>);
   template <typename ContIter>
   void compress_n_impl           (shared_ptr<CompressPar>, ContIter);
