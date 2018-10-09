9de36dbedc6ac1c5500c409ed14d072716fbe047
Morteza <s_morteza_h@yahoo.com>
2018-10-09 15:47:03 +0100

    Minor changes
    Former-commit-id: 0e8dc93932ab644c7825503df60edec2dd21df37

--- src/tbl64.cpp
+++ src/tbl64.cpp
@@ -13,7 +13,7 @@ Table64::Table64 (u8 k_) {
 
 void Table64::config (u8 k_) {
   k   = k_;
-  tot = 0;
+//  tot = 0;
   try { tbl.resize(4ull<<(k<<1u)); }    // 4<<2k = 4*2^2k = 4*4^k = 4^(k+1)
   catch (std::bad_alloc& b) {
     error("failed memory allocation.");
@@ -22,26 +22,12 @@ void Table64::config (u8 k_) {
 
 void Table64::update (u32 ctx) {
   ++tbl[ctx];
-  ++tot;
+//  ++tot;
 }
 
 u64 Table64::query (u32 ctx) const {
   return tbl[ctx];
 }
-
-#ifdef DEBUG
-u64 Table64::getTotal () const {
-  return tot;
-}
-
-u64 Table64::countMty () const {
-  return static_cast<u64>(std::count(tbl.begin(), tbl.end(), 0));
-}
-
-u64 Table64::maxTblVal () const {
-  return *std::max_element(tbl.begin(), tbl.end());
-}
-#endif
 
 void Table64::dump (ofstream& ofs) const {
   ofs.write((const char*) &tbl[0], tbl.size());
@@ -53,6 +39,18 @@ void Table64::load (ifstream& ifs) const {
 }
 
 #ifdef DEBUG
+u64 Table64::getTotal () const {
+//  return tot;
+}
+
+u64 Table64::countMty () const {
+  return static_cast<u64>(std::count(tbl.begin(), tbl.end(), 0));
+}
+
+u64 Table64::maxTblVal () const {
+  return *std::max_element(tbl.begin(), tbl.end());
+}
+
 void Table64::print () const {
   constexpr u8 context_width {12};
   cerr.width(context_width);  cerr<<std::left<<"Context";
--- src/tbl64.hpp
+++ src/tbl64.hpp
@@ -11,26 +11,24 @@
 class Table64
 {
 public:
-  Table64          () : k(0), tot(0) {}
+  Table64          () : k(0)/*, tot(0)*/ {}
   explicit Table64 (u8);
   void config      (u8);
   void update      (u32);               // Update table
   u64  query       (u32)       const;   // Query count of ctx
+  void dump        (ofstream&) const;
+  void load        (ifstream&) const;
 #ifdef DEBUG
   u64  getTotal    ()          const;   // Total count of all items in the table
   u64  countMty    ()          const;   // Number of empty cells in the table
   u64  maxTblVal   ()          const;
-#endif
-  void dump        (ofstream&) const;
-  void load        (ifstream&) const;
-#ifdef DEBUG
   void print       ()          const;
 #endif
   
 private:
   vector<u64> tbl;                      // Table of 64 bit counters
   u8          k;                        // Ctx size
-  u64         tot;                      // Total # elements so far
+//  u64         tot;                      // Total # elements so far
 };
 
 #endif //SMASHPP_TABLE64_HPP
