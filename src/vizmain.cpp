9c24fcb53e580c17806bc484f07c0ad9eeb58c68
Morteza Hosseini <s_morteza_h@yahoo.com>
2018-11-01 00:19:15 +0000

    Minor Changes
    Former-commit-id: 2726eae90e816da73324d950ace6ed0d5f63beeb

--- src/vizmain.cpp
+++ src/vizmain.cpp
@@ -9,19 +9,23 @@ using namespace smashpp;
 // - - - - - - - - - - - - - - - - - M A I N - - - - - - - - - - - - - - - - -
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 int main (int argc, char* argv[]) {
-  const auto t0 {now()};
+  try {
+    const auto t0 {now()};
 
-  VizParam par;
-  par.parse(argc, argv);
+    VizParam par;
+    par.parse(argc, argv);
 
-  cerr << endl;
-  auto paint = make_shared<VizPaint>();
-  paint->print_plot(par);
-  cerr << endl;
+    cerr << endl;
+    auto paint = make_shared<VizPaint>();
+    paint->print_plot(par);
+    cerr << endl;
 
-  const auto t1 {now()};
-  cerr << "==[ STATISTICS ]====================\n"
-       << "Total time: " << hms(t1-t0) << endl;
+    const auto t1 {now()};
+    cerr << "==[ STATISTICS ]====================\n"
+         << "Total time: " << hms(t1-t0) << endl;
+  }
+  catch (std::exception& e) { cout << e.what(); }
+  catch (...) { return EXIT_FAILURE; }
 
-  return EXIT_SUCCESS;
+  return 0;
 }
