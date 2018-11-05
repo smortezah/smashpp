90e76d84fcd6a20c8b3ef069a3d3ed9540343b5a
Morteza <s_morteza_h@yahoo.com>
2018-11-05 11:51:55 +0000

    Minor change

--- src/main.cpp
+++ src/main.cpp
@@ -24,76 +24,89 @@ using namespace smashpp;
 int main (int argc, char* argv[]) {
   try {
     const auto t0{now()};
-    Param par;
-    par.parse(argc, argv);              // Parse the command
+//    Param par;
+//    par.parse(argc, argv);              // Parse the command
 
-    if (par.viz) {
-      VizParam vizpar;
-      vizpar.parse(argc, argv);
-      auto paint = make_shared<VizPaint>();
-      paint->print_plot(vizpar);
+
+    for(int i=0;i<argc;++i) {
+//      cerr << argv[i] << ' ';
+    if (std::find(argv, argv + argc, "-viz") == argv + argc)
+      cerr << "yas";
     }
-    else {
-      if (par.compress) {
-        auto models = make_shared<FCM>(par);
-        models->store(par);
-        models->compress(par);
-      }
-      else if (par.filter) {
-        auto filter = make_shared<Filter>(par);
-        filter->smooth_seg(par);
-      }
-      else if (par.segment) {
-        auto filter = make_shared<Filter>(par);
-        filter->smooth_seg(par);
-        filter->extract_seg(par.tar, par.ref);
-      }
-      else {
-        const auto tempRef = par.ref, tempTar = par.tar;
-        auto models = make_shared<FCM>(par);    // == auto* models=new FCM(par);
-        models->store(par);                     // Build models
-        models->compress(par);                  // Compress
-        auto filter = make_shared<Filter>(par);
-        filter->smooth_seg(par);                // Filter and segment
-        filter->extract_seg(par.tar, par.ref);  // Extract segs from target
-
-        cerr <<"\n===[ Building reference map for each target pattern ]=======";
-        // Consider the ref as new tar and the tar segments as new refs
-        const auto newTar  = par.ref;
-        const auto segName = gen_name(par.ref, par.tar, Format::SEGMENT);
-        par.tar = newTar;
-        for (u64 i=0; i!=filter->nSegs; ++i) {
-          par.ref = segName+to_string(i);
-          par.thresh = 1.5;//todo
-          models = make_shared<FCM>(par);
-          models->store(par);
-          models->compress(par);
-          filter = make_shared<Filter>(par);
-          filter->smooth_seg(par);
-        }
-        filter->aggregate_pos(tempRef, tempTar);
-
-        for (u64 i=0; i!=filter->nSegs; ++i)
-          if (!par.saveAll && !par.saveSegment)
-            remove((segName+to_string(i)).c_str());
-
-        // Remove temporary sequences generated from Fasta/Fastq input files
-        if (!par.saveSeq) {
-          if (par.refType==FileType::FASTA || par.refType==FileType::FASTQ) {
-            remove(tempRef.c_str());
-            rename((tempRef+LBL_BAK).c_str(), tempRef.c_str());
-          }
-          if (par.tarType==FileType::FASTA || par.tarType==FileType::FASTQ) {
-            remove(tempTar.c_str());
-            rename((tempTar+LBL_BAK).c_str(), tempTar.c_str());
-          }
-        }
+//    if (std::find(argv, argv + argc, viz) != argv + argc)
+//      cerr << "yas";
 
 
-//        // Report
-//        models->report(par); // Without "-R" does nothing
-      }
-    }
+
+
+
+//    if (par.viz) {
+//      VizParam vizpar;
+//      vizpar.parse(argc, argv);
+//      auto paint = make_shared<VizPaint>();
+//      paint->print_plot(vizpar);
+//    }
+//    else {
+//      if (par.compress) {
+//        auto models = make_shared<FCM>(par);
+//        models->store(par);
+//        models->compress(par);
+//      }
+//      else if (par.filter) {
+//        auto filter = make_shared<Filter>(par);
+//        filter->smooth_seg(par);
+//      }
+//      else if (par.segment) {
+//        auto filter = make_shared<Filter>(par);
+//        filter->smooth_seg(par);
+//        filter->extract_seg(par.tar, par.ref);
+//      }
+//      else {
+//        const auto tempRef = par.ref, tempTar = par.tar;
+//        auto models = make_shared<FCM>(par);    // == auto* models=new FCM(par);
+//        models->store(par);                     // Build models
+//        models->compress(par);                  // Compress
+//        auto filter = make_shared<Filter>(par);
+//        filter->smooth_seg(par);                // Filter and segment
+//        filter->extract_seg(par.tar, par.ref);  // Extract segs from target
+//
+//        cerr <<"\n===[ Building reference map for each target pattern ]=======";
+//        // Consider the ref as new tar and the tar segments as new refs
+//        const auto newTar  = par.ref;
+//        const auto segName = gen_name(par.ref, par.tar, Format::SEGMENT);
+//        par.tar = newTar;
+//        for (u64 i=0; i!=filter->nSegs; ++i) {
+//          par.ref = segName+to_string(i);
+//          par.thresh = 1.5;//todo
+//          models = make_shared<FCM>(par);
+//          models->store(par);
+//          models->compress(par);
+//          filter = make_shared<Filter>(par);
+//          filter->smooth_seg(par);
+//        }
+//        filter->aggregate_pos(tempRef, tempTar);
+//
+//        for (u64 i=0; i!=filter->nSegs; ++i)
+//          if (!par.saveAll && !par.saveSegment)
+//            remove((segName+to_string(i)).c_str());
+//
+//        // Remove temporary sequences generated from Fasta/Fastq input files
+//        if (!par.saveSeq) {
+//          if (par.refType==FileType::FASTA || par.refType==FileType::FASTQ) {
+//            remove(tempRef.c_str());
+//            rename((tempRef+LBL_BAK).c_str(), tempRef.c_str());
+//          }
+//          if (par.tarType==FileType::FASTA || par.tarType==FileType::FASTQ) {
+//            remove(tempTar.c_str());
+//            rename((tempTar+LBL_BAK).c_str(), tempTar.c_str());
+//          }
+//        }
+//
+//
+////        // Report
+////        models->report(par); // Without "-R" does nothing
+//      }
+//    }
 
     const auto t1{now()};
     cerr << OUT_SEP << "Total time: " << hms(t1-t0);
