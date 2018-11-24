8012f5e4892264a313644707292ddc70ca969f75
Morteza Hosseini <s_morteza_h@yahoo.com>
2018-11-24 22:10:51 +0000

    minor change
    Former-commit-id: 44bc25963a70b281ed9cb1b2e18b9bdd821ec263 [formerly d857ca5822a54b874a1feaaa5aeb9401d6638ae6] [formerly e60595996d6ec5bd4c3dfa0fd97d36ed32462642] [formerly e33189d512ca40e5abf8ad008d374066fa50b1e3 [formerly 86c046d146689720ef7a16c2f38e9bd5e345748e]]
    Former-commit-id: 42b444dbc2f9319368e540b609cb58b15299db9c
    Former-commit-id: e93b8004431ae7f1537e61b601c14e651443cb19
    Former-commit-id: f90982ed23848b53f2648d9901c9dbfe6f749708

--- src/vizpaint.hpp
+++ src/vizpaint.hpp
@@ -23,9 +23,8 @@ class VizPaint {
   u32 ratio;
   u32 mult;
 
-  auto show_info (VizParam&, const string&, const string&, u64, u64) const ->
-    void;
-  auto config (double, double, u64, u64) -> void;
+  void show_info (VizParam&, const string&, const string&, u64, u64) const;
+  void config (double, double, u64, u64);
   auto hsv_to_rgb (const HsvColor&) const -> RgbColor;
   auto rgb_to_hsv (const RgbColor&) const -> HsvColor;
   auto rgb_color (u8) const -> string;
@@ -36,16 +35,16 @@ class VizPaint {
   auto customColor (u32) const -> string;
   auto nrc_color (double, u32) const -> string;
   auto redun_color (double, u32) const -> string;
-  auto print_head (ofstream&, double, double) const -> void;
-  auto print_tail (ofstream&) const -> void;
+  void print_head (ofstream&, double, double) const;
+  void print_tail (ofstream&) const;
   template <typename Value>
   auto get_point (Value) const -> double;
-  auto plot_legend (ofstream&, const VizParam&) const -> void;
+  void plot_legend (ofstream&, const VizParam&) const;
   auto tspan (u32, i64) const -> string;
   auto tspan (u32, const string&) const -> string;
-  auto sort_merge (string&) const -> void;
-  // template <typename Position>
-  // auto plot_pos (const Position&, bool, bool, u64) const -> void;
+  void sort_merge (string&) const;
+  template <typename Position>
+  void plot_pos (ofstream&, const Position&, double, u64, string&&) const;
 };
 }
 
