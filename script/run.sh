1563d63d32cf9a7568676817d857d506fc7198fc
Morteza <s_morteza_h@yahoo.com>
2018-12-06 12:18:48 +0000

    change

--- script/run.sh
+++ script/run.sh
@@ -26,10 +26,10 @@ if [[ $SYNTHETIC -eq 1 ]]; then
                           -ls 50 -n 5 -f 0.3,0.2,0.2,0.3,0.0                 r_d
   cat r_a r_b r_c r_d > refs
 
-  ./goose-mutatedna -mr 0.03 < r_d > t_a
-  ./goose-mutatedna -mr 0.05 < r_b > t_c
-  ./goose-mutatedna -mr 0.10 < r_a > t_b
-  ./goose-mutatedna -mr 0.15 < r_c > t_d
+  ./goose-mutatedna -mr 0.00 < r_d > t_a
+  ./goose-mutatedna -mr 0.01 < r_b > t_c
+  ./goose-mutatedna -mr 0.02 < r_a > t_b
+  ./goose-mutatedna -mr 0.05 < r_c > t_d
   cat t_a t_b t_c t_d > tars
 
   ### Medium sizes: ref:100000, tar:100000
@@ -43,10 +43,10 @@ if [[ $SYNTHETIC -eq 1 ]]; then
                           -ls 100 -n 250 -f 0.25,0.25,0.25,0.25,0.0          r_d
   cat r_a r_b r_c r_d > refm
 
-  ./goose-mutatedna -mr 0.03 < r_d > t_a
-  ./goose-mutatedna -mr 0.05 < r_c > t_b
-  ./goose-mutatedna -mr 0.10 < r_b > t_c
-  ./goose-mutatedna -mr 0.15 < r_a > t_d
+  ./goose-mutatedna -mr 0.00 < r_d > t_a
+  ./goose-mutatedna -mr 0.01 < r_c > t_b
+  ./goose-mutatedna -mr 0.02 < r_b > t_c
+  ./goose-mutatedna -mr 0.05 < r_a > t_d
   cat t_a t_b t_c t_d > tarm
 
   ### Large sizes: ref:5000000, tar:5000000
@@ -59,11 +59,11 @@ if [[ $SYNTHETIC -eq 1 ]]; then
   ./goose-fastqsimulation -eh -eo -es -edb -rn 1 -ri 500 -ra 1500 -rm 0 \
                           -ls 100 -n 12500 -f 0.25,0.25,0.25,0.25,0.0 -s 7   r_d
   cat r_a r_b r_c r_d > refl
-
-  ./goose-mutatedna -mr 0.03 < r_c > t_a
-  ./goose-mutatedna -mr 0.05 < r_b > t_b
-  ./goose-mutatedna -mr 0.10 < r_a > t_c
-  ./goose-mutatedna -mr 0.15 < r_d > t_d
+  
+  ./goose-mutatedna -mr 0.00 < r_c > t_a
+  ./goose-mutatedna -mr 0.01 < r_b > t_b
+  ./goose-mutatedna -mr 0.02 < r_a > t_c
+  ./goose-mutatedna -mr 0.05 < r_d > t_d
   cat t_a t_b t_c t_d > tarl
 
   rm r_* t_*
