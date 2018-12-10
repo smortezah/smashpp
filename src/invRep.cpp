b4bbcec16f508e3c386c506d5e7ddd9b61c8bc98
Morteza Hosseini <s_morteza_h@yahoo.com>
2018-12-10 19:41:57 +0000

    change
    Former-commit-id: c2f5e5839476ccc0f0fbf05d3069726e6010b49f [formerly e998fd2bc7565d390c70d41179498018de4fd3ba] [formerly 814d23d6443010150a3922a7b58e2d90cc938b08] [formerly d5e11bc3c40e775901a227b40a559ab6afb0122f [formerly 022295429eb0fe814364600260338d4f4d712d63]] [formerly 03be099fa0ac9e528a3c54e0441dad43c9e488ff [formerly 24207596dd467cb296251bf7bfecb33f76b471e6]]
    Former-commit-id: 04369550192eb29c05c56daba250c3a0b3b98f7e
    Former-commit-id: 1c12f25eb23e281d7ca0ed63a680b3bcd93acab3
    Former-commit-id: 8fa891c2e932fa11e90d5793892c2f75757c93a0 [formerly 40d6bb97ee6b4d93eb9d143ce77b04a3c58b5729]
    Former-commit-id: 254a2dddae8be82d0c8c50158324cc670d5ac467
    Former-commit-id: 099fe44b028bcde2b4c21c57a3f10cc22cdccd33

--- src/invRep.cpp
+++ src/invRep.cpp
@@ -30,15 +30,13 @@ int main (int argc, char* argv[]) {
   inFile.read(buffer.data(), size);
   inFile.close();
 
-  // reverse(buffer.begin(), buffer.end());
+  reverse(buffer.begin(), buffer.end());
 
-  // // for (auto& c : buffer)
-  // //   c = REV[static_cast<unsigned char>(c)];
+  for (auto& c : buffer)
+    c = REV[static_cast<unsigned char>(c)];
 
   //   for (auto it=buffer.data();it<buffer.data()+size;++it)
   //     *it = REV[static_cast<unsigned char>(*it)];
-
-
 
   ofstream outFile(outFileName);
   outFile.write(buffer.data(), size);
