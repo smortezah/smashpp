90e15d31cf6ea79c8cfe8886ae90348dc16f8515
Morteza <s_morteza_h@yahoo.com>
2018-12-11 14:23:18 +0000

    change
    Former-commit-id: 047049f16d463c581d922f105c160780abab003b [formerly 5313725b13ffc59ce8f3d63b96772ef35feace3b] [formerly 15a6643970d0c8e751f0f27507383094d6e08a4e] [formerly f8f7f926d15eaecbac590b87de57bae2b8b428a4 [formerly 4f8cccd6b7a3e69461eba8d46edcda52e96cbbe7]] [formerly dc7ad1eee03bcf475e471d8ad763b1e78e0e1236 [formerly 06261a083a15855cf4c8085ab2396fad4d6c5aa0]]
    Former-commit-id: c4f170a412cde7876559df608770e4c34b192b5b
    Former-commit-id: 1c9215278541b06bd6108e7bf072e7c0fe0edecd
    Former-commit-id: abb4907c7fce275e9c85eb59f67036a725031edd [formerly 639afaa5e34ef2118fbaf03490550ab1e46ac6e4]
    Former-commit-id: 0abc88fd8a87237ea6d22bc0a0bad8eedd2c3edc
    Former-commit-id: 99bee138208b448ac572470024d27f362672bcfe

--- script/run.sh
+++ script/run.sh
@@ -16,79 +16,55 @@ fi
 ### Simulate synthetic dataset
 if [[ $SYNTHETIC -eq 1 ]]; then
   ### Small sizes: ref:1000, tar:1000
-  # # ./goose-fastqsimulation -eh -eo -es -edb -rn 2 -ri 5 -ra 15 -rm 0 -s 101 \
-  # #                         -ls 50 -n 5 -f 0.2,0.3,0.3,0.2,0.0                 r_a
-  # # ./goose-fastqsimulation -eh -eo -es -edb -rn 1 -ri 5 -ra 20 -rm 0 -s 68 \
-  # #                         -ls 50 -n 5 -f 0.25,0.25,0.25,0.25,0.0             r_b
-  # # ./goose-fastqsimulation -eh -eo -es -edb -rn 3 -ri 5 -ra 10 -rm 0 -s 3 \
-  # #                         -ls 50 -n 5 -f 0.25,0.25,0.25,0.25,0.0             r_c
-  # # ./goose-fastqsimulation -eh -eo -es -edb -rn 1 -ri 5 -ra 20 -rm 0 -s 1785 \
-  # #                         -ls 50 -n 5 -f 0.3,0.2,0.2,0.3,0.0                 r_d
-  # ./goose-fastqsimulation -eh -eo -es -edb -rm 0 -s 201 -ls 50 -n 5 \
-  #                         -f 0.2,0.3,0.3,0.2,0.0                 r_a
-  # ./goose-fastqsimulation -eh -eo -es -edb -rm 0 -s 58 -ls 50 -n 5 \
-  #                         -f 0.25,0.25,0.25,0.25,0.0             r_b
-  # ./goose-fastqsimulation -eh -eo -es -edb -rm 0 -s 3 -ls 50 -n 5 \
-  #                         -f 0.25,0.25,0.25,0.25,0.0             r_c
-  # ./goose-fastqsimulation -eh -eo -es -edb -rm 0 -s 1785 -ls 50 -n 5 \
-  #                         -f 0.3,0.2,0.2,0.3,0.0                 r_d
-  # cat r_a r_b r_c r_d > refs
+  ./goose-fastqsimulation -eh -eo -es -edb -rm 0 -s 201 -ls 50 -n 5 \
+                          -f 0.2,0.3,0.3,0.2,0.0      r_a
+  ./goose-fastqsimulation -eh -eo -es -edb -rm 0 -s 58 -ls 50 -n 5 \
+                          -f 0.25,0.25,0.25,0.25,0.0  r_b
+  ./goose-fastqsimulation -eh -eo -es -edb -rm 0 -s 3 -ls 50 -n 5 \
+                          -f 0.25,0.25,0.25,0.25,0.0  r_c
+  ./goose-fastqsimulation -eh -eo -es -edb -rm 0 -s 1785 -ls 50 -n 5 \
+                          -f 0.3,0.2,0.2,0.3,0.0      r_d
+  cat r_a r_b r_c r_d > refs
 
-  # echo "CTNGATNNCCGGA" > refs;
-  # echo "CTNGATNNCCGGA" > tars;
-
-  # cp r_a t_b
-  # cp r_b t_c
-  # cp r_c t_d
-  # cp r_d t_a
-  # # ./goose-mutatedna -mr 0.00 < r_d > t_a
-  # # ./goose-mutatedna -mr 0.01 < r_b > t_c
-  # # ./goose-mutatedna -mr 0.02 < r_a > t_b
-  # # ./goose-mutatedna -mr 0.05 < r_c > t_d
-  # cat t_a t_b t_c t_d > tars
+  cp                           r_d   t_a
+  cp                           r_a   t_b
+  ./goose-mutatedna -mr 0.01 < r_b > t_c
+  ./smashpp-inv-rep            r_c   t_d
+  cat t_a t_b t_c t_d > tars
 
   ### Medium sizes: ref:100000, tar:100000
-  # ./goose-fastqsimulation -eh -eo -es -edb -rm 0 -s 1001 -ls 100 -n 250 \
-  #                         -f 0.25,0.25,0.25,0.25,0.0  r_a
-  # ./goose-fastqsimulation -eh -eo -es -edb -rm 0 -s 608 -ls 100 -n 250 \
-  #                         -f 0.3,0.2,0.2,0.3,0.0      r_b
-  # ./goose-fastqsimulation -eh -eo -es -edb -rm 0 -s 30 -ls 100 -n 250 \
-  #                         -f 0.2,0.3,0.3,0.2,0.0      r_c
-  # ./goose-fastqsimulation -eh -eo -es -edb -rm 0 -s 17805 -ls 100 -n 250 \
-  #                         -f 0.25,0.25,0.25,0.25,0.0  r_d
-  # cat r_a r_b r_c r_d > refm
+  ./goose-fastqsimulation -eh -eo -es -edb -rm 0 -s 1001 -ls 100 -n 250 \
+                          -f 0.25,0.25,0.25,0.25,0.0  r_a
+  ./goose-fastqsimulation -eh -eo -es -edb -rm 0 -s 608 -ls 100 -n 250 \
+                          -f 0.3,0.2,0.2,0.3,0.0      r_b
+  ./goose-fastqsimulation -eh -eo -es -edb -rm 0 -s 30 -ls 100 -n 250 \
+                          -f 0.2,0.3,0.3,0.2,0.0      r_c
+  ./goose-fastqsimulation -eh -eo -es -edb -rm 0 -s 17805 -ls 100 -n 250 \
+                          -f 0.25,0.25,0.25,0.25,0.0  r_d
+  cat r_a r_b r_c r_d > refm
 
-  # ./goose-mutatedna -mr 0.00 < r_a > t_a
-  # ./smashpp-inv-rep            r_b   t_b
-#  ./goose-mutatedna -mr 0.00  < r_b > t_c
-  # ./goose-mutatedna -mr 0.00 < r_c > t_b
-  # ./goose-mutatedna -mr 0.00 < r_d > t_a
-  # cat t_a t_b t_c t_d > tarm
+  ./smashpp-inv-rep            r_b   t_a
+  cp                           r_c   t_b
+  ./goose-mutatedna -mr 0.01 < r_d > t_c
+  cp                           r_a   t_d
+  cat t_a t_b t_c t_d > tarm
 
-  # ### Large sizes: ref:5000000, tar:5000000
+  ### Large sizes: ref:5000000, tar:5000000
   ./goose-fastqsimulation -eh -eo -es -edb -rn 2 -ri 500 -ra 1000 -rm 0 \
                           -ls 100 -n 12500 -f 0.3,0.2,0.3,0.2,0.0 -s 10101   r_a
   ./goose-fastqsimulation -eh -eo -es -edb -rn 1 -ri 500 -ra 1000 -rm 0 \
                           -ls 100 -n 12500 -f 0.3,0.2,0.2,0.3,0.0 -s 6018    r_b
-  # ./goose-fastqsimulation -eh -eo -es -edb -rn 3 -ri 500 -ra 1000 -rm 0 \
-  #                         -ls 100 -n 12500 -f 0.25,0.25,0.25,0.25,0.0 -s 10  r_c
-  # ./goose-fastqsimulation -eh -eo -es -edb -rn 1 -ri 500 -ra 1000 -rm 0 \
-  #                         -ls 100 -n 12500 -f 0.25,0.25,0.25,0.25,0.0 -s 7   r_d
-  # cat r_a r_b r_c r_d > refl
-
-  cat r_a r_b > refl
-  cp r_b t_a
-  ./smashpp-inv-rep r_a t_b
-  cat t_a t_b > tarl
-
-  # cp r_a refl
-  # ./smashpp-inv-rep r_a tarl
+  ./goose-fastqsimulation -eh -eo -es -edb -rn 3 -ri 500 -ra 1000 -rm 0 \
+                          -ls 100 -n 12500 -f 0.25,0.25,0.25,0.25,0.0 -s 10  r_c
+  ./goose-fastqsimulation -eh -eo -es -edb -rn 1 -ri 500 -ra 1000 -rm 0 \
+                          -ls 100 -n 12500 -f 0.25,0.25,0.25,0.25,0.0 -s 7   r_d
+  cat r_a r_b r_c r_d > refl
   
-  # ./goose-mutatedna -mr 0.00 < r_c > t_a
-  # ./goose-mutatedna -mr 0.01 < r_b > t_b
-  # ./goose-mutatedna -mr 0.02 < r_a > t_c
-  # ./goose-mutatedna -mr 0.05 < r_d > t_d
-  # cat t_a t_b t_c t_d > tarl
+  cp                           r_c   t_a
+  ./goose-mutatedna -mr 0.01 < r_b > t_b
+  cp                           r_a   t_c
+  ./smashpp-inv-rep            r_d   t_d
+  cat t_a t_b t_c t_d > tarl
 
   rm r_* t_*
 fi
