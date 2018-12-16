673896780cc2ce129a663e04a712ce388e55a68d
Morteza Hosseini <s_morteza_h@yahoo.com>
2018-12-16 22:48:43 +0000

    change
    Former-commit-id: 0179c26953ef925647ef2a3c0f0c51e2d65e0728 [formerly 99ce62883f7516eb981f0e4a405e9556d09b85e1] [formerly b6fdd0e618a7988f078ee55fe72d107cf8a38250] [formerly abf52a093c44fd175607a01769615f865b0c2e63 [formerly d7721c89083631d3314ed862f235c7555204c446]] [formerly 93b9df93290dd12903ac820fbd96ef5a6139b901 [formerly 9e066b72c0798ab21bb3e2b87165c07b701bba34]]
    Former-commit-id: 5dcb5081281c969d0b89129798da2d4c5266a86f
    Former-commit-id: 9c70bdfcd7393d4aa95af75248a173263eafdad3
    Former-commit-id: 8b8e5afe85d5d39e6f5b1ddc5b783655109df4cf
    Former-commit-id: 278ee6dc73cd507c15b0f248698634184c38ba36 [formerly b3b56776c5c44b4c65932da8231cf0d9a724ce2a]
    Former-commit-id: c5d461ece220ef2e7a0ef6bea989a7854311d3a1
    Former-commit-id: baa8fb67bc6f1b830ce25b8636f65dbb58b0264b

--- script/run.sh
+++ script/run.sh
@@ -18,56 +18,56 @@ fi
 if [[ $DATASET_SYNTH -eq 1 ]]; then
   chmod 777 smashpp-inv-rep
 
-  # ### Small sizes: ref:1.000, tar:1.000
-  # ./goose-fastqsimulation \
-  #   -eh -eo -es -edb -rm 0 -f 0.25,0.25,0.25,0.25,0.0 -ls 50 -n 5 -s 201   r_a
-  # ./goose-fastqsimulation \
-  #   -eh -eo -es -edb -rm 0 -f 0.20,0.30,0.30,0.20,0.0 -ls 50 -n 5 -s 58    r_b
-  # ./goose-fastqsimulation \
-  #   -eh -eo -es -edb -rm 0 -f 0.25,0.25,0.25,0.25,0.0 -ls 50 -n 5 -s 3     r_c
-  # ./goose-fastqsimulation \
-  #   -eh -eo -es -edb -rm 0 -f 0.30,0.20,0.20,0.30,0.0 -ls 50 -n 5 -s 1797  r_d
-  # cat r_a r_b r_c r_d > RefS
+  ### Small sizes: ref:1.000, tar:1.000
+  ./goose-fastqsimulation \
+    -eh -eo -es -edb -rm 0 -f 0.25,0.25,0.25,0.25,0.0 -ls 50 -n 5 -s 201   r_a
+  ./goose-fastqsimulation \
+    -eh -eo -es -edb -rm 0 -f 0.20,0.30,0.30,0.20,0.0 -ls 50 -n 5 -s 58    r_b
+  ./goose-fastqsimulation \
+    -eh -eo -es -edb -rm 0 -f 0.25,0.25,0.25,0.25,0.0 -ls 50 -n 5 -s 3     r_c
+  ./goose-fastqsimulation \
+    -eh -eo -es -edb -rm 0 -f 0.30,0.20,0.20,0.30,0.0 -ls 50 -n 5 -s 1797  r_d
+  cat r_a r_b r_c r_d > RefS
 
-  # cp                           r_b   t_a
-  # ./smashpp-inv-rep            r_a   t_b
-  # ./smashpp-inv-rep            r_d   t_c
-  # cp                           r_c   t_d
-  # cat t_a t_b t_c t_d > TarS
+  ./goose-mutatedna -mr 0.01 < r_b > t_a
+  ./smashpp-inv-rep            r_a   t_b
+  ./smashpp-inv-rep            r_d   t_c
+  cp                           r_c   t_d
+  cat t_a t_b t_c t_d > TarS
 
-  # ### Medium sizes: ref:100.000, tar:100.000
-  # ./goose-fastqsimulation \
-  #   -eh -eo -es -edb -rm 0 -f 0.25,0.25,0.25,0.25,0.0 -ls 100 -n 250 -s 191  r_a
-  # ./goose-fastqsimulation \
-  #   -eh -eo -es -edb -rm 0 -f 0.30,0.20,0.20,0.30,0.0 -ls 100 -n 250 -s 608  r_b
-  # ./goose-fastqsimulation \
-  #   -eh -eo -es -edb -rm 0 -f 0.20,0.30,0.30,0.20,0.0 -ls 100 -n 250 -s 30   r_c
-  # ./goose-fastqsimulation \
-  #   -eh -eo -es -edb -rm 0 -f 0.25,0.25,0.25,0.25,0.0 -ls 100 -n 250 -s 138  r_d
-  # cat r_a r_b r_c r_d > RefM
+  ### Medium sizes: ref:100.000, tar:100.000
+  ./goose-fastqsimulation \
+    -eh -eo -es -edb -rm 0 -f 0.25,0.25,0.25,0.25,0.0 -ls 100 -n 250 -s 191  r_a
+  ./goose-fastqsimulation \
+    -eh -eo -es -edb -rm 0 -f 0.30,0.20,0.20,0.30,0.0 -ls 100 -n 250 -s 608  r_b
+  ./goose-fastqsimulation \
+    -eh -eo -es -edb -rm 0 -f 0.20,0.30,0.30,0.20,0.0 -ls 100 -n 250 -s 30   r_c
+  ./goose-fastqsimulation \
+    -eh -eo -es -edb -rm 0 -f 0.25,0.25,0.25,0.25,0.0 -ls 100 -n 250 -s 138  r_d
+  cat r_a r_b r_c r_d > RefM
 
-  # ./smashpp-inv-rep            r_b   t_a
-  # cp                           r_c   t_b
-  # ./goose-mutatedna -mr 0.01 < r_d > t_c
-  # cp                           r_a   t_d
-  # cat t_a t_b t_c t_d > TarM
+  ./smashpp-inv-rep            r_b   t_a
+  cp                           r_c   t_b
+  ./goose-mutatedna -mr 0.01 < r_d > t_c
+  cp                           r_a   t_d
+  cat t_a t_b t_c t_d > TarM
 
-  # ### Large sizes: ref:5.000.000, tar:5.000.000
-  # ./goose-fastqsimulation -eh \
-  #   -eo -es -edb -rm 0 -f 0.30,0.20,0.30,0.20,0.0 -ls 100 -n 12500 -s 10101  r_a
-  # ./goose-fastqsimulation -eh \
-  #   -eo -es -edb -rm 0 -f 0.30,0.20,0.20,0.30,0.0 -ls 100 -n 12500 -s 6018   r_b
-  # ./goose-fastqsimulation -eh \
-  #   -eo -es -edb -rm 0 -f 0.25,0.25,0.25,0.25,0.0 -ls 100 -n 12500 -s 10     r_c
-  # ./goose-fastqsimulation -eh \
-  #   -eo -es -edb -rm 0 -f 0.25,0.25,0.25,0.25,0.0 -ls 100 -n 12500 -s 7      r_d
-  # cat r_a r_b r_c r_d > RefL
+  ### Large sizes: ref:5.000.000, tar:5.000.000
+  ./goose-fastqsimulation -eh \
+    -eo -es -edb -rm 0 -f 0.30,0.20,0.30,0.20,0.0 -ls 100 -n 12500 -s 10101  r_a
+  ./goose-fastqsimulation -eh \
+    -eo -es -edb -rm 0 -f 0.30,0.20,0.20,0.30,0.0 -ls 100 -n 12500 -s 6018   r_b
+  ./goose-fastqsimulation -eh \
+    -eo -es -edb -rm 0 -f 0.25,0.25,0.25,0.25,0.0 -ls 100 -n 12500 -s 10     r_c
+  ./goose-fastqsimulation -eh \
+    -eo -es -edb -rm 0 -f 0.25,0.25,0.25,0.25,0.0 -ls 100 -n 12500 -s 7      r_d
+  cat r_a r_b r_c r_d > RefL
   
-  # cp                           r_c   t_a
-  # ./goose-mutatedna -mr 0.01 < r_b > t_b
-  # cp                           r_a   t_c
-  # ./smashpp-inv-rep            r_d   t_d
-  # cat t_a t_b t_c t_d > TarL
+  cp                           r_c   t_a
+  ./goose-mutatedna -mr 0.01 < r_b > t_b
+  cp                           r_a   t_c
+  ./smashpp-inv-rep            r_d   t_d
+  cat t_a t_b t_c t_d > TarL
 
   ### Extra Large sizes: ref:100.000.000, tar:100.000.000
   ./goose-fastqsimulation -eh \
@@ -131,19 +131,18 @@ fi
 ### Get real dataset
 if [[ $RUN -eq 1 ]]; then
   ### S
-  ./smashpp -r RefS -t TarS \
-    -rm 4,0,0.4,0.95:6,0,0.2,0.95:8,0,0.1,0.95:10,0,0.05,0.95 -w 31
-  ./smashpp -viz -l 5 -m 200 -p 1 -o S.svg RefS-TarS.pos
+  ./smashpp -r RefS -t TarS -l 3 -w 45
+  ./smashpp -viz -l 5 -p 1 -o S.svg RefS-TarS.pos
   ### M
-  ./smashpp -r RefM -t TarM -l 4 -w 100
+  ./smashpp -r RefM -t TarM -l 3 -w 100
   ./smashpp -viz -l 5 -p 1 -b 2 -t 80 -o M.svg RefM-TarM.pos
   ### L
-  ./smashpp -r RefL -t TarL -l 4 -w 130
+  ./smashpp -r RefL -t TarL -l 3 -w 135
   ./smashpp -viz -l 5 -p 1 -t 100 -o L.svg RefL-TarL.pos
   ### XL
-  ./smashpp -r RefXL -t TarXL -rm 14,0,0.005,0.95/1,0,0.5,0,95 -w 750 -th 1.9
+  ./smashpp -r RefXL -t TarXL -l 3 -w 275
   ./smashpp -viz -l 5 -p 1 -m 20000000 -o XL.svg RefXL-TarXL.pos
   ### Mut
-  ./smashpp -r RefMut -t TarMut -rm 20,0,0.002,0.95/5,0,0.6,0.95 -w 100 -th 1.7
-  ./smashpp -viz -l 5 -p 1 -m 600000 -o Mut.svg RefMut-TarMut.pos
+  ./smashpp -r RefMut -t TarMut-m 700000 -th 1.5 -l 4 -w 650
+  ./smashpp -viz -l 5 -p 1 -b 2 -t 50 -o Mut.svg RefMut-TarMut.pos
 fi
