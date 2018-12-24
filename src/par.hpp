74102536dd409830e637c967f8649dd74cd96898
Morteza Hosseini <s_morteza_h@yahoo.com>
2018-12-24 19:49:55 +0000

    change
    Former-commit-id: fd276d8c0ebdd71200e6f0bd18fdea171a2a9f9e [formerly f87a598c58807597b4287a0b31f1bf997a6d0f81] [formerly 3b98cd99eebb41bd7f3cd6d11e3a066e4af04a28] [formerly 2a399c2a7207b3877f87707e7f957b760c5345f4 [formerly 1b2e8efed3ebc494b65d51790fec047c68c4faaf]] [formerly bf8e300afa23822da7dd6e9c2ee6a39858e9c03f [formerly 6479de5372fa57d485c65d42d8126d3c5a98359d]]
    Former-commit-id: 7aeb05fff841e39234654ef0088e9e5730350deb
    Former-commit-id: 61e8d9b158f631e4088dc92841de7ecbb27a46f6
    Former-commit-id: 99fc2577ec739c230d47bf1f60333059ff54a0b8
    Former-commit-id: 6ba17ae631c28547c2227e95eca660d10716ebeb [formerly 4d9042e339c51031e756cb0cd4a9612ba2c5b5b2]
    Former-commit-id: a791db59de77e7d06c8e29f40f2f5b037a9eb1b0
    Former-commit-id: cb877dfa41ea2da2544cff56da0281cfd7891d81

--- src/par.hpp
+++ src/par.hpp
@@ -436,13 +436,25 @@ inline string Param::print_filter_scale () const {
   }
 }
 
+//todo
+// inline void get_arg (vector<string>::const_iterator iter, const string& shortName, const string& longName) {
+//   const auto curr = *iter;
+//   const auto next = *(iter + 1);
+//   if (next[0] == '-') {
+//     if (curr[1]=='-')  return iter->substr(longName.size()+2);
+//     else                return iter->substr(shortName.size()+1);
+//   } 
+//   else {
+//     return *(iter+1);
+//   }
+// }
+
 inline void VizParam::parse (int argc, char**& argv) {
   if (argc < 3) { help();  throw EXIT_SUCCESS; }
 
   vector<string> vArgs;    vArgs.reserve(static_cast<u64>(argc));
   for (int i=0; i!=argc; ++i)
     vArgs.emplace_back(static_cast<string>(argv[i]));
-
 
   for (auto i=vArgs.begin(); i!=vArgs.end(); ++i) {
     if ((*i=="-o" || *i=="--out") && i+1!=vArgs.end())
