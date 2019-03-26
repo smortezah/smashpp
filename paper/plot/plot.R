d490e8f696124917c3f19ab859fe1c817d4bdac3
mori <s_morteza_h@yahoo.com>
2019-03-26 00:24:24 +0000

    change
    Former-commit-id: 824b86ceccf5da668aedd718f62813e4d8770e74 [formerly 9e80ea7398f08c005f4803b0ee73f6876993c72e] [formerly 2be01fd57b70b86f612fae13ae127238341959de] [formerly 191a3f26f81a337eb6d6e82b5435c0ae8a493289 [formerly d8d88348751b529050827ce0cb41123486e96f99]] [formerly e7238faf4a3d39267f1ee4a61114ec253666db64 [formerly 13740946a508240a219c46c1883b32870db8f7ff]]
    Former-commit-id: 4e67221c1941fed0d36586354db0eb567201564b
    Former-commit-id: 24bd08942e5c122e16cba16975c48aa29fa225a0
    Former-commit-id: df301e11fc9ffeccc6910f4bd10f0a3b1dcdaf02
    Former-commit-id: 4f5648aa4855b6b0cb28143faf42ac13b4fb95f3
    Former-commit-id: ea3d48fe9ef44f9eef426e1eba66dbfd8bffdfb6 [formerly dec4a3523feabd964a92e5c66a1d5996abde880e]
    Former-commit-id: cd671b137e572044b0e35552ef17de24ab3dde34
    Former-commit-id: f1a14bdb70bb674c1c2ef37f8632c37f6f174f5b

--- paper/plot/plot.R
+++ paper/plot/plot.R
@@ -1,16 +1,7 @@
 library(ggplot2)
 
-hann <- function(n) {
-  if (!(length(n) == 1 && (n == round(n)) && (n > 0)))
-    stop("hanning: n has to be an integer > 0")
-  
-  if (n == 1)
-    c = 1
-  else {
-    n = n - 1
-    c = 0.5 - 0.5 * cos(2 * pi * (0:n) / n)
-  }
-  c
-}
+N<-50
+hann <- function(n) {0.5 - 0.5 * cos(2 * pi * n / N)}
 
-hann(5)
+ggplot(NULL, aes(c(0,N))) + 
+  geom_area(stat = "function", fun = hann, fill = "red")
