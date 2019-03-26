f7688144f968609f590dfa7116f7829835d5967f
Morteza Hosseini <s_morteza_h@yahoo.com>
2019-03-26 17:57:43 +0000

    change

--- paper/plot/plot.R
+++ paper/plot/plot.R
@@ -8,12 +8,10 @@ blackman <- function(n) {0.42 - 0.5 * cos(2 * pi * n / N) +
     0.08 * cos(4 * pi * n / N)}
 nuttall <- function(n) {0.36 - 0.49 * cos(2 * pi * n / N) + 
     0.14 * cos(4 * pi * n / N) - 0.01 * cos(6 * pi * n / N)}
-
-
-# % void triangular();  // Bartlett window
-# % void welch();
-# % void sine();
-# % void nuttall();
+welch <- function(n) {1 - ((n - N/2)/(N/2))^2}
+sine <- function(n) {sin(pi * n / N)}
+triangular <- function(n) {1 - abs((n - N/2)/(N/2))}
+rectangular <- function(n) {1}
 
 
 x <- seq(0, N)
@@ -23,13 +21,19 @@ df <-
     hann = hann(x),
     hamming = hamming(x),
     blackman = blackman(x),
-    nuttall = nuttall(x)
+    nuttall = nuttall(x),
+    welch = welch(x),
+    sine = sine(x),
+    triangular = triangular(x),
+    rectangular = rectangular(x)
   )
 df <- melt(df, id.vars = "Samples")
-df$name <-
-  as.factor(rep(c("Hann", "Hamming", "Blackman", "Nuttall"), each = length(x)))
+df$name <- as.factor(rep(c("Hann", "Hamming", "Blackman", "Nuttall", "Welch", 
+                           "Sine", "Triangular", "Rectangular"), each = length(x)))
 
 ggplot(df, aes(x = Samples, y = value)) +
-  geom_area(aes(fill = variable)) +
-  facet_wrap( ~ name) +
-  theme_bw()
+  geom_area(aes(color=variable, fill = variable)) +
+  facet_wrap( ~ name, nrow = 2) +
+  ylab("Amplitude") +
+  theme_bw() + 
+  theme(legend.position = "none")
