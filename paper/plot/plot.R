acaf725efb298094e4199b0530af3b705245c573
Morteza Hosseini <s_morteza_h@yahoo.com>
2019-03-26 11:44:04 +0000

    change
    Former-commit-id: f4b01019f550991a4f953f8f8f6e249312e5233c [formerly f130383dfa5a7a0734a938512a1c9722cf428dd1] [formerly 7f68734d39e2e6eeedd49a120ff5e2f1ddbde43d] [formerly 32cae0c58ccf70bc2cfd4605c134cbf6b1765713 [formerly 8ccbf68636ab52c5f62dbd0c68319829395abb2f]] [formerly a66cc91c9034a507b4f297df90b2142603fad9d2 [formerly 92534511d5a856bbea2b7c7f4196581b903028e9]]
    Former-commit-id: b6a0059ed4d9109128b4fa104ab0fce311be92ee
    Former-commit-id: e35d2ef2e03484745435b8a31b9bb5bf3c55e185
    Former-commit-id: ed1b8559fc4ebd1066de3654f38d35ce400988dd
    Former-commit-id: 83b1a4dbbbffb3363bf7f3059f5aee8cd238c7a6
    Former-commit-id: 7d8e9c4784a36010326b18f5fc105a3c701f29e7 [formerly 08013e1ab699a7381835e6a594fea3a3a31fee83]
    Former-commit-id: e44a90cfafb143cb3da81594b21d85fb010f1677
    Former-commit-id: 059a5df4124a090c244063e4ff5519f2baf0a5e7

--- paper/plot/plot.R
+++ paper/plot/plot.R
@@ -1,7 +1,35 @@
 library(ggplot2)
+library(reshape2)
 
-N<-50
+N <- 100
 hann <- function(n) {0.5 - 0.5 * cos(2 * pi * n / N)}
+hamming <- function(n) {0.54 - 0.46 * cos(2 * pi * n / N)}
+blackman <- function(n) {0.42 - 0.5 * cos(2 * pi * n / N) + 
+    0.08 * cos(4 * pi * n / N)}
+nuttall <- function(n) {0.36 - 0.49 * cos(2 * pi * n / N) + 
+    0.14 * cos(4 * pi * n / N) - 0.01 * cos(6 * pi * n / N)}
 
-ggplot(NULL, aes(c(0,N))) + 
-  geom_area(stat = "function", fun = hann, fill = "red")
+
+# % void triangular();  // Bartlett window
+# % void welch();
+# % void sine();
+# % void nuttall();
+
+
+x <- seq(0, N)
+df <-
+  data.frame(
+    Samples = x,
+    hann = hann(x),
+    hamming = hamming(x),
+    blackman = blackman(x),
+    nuttall = nuttall(x)
+  )
+df <- melt(df, id.vars = "Samples")
+df$name <-
+  as.factor(rep(c("Hann", "Hamming", "Blackman", "Nuttall"), each = length(x)))
+
+ggplot(df, aes(x = Samples, y = value)) +
+  geom_area(aes(fill = variable)) +
+  facet_wrap( ~ name) +
+  theme_bw()
