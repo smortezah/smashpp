db6f18a34f384cde29feeb491c8266f67f8cea26
mori <s_morteza_h@yahoo.com>
2019-04-06 21:14:30 +0100

    update

--- src/segment.cpp
+++ src/segment.cpp
@@ -8,15 +8,22 @@ using namespace smashpp;
 void Segment::partition(std::ofstream& posF, float filtered) {
   if (filtered > thresh) {
     begun = false;
+
     if (begPos != endPos) {
       if (endPos - begPos >= minSize) {
         ++nSegs;
-        posF << (begPos < beg_guard ? 0 : begPos - beg_guard) << '\t'
+
+        if (static_cast<int64_t>(begPos - beg_guard) < 0)
+          posF << 0;
+        else
+          posF << begPos - beg_guard;
+        posF << '\t'
              << (endPos + end_guard > totalSize ? totalSize
                                                 : endPos + end_guard)
              << '\t' << fixed_precision(PREC_FIL) << sumEnt / numEnt << '\n';
       }
     }
+    
     begPos = 0, endPos = 0, sumEnt = 0, numEnt = 0;
   } else {
     if (!begun) {
@@ -33,7 +40,12 @@ void Segment::partition_last(std::ofstream& posF) {
   if (begPos != endPos) {
     if (endPos - begPos >= minSize) {
       ++nSegs;
-      posF << (begPos < beg_guard ? 0 : begPos - beg_guard) << '\t'
+
+      if (static_cast<int64_t>(begPos - beg_guard) < 0)
+        posF << 0;
+      else
+        posF << begPos - beg_guard;
+      posF << '\t'
            << (endPos + end_guard > totalSize ? totalSize : endPos + end_guard)
            << '\t' << fixed_precision(PREC_FIL) << sumEnt / numEnt << '\n';
     }
@@ -48,4 +60,4 @@ void Segment::set_guards(uint8_t maxCtx, int16_t ref_beg_guard,
   // todo
   beg_guard = static_cast<uint64_t>(maxCtx + ref_beg_guard);
   end_guard = static_cast<uint64_t>(ref_end_guard);
-}
+  }
