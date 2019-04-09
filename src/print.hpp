0486f2e1e42cd68e790201051bbad7d94da52462
Morteza Hosseini <s_morteza_h@yahoo.com>
2019-04-09 15:29:30 +0100

    update
    Former-commit-id: 99a7558718b5a99033fe2affc485c57dcadf8a18 [formerly dba9b296c4206040a2e4b63bc0a7249c11cc015a] [formerly c4d7b95abe23a52aaf5c457506cad80e46663c8f] [formerly b4de5da13f5796ec078a8b437cc0eff208178d7f [formerly 3d32687666c4e5759f77ec413fead4c16b4eaa04]] [formerly 2564c3e507140e4c4b0fd57733e9db0aa8e2cb22 [formerly cffb4b2d8296fc28f8620d7e30d642b6292b5af9]]
    Former-commit-id: 3a37997dfaf0469d56492502ddd8f9943a8669c3
    Former-commit-id: 58fe1814d673b9410cc12b5d6dd6a40e8a86e0dd
    Former-commit-id: 363db651d5b6a699af2b429bb378a771d6f50d7b
    Former-commit-id: aeb472dcde5a22b794e9d15ba65d79a6ad3394bc
    Former-commit-id: e88a076fdd80474b3370a045466b9502cd3d7078 [formerly 2b33dbaf3bbb796e8d5f0946b6c383d1d08da556]
    Former-commit-id: a45f3188d987ecb071969ba5846dc18b12f719d5
    Former-commit-id: e2969435265e759e8df774a66d7c62137224cb7b

--- src/print.hpp
+++ src/print.hpp
@@ -10,24 +10,52 @@
 #include "string.hpp"
 
 namespace smashpp {
-void title(std::string str) { std::cerr << bold(str) << '\n'; }
+void print_title(std::string str) { std::cerr << bold(str) << '\n'; }
 
-void line(const std::string& str) { std::cerr << "  " << str << '\n'; }
+void print_line(const std::string& str) { std::cerr << "  " << str << '\n'; }
 
 // Print column 1: left-aligned + column 2: left-aligned
-void line_left_left(const std::string& strL, uint8_t n,
-                    const std::string& strR) {
-  std::cerr << "  " << std::left << std::setw(27 + n * 8) << strL;
-  std::cerr.clear();
-  std::cerr << strR << '\n';
+// void line_left_left(std::string str_left, uint8_t n,
+//                     const std::string& str_mid, std::string str_right) {
+//   std::cerr << "  " << std::left << std::setw(17 + n * 8) << str_left;
+//   std::cerr.clear();
+//   std::cerr << std::left << std::setw(60) << str_mid;
+//   std::cerr << str_right << '\n';
+// }
+
+void print_aligned(std::string arg, std::string type,
+                   std::string delim_description,
+                   const std::string& description, std::string delim_default="",
+                   std::string default_val="") {
+  std::cerr << "  ";
+  std::cerr << std::left << std::setw(12) << bold(arg);
+  std::cerr << std::left << std::setw(15)
+            << (type == "" ? type : "<" + type + ">");
+  std::cerr << std::left << std::setw(2) << delim_description;
+  std::cerr << std::left << std::setw(38) << description;
+  std::cerr << std::left << std::setw(3) << delim_default;
+  std::cerr << default_val << '\n';
+}
+
+void print_aligned_right_left(std::string arg, std::string type,
+                   std::string delim_description,
+                   const std::string& description, std::string delim_default="",
+                   std::string default_val="") {
+  std::cerr << "  ";
+  std::cerr << std::left << std::setw(12) << bold(arg);
+  std::cerr << std::right << std::setw(15) << type;
+  std::cerr << std::left << std::setw(2) << delim_description;
+  std::cerr << std::left << std::setw(38) << description;
+  std::cerr << std::left << std::setw(3) << delim_default;
+  std::cerr << default_val << '\n';
 }
 
 // Print column 1: right-aligned + column 2: left-aligned
-void line_right_left(const std::string& strL, uint8_t n,
-                     const std::string& strR) {
-  std::cerr << "  " << std::right << std::setw(27 + n * 8) << strL;
+void line_right_left(std::string str_left, uint8_t n,
+                     const std::string& str_right) {
+  std::cerr << "  " << std::right << std::setw(17 + n * 8) << str_left;
   std::cerr.clear();
-  std::cerr << strR << '\n';
+  std::cerr << str_right << '\n';
 }
 }  // namespace smashpp
 
