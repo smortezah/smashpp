654ec0e2f2c87a1d6056db5cb3b4caba8ea714e0
Morteza Hosseini <s_morteza_h@yahoo.com>
2019-04-15 11:50:35 +0100

    update
    Former-commit-id: f260dc6536198017a4b9caeb8326c0a839af823a [formerly 3766e142d12b03b7f3d9ed652f116fe48d9293ae] [formerly f87d46696ca9dd80cb8477119cbe68406e800522] [formerly 2e31d947f84bcd5f2245b52cbe77231a23a2885f [formerly bfb14d59b28247f681e7e901f278f7cef85308d2]] [formerly 2b58885546a339a914ea1290fc43be8540790e51 [formerly 8dd6d463ea35a96ec9a9bb02be1126a8c5fb55ec]]
    Former-commit-id: 4e128cdc857cf8808148ff8cd24ac96d538541dd
    Former-commit-id: f621c0949cfce8c254791c76fb6fadfdfde0ecfa
    Former-commit-id: f3c782a9bfb30896e40d14bd65d9a61fb6b4528e
    Former-commit-id: d64a2ebf7e7c21c7af9ad425c4a2d0040f9f6701
    Former-commit-id: 9af3f4968c4aa8f388743e14eb324760e457be88 [formerly 158128068a0c642ae48d6be588d795cfead817f5]
    Former-commit-id: b3432db9432522420bd0fe85f0b553b842908f91
    Former-commit-id: cdae0c466cb0388c1009713c689590f4e809868e

--- src/main.cpp
+++ src/main.cpp
@@ -156,48 +156,71 @@ void make_pos_pair(const std::vector<PosRow>& left,
 void make_pos_pair(const std::vector<PosRow>& left,
                    const std::vector<PosRow>& right1,
                    const std::vector<PosRow>& right3) {
-  struct Out {
+  struct OutRow {
     PosRow pos2;
     PosRow pos1;
     std::vector<PosRow> pos3;
+    OutRow() = default;
+    OutRow(PosRow pos2_, PosRow pos1_) : pos2(pos2_), pos1(pos1_) {}
   };
-  std::vector<Out> out{};
-  out.resize(1);
+  std::vector<OutRow> out{std::vector<OutRow>()};
 
-  uint64_t line = 0;
-  for (auto right1_iter = std::begin(right1), right1_begin = right1_iter,
-            left_iter = std::begin(left);
+  for (auto left_iter = std::begin(left), right1_iter = std::begin(right1),
+            right1_begin = right1_iter;
        left_iter != std::end(left); ++left_iter) {
-    // left_iter->print();
-    // left_iter->show();
-    // Out tmp_out;
-    // tmp_out.pos2=*left_iter;
-    // out.push_back(tmp_out);
-// out[line].pos2=*left_iter;
-    std::cerr << '\t';
-
     for (; right1_iter != std::end(right1); ++right1_iter) {
       auto seg_name{gen_name(right1_iter->run_num, right1_iter->ref,
                              right1_iter->tar, Format::segment) +
                     std::to_string(std::distance(right1_begin, right1_iter))};
       if (left_iter->ref == seg_name) {
-        // right_first_iter->print();
-        // right1_iter->show();
-        // out[line].pos1 = *right1_iter;
+        out.push_back(OutRow(*left_iter, *right1_iter));
         break;
       }
     }
-    std::cerr << '\n';
+  }
 
-    ++line;
+  for (auto right3_iter = std::begin(right3), left_iter = std::begin(left),
+            left_begin = left_iter;
+       right3_iter != std::end(right3); ++right3_iter) {
+    for (auto line{static_cast<uint64_t>(std::distance(left_begin, left_iter))};
+         left_iter != std::end(left); ++left_iter) {
+      auto seg_name{gen_name(left_iter->run_num, left_iter->ref, left_iter->tar,
+                             Format::segment) +
+                    std::to_string(line)};
+      if (right3_iter->ref == seg_name) {
+        out[line].pos3.push_back(*right3_iter);
+        ++line;
+        break;
+      }
+    }
   }
 
   // todo
-  // for (auto e : out) {
-  //   e.pos2.show();
-  //   e.pos1.show();
-  //   for (auto ee : e.pos3) ee.show();
-  // }
+  for (std::size_t i = 0; i != out.size(); ++i) {
+    out[i].pos2.show();
+    std::cerr << "->";
+    out[i].pos1.show();
+    std::cerr << "->";
+
+    for (std::size_t j = 0; j != out[i].pos3.size(); ++j) {
+      out[i].pos3[j].show();
+      std::cerr << " + ";
+    }
+    std::cerr << '\n';
+  }
+
+  //   for (auto e : out) {
+  //     e.pos2.show();
+  //     std::cerr << "->";
+  //     e.pos1.show();
+  //     std::cerr << "->";
+  //     for (auto ee : e.pos3) {
+  //       ee.show();
+  //       std::cerr << '\t';
+  //     }
+  //     std::cerr << '\n';
+  //   }
+  // std::cerr << "\n+++++++++++++++++\n";
 }
 
 void write_pos_file(const std::vector<PosRow>& pos_out) {
