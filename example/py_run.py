584687d84a6d1170caafb484cc85cd42acc3397e
Morteza Hosseini <s_morteza_h@yahoo.com>
2019-04-20 18:49:11 +0100

    update

--- example/py_run.py
+++ example/py_run.py
@@ -15,7 +15,7 @@ for idx_ref in range(1, num_files+1):
         ref = str(idx_ref)
         tar = str(idx_tar)
         cmd = './smashpp -w 200 -rm 11,0,1,0.95/8,0,1,0.9 -r ' + ref + \
-            ' -t ' + tar + ' -th 1.8 -v -dp';# + ' -rb 20 -re 10;'
+            ' -t ' + tar + ' -th 1.8 -v -dp -m 60'  # + ' -rb 20 -re 10;'
         os.popen(cmd).read()
 
 
