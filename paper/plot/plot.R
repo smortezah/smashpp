c83fedff422ae1609075954096b417a796a353c3
Morteza Hosseini <s_morteza_h@yahoo.com>
2019-04-27 18:52:27 +0100

    update
    Former-commit-id: 0ba97d0a5baf03876cb45973985f76a9acb48a22 [formerly 802246062e9c672547d387219016dbaa9b2d45cd] [formerly a7393b46f1a8388814f6fd3ced9d02866a4da615] [formerly 38e5b58756a755f63cede8610c9fa21adbd01fe6 [formerly c0029b2c992b61df36266709167e77099c250f65]] [formerly 9b2974ca6bea5f01b5e91ac97de2bb2d5320b7e4 [formerly fcddf0a82a8791519c1e6bd502c15dc1fe46c7dc]]
    Former-commit-id: b875f91bdd734e5fcd32d266334d858c5b8fa7a7
    Former-commit-id: cfdb438547696a5e1d41dcb3043ec292616905ba
    Former-commit-id: 611db09ddad32d0c0ae948789e1cba10a638ba0a
    Former-commit-id: d3eaedae1d54a0f978447f304e73d01099fd190b
    Former-commit-id: d38b0ff90211c15a2919646784a4726ac0cb8ad3 [formerly f82c6ab01e8b574ddd2bee842ea167d5e27ed0de]
    Former-commit-id: 7ba185bd24f198fd80d9c5fe2cda05c5058c4e49
    Former-commit-id: 5c78a243630d927bd7553f68d2c69222d6c8d7fe

--- paper/plot/plot.R
+++ paper/plot/plot.R
@@ -1,39 +1,122 @@
 library(ggplot2)
 library(reshape2)
 
-N <- 100
-hann <- function(n) {0.5 - 0.5 * cos(2 * pi * n / N)}
-hamming <- function(n) {0.54 - 0.46 * cos(2 * pi * n / N)}
-blackman <- function(n) {0.42 - 0.5 * cos(2 * pi * n / N) + 
-    0.08 * cos(4 * pi * n / N)}
-nuttall <- function(n) {0.36 - 0.49 * cos(2 * pi * n / N) + 
-    0.14 * cos(4 * pi * n / N) - 0.01 * cos(6 * pi * n / N)}
-welch <- function(n) {1 - ((n - N/2)/(N/2))^2}
-sine <- function(n) {sin(pi * n / N)}
-triangular <- function(n) {1 - abs((n - N/2)/(N/2))}
-rectangular <- function(n) {1}
+filters <- 0
+filters_scatter <- 1
 
-
-x <- seq(0, N)
-df <-
-  data.frame(
-    Samples = x,
-    hann = hann(x),
-    hamming = hamming(x),
-    blackman = blackman(x),
-    nuttall = nuttall(x),
-    welch = welch(x),
-    sine = sine(x),
-    triangular = triangular(x),
-    rectangular = rectangular(x)
-  )
-df <- melt(df, id.vars = "Samples")
-df$name <- as.factor(rep(c("Hann", "Hamming", "Blackman", "Nuttall", "Welch", 
-                           "Sine", "Triangular", "Rectangular"), each = length(x)))
-
-ggplot(df, aes(x = Samples, y = value)) +
-  geom_area(aes(color=variable, fill = variable)) +
-  facet_wrap( ~ name, nrow = 2) +
-  ylab("Amplitude") +
-  theme_bw() + 
-  theme(legend.position = "none")
+if (filters == 1) {
+  N <- 100
+  hann <- function(n) {
+    0.5 - 0.5 * cos(2 * pi * n / N)
+  }
+  hamming <- function(n) {
+    0.54 - 0.46 * cos(2 * pi * n / N)
+  }
+  blackman <- function(n) {
+    0.42 - 0.5 * cos(2 * pi * n / N) +
+      0.08 * cos(4 * pi * n / N)
+  }
+  nuttall <- function(n) {
+    0.36 - 0.49 * cos(2 * pi * n / N) +
+      0.14 * cos(4 * pi * n / N) - 0.01 * cos(6 * pi * n / N)
+  }
+  welch <- function(n) {
+    1 - ((n - N / 2) / (N / 2)) ^ 2
+  }
+  sine <- function(n) {
+    sin(pi * n / N)
+  }
+  triangular <- function(n) {
+    1 - abs((n - N / 2) / (N / 2))
+  }
+  rectangular <- function(n) {
+    1
+  }
+  
+  
+  x <- seq(0, N)
+  df <-
+    data.frame(
+      Samples = x,
+      hann = hann(x),
+      hamming = hamming(x),
+      blackman = blackman(x),
+      nuttall = nuttall(x),
+      welch = welch(x),
+      sine = sine(x),
+      triangular = triangular(x),
+      rectangular = rectangular(x)
+    )
+  df <- melt(df, id.vars = "Samples")
+  df$name <-
+    as.factor(rep(
+      c(
+        "Hann",
+        "Hamming",
+        "Blackman",
+        "Nuttall",
+        "Welch",
+        "Sine",
+        "Triangular",
+        "Rectangular"
+      ),
+      each = length(x)
+    ))
+  
+  ggplot(df, aes(x = Samples, y = value)) +
+    geom_area(aes(color = variable, fill = variable)) +
+    facet_wrap( ~ name, nrow = 2) +
+    ylab("Amplitude") +
+    theme_bw() +
+    theme(legend.position = "none")
+} else if (filters_scatter == 1) {
+  index<-1:999
+  blackman <- read.table("blackman.fil")
+  hamming <- read.table("hamming.fil")
+  hann <- read.table("hann.fil")
+  nuttall <- read.table("nuttall.fil")
+  rectangular <- read.table("rectangular.fil")
+  sine <- read.table("sine.fil")
+  triangular <- read.table("triangular.fil")
+  welch <- read.table("welch.fil")
+  
+  df <-
+    data.frame(
+      Samples = index,
+      Blackman = blackman$V1,
+      Hamming = hamming$V1,
+      Hann = hann$V1,
+      Nuttall = nuttall$V1,
+      Rectangular = rectangular$V1,
+      Sine = sine$V1,
+      Triangular = triangular$V1,
+      Welch = welch$V1
+    )
+  df <- melt(df, id.vars = "Samples")
+  df$name <-
+    as.factor(rep(
+      c(
+        "Blackman",
+        "Hamming",
+        "Hann",
+        "Nuttall",
+        "Rectangular",
+        "Sine",
+        "Triangular",
+        "Welch"
+      ),
+      each = length(index)
+    ))
+  
+  ggplot(df, aes(x = Samples, y = value)) +
+    geom_line(aes(color = variable)) +
+    facet_wrap( ~ name, nrow = 4) +
+    ylab("Value") +
+    theme_bw() +
+    theme(legend.position = "none")
+  
+  
+  
+  # ggplot(data=NULL,aes(x=index, y=blackman$V1) ) +
+  #   geom_line()
+}
