ec3ec26d3c81de91352cb1a35593e67349fdbb59
Morteza Hosseini <s_morteza_h@yahoo.com>
2019-04-29 15:10:34 +0100

    update
    Former-commit-id: 8bd1d6c3956681ee8ee656cd9ab119b5b17c3d7f [formerly 1a6a4c22561821f391ef2c8624ff8f27069f4160] [formerly 14e148af072de7a1dfcf5c3433b33e8ecd0b2477] [formerly 087aae949bf540c87661fcee9f6bfc842b4473f3 [formerly 78e64ddb53aa322c1c1f8bdce1faf180f0068101]] [formerly 7fe449e0e6b28c1f079936298aff07f034cc3be5 [formerly e7bfab48e69e59cd0b6792cc0757dd9312ecea05]]
    Former-commit-id: b5bad7f2ea3b79a8d2d12e29e6f760e5f0fce2e5
    Former-commit-id: 5499db0f637ddd80b121b6f71a35125e00759cd4
    Former-commit-id: 73b3c0a9ad5b3bdfa8f322a460fc46969bf51786
    Former-commit-id: e0f49e9e735ad0ec88c81c123df2edf393a236d7
    Former-commit-id: e2eaf32d142669e40eaf70b163c8600f673ff09e [formerly 994ca0f2c83ca3b2fce73c53f1d5a5963bb11232]
    Former-commit-id: 82c84b5a08bcbd3eb7b5b8b7fb42d3e9636290d5
    Former-commit-id: 245391713efe16ceb3bbd2e00582165a42e34326

--- paper/plot/plot.R
+++ paper/plot/plot.R
@@ -70,7 +70,7 @@ if (filters == 1) {
     theme_bw() +
     theme(legend.position = "none")
 } else if (filters_scatter == 1) {
-  index<-1:999
+  index<-1:1000
   blackman <- read.table("blackman.fil")
   hamming <- read.table("hamming.fil")
   hann <- read.table("hann.fil")
@@ -79,42 +79,61 @@ if (filters == 1) {
   sine <- read.table("sine.fil")
   triangular <- read.table("triangular.fil")
   welch <- read.table("welch.fil")
-  main <- read.table("0.prf")
+  main <- read.table("main.prf")
 
   df <-
     data.frame(
       Samples = index,
-      Blackman = blackman$V1,
+      Main = main$V1,
+      Rectangular = rectangular$V1,
       Hamming = hamming$V1,
       Hann = hann$V1,
-      Nuttall = nuttall$V1,
-      Rectangular = rectangular$V1,
-      Sine = sine$V1,
+      Blackman = blackman$V1,
       Triangular = triangular$V1,
       Welch = welch$V1,
-      Main = main$V1[1:999]
+      Sine = sine$V1,
+      Nuttall = nuttall$V1
     )
   df <- melt(df, id.vars = "Samples")
   df$name <-
     as.factor(rep(
       c(
-        "Blackman",
+        "Main",
+        "Rectangular",
         "Hamming",
         "Hann",
-        "Nuttall",
-        "Rectangular",
-        "Sine",
+        "Blackman",
         "Triangular",
         "Welch",
-        "Main"
+        "Sine",
+        "Nuttall"
       ),
       each = length(index)
     ))
-
+  df$name <-
+    factor(
+      df$name,
+      c(
+        "Main",
+        "Rectangular",
+        "Hamming",
+        "Hann",
+        "Blackman",
+        "Triangular",
+        "Welch",
+        "Sine",
+        "Nuttall"
+      )
+    )
+  
   ggplot(df, aes(x = Samples, y = value)) +
     geom_line(aes(color = variable)) +
-    facet_wrap( ~ name, nrow = 4) +
-    ylab("Value") +
-    theme_bw() +
+    facet_wrap(~name, ncol = 2, strip.position = "top"
+               # , scales = "free_y"
+               ) +
+    geom_ribbon(data=subset(df, value<=1.5),aes(ymin = value, ymax = 1.5), fill = "gray", alpha = .5) +
+    xlab("Base") +
+    ylab("Entropy") +
+    theme_bw() + 
     theme(legend.position = "none")
 }
