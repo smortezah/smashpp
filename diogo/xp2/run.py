a7bd4ff74259b3e30eee15b778c7a0a25b9c06ad
Morteza Hosseini <s_morteza_h@yahoo.com>
2019-05-14 13:33:06 +0100

    update
    Former-commit-id: 8ab0a13958910e02c7d9c72060650a6391f4a8c4 [formerly 6b600d9fe24b984d2b0c9d703621818506c22bca] [formerly 79c0e0c56f11fcc283b3724103b7560c83b8d0fd] [formerly 156d289b09db001d939037a0bf15ede6f331d07c [formerly 01f9bfd3e04e2048b180cc7d0085c2d68d372dc1]] [formerly 95d1dc29fe5b3bf41a0668ca753d5103a34a6cfc [formerly 2a16c368fb75b6769422917c443ed97d3d8c2e47]]
    Former-commit-id: c3992ed0ac104b752589b9618eae489324497423
    Former-commit-id: cc5252e15208c812353e669ee84d0a73a376a79d
    Former-commit-id: 1d557c36e961e4def9b49f97509ee49299dbc80e
    Former-commit-id: ad48023836e9fcb851d1a722562881c61b44a0b2
    Former-commit-id: 56707488ccfcee8b386fc123d8c08163434c97ea [formerly ef0050d531dfc5bcdda24e0129539f55e005207a]
    Former-commit-id: b94b3fea7f8ad63a51a60f21e3f36949078a75c3
    Former-commit-id: 92a5abfbdd14c96b7ddaa4bc0083af3d71b81b7f

--- diogo/xp2/run.py
+++ diogo/xp2/run.py
@@ -1,32 +1,36 @@
 import os
 import matplotlib.pyplot as plt
+import numpy as np
 
-split_reads = False
-fasta_to_seq = False
-compress = True
-plot_simil_matrix = False
+prepare_data = False
+compress = False
+plot_simil = True
 
-num_files = 3  # 3327
+main_file = 'mtDNA_Chordata_3327_22-03-2019.fasta'
+num_files = 10  # 3327
 nrc_file = 'nrc'
-threshold = 1.9
+threshold = 1.8
+# nrc_mat = [[0 for x in range(num_files)]
+#            for y in range(num_files)]
+simil_mat = [[0 for x in range(num_files)]
+             for y in range(num_files)]
 
-if split_reads:
-    main_file = 'mtDNA_Chordata_3327_22-03-2019.fasta'
+if prepare_data:
+    # Split reads
     cmd = './goose-splitreads < ' + main_file
     os.popen(cmd).read()
 
-if fasta_to_seq:
+    # Convert fasta to seq
     for file in os.listdir("."):
         if file.endswith("fasta"):
-            cmd = './goose-fasta2seq < ' + file + ' > ' + file[3:-6] + ';' \
-                'rm -f ' + file
+            cmd = './goose-fasta2seq < ' + file + ' > ' + file[3:-6]
             os.popen(cmd).read()
+            if os.path.exists(file):
+                os.remove(file)
 
 if compress:
     first = 1
-    nrc_mat = [[0 for x in range(num_files)]
-               for y in range(num_files)]
-    # os.popen("rm -f " + nrc_file).read()
+    nrc_mat = [[0 for x in range(num_files)] for y in range(num_files)]
     if os.path.exists(nrc_file):
         os.remove(nrc_file)
 
@@ -40,47 +44,31 @@ if compress:
                 for line in log_file:
                     line_list = line.split()
                     if len(line_list) > 5:
-                        # print(line_list[5])
                         nrc_mat[i-1][j-1] = line_list[5]
+    np.savetxt(nrc_file, nrc_mat, fmt="%s")
 
-    for x in nrc_mat:
-        print(*x, sep="\t")
+    for file in ["log", "*.co"]:
+        if os.path.exists(file):
+            os.remove(file)
 
-            # cmd = './geco -rm 6:1:0:0/0 -rm 10:10:1:0/0 -rm 14:50:1:3/10 ' + \
-            #     '-c 30 -g 0.95 -v -r ' + str(i) + ' ' + str(j) + ' > log;' + \
-            #     'grep bpb log | cut -d\' \' -f6 >> ' + nrc_file
-
-    # # os.popen("rm -f log *.co").read()
-    # for file in ["log", "*.co"]:
-    #     if os.path.exists(file):
-    #         os.remove(file)
-
-
-def build_simil_matrix():
-    nrc_mat = [[0 for x in range(num_files)]
-               for y in range(num_files)]
-    simil_mat = [[0 for x in range(num_files)]
-                 for y in range(num_files)]
-
-    with open(nrc_file) as file:
+if plot_simil:
+    def build_simil_matrix(nrc_mat):
         for i in range(0, num_files):
-            for j in range(0, num_files):
-                nrc_mat[i][j] = file.readline()[:-1]
+            for j in range(i, num_files):
+                if i != j:
+                    if min(nrc_mat[i][j], nrc_mat[j][i]) < threshold:
+                        simil_mat[i][j] = 1
+                        # simil_mat[j][i] = 1
+        return simil_mat
 
-    for i in range(0, num_files):
-        for j in range(i, num_files):
-            if min(float(nrc_mat[i][j]), float(nrc_mat[j][i])) < threshold:
-                simil_mat[i][j] = 1
-                # simil_mat[j][i] = 1
+    nrc_mat = np.genfromtxt(nrc_file, dtype=float)
+    # for x in nrc_mat:
+    #     print(*x, sep="\t")
 
-    return simil_mat
+    simil_mat = build_simil_matrix(nrc_mat)
+    # for x in simil_mat:
+    #     print(*x, sep=" ")
 
-
-if plot_simil_matrix:
-    #     for x in nrc_mat:
-    #         print(*x, sep="\t")
-    #     for x in simil_mat:
-    #         print(*x, sep=" ")
-    simil_mat = build_simil_matrix()
     plt.matshow(simil_mat)
+    # plt.matshow(nrc_mat)
     plt.show()
