7274f96e30a867bc19a3c07d6c7de3b6d9f03977
Morteza Hosseini <s_morteza_h@yahoo.com>
2019-05-14 10:29:34 +0100

    update

--- diogo/xp2/run.py
+++ diogo/xp2/run.py
@@ -2,7 +2,8 @@ import os
 
 split_reads = False
 fasta_to_seq = False
-run_geco = False
+run_geco = True
+build_simil_matrix = True
 filter_nrc = False
 
 if split_reads:
@@ -18,20 +19,43 @@ if fasta_to_seq:
             os.popen(cmd).read()
 
 if run_geco:
-    num_files = 3327
+    num_files = 4  # 3327
     first = 1
-    os.popen("rm -f nrc").read()
+    out_file = 'nrc'
+    os.popen("rm -f " + out_file).read()
 
-    for i in range(first, num_files-first+1):
-        cmd = 'printf \"' + str(i) + '\t\" >> nrc;' + \
-            './geco -tm 6:1:0:0/0 -tm 10:10:1:0/0 -tm 14:50:1:3/10 -c 20' + \
-            '-g 0.95 -v ' + str(i) + ' > log;' + \
-            'grep bpb log | cut -d\' \' -f6 >> nrc'
-        os.popen(cmd).read()
+    for i in range(first, num_files - first + 1):
+        for j in range(first, num_files - first + 1):
+            cmd = './geco -rm 6:1:0:0/0 -rm 10:10:1:0/0 -rm 14:50:1:3/10 ' + \
+                '-c 30 -g 0.95 -v -r ' + str(i) + ' ' + str(j) + ' > log;' + \
+                'grep bpb log | cut -d\' \' -f6 >> ' + out_file
+            os.popen(cmd).read()
 
     os.popen("rm -f log *.co").read()
     # if os.path.exists("log"):
     #     os.remove("log")
+
+if build_simil_matrix:
+    out_file = 'nrc'
+    threshold = 2.0
+    num_files_prev = 3  # 3327
+    nrc_mat = [[0 for x in range(num_files_prev)]
+               for y in range(num_files_prev)]
+    simil_mat = [[0 for x in range(num_files_prev)]
+                 for y in range(num_files_prev)]
+
+    with open(out_file) as file:
+        for i in range(0, num_files_prev):
+            for j in range(0, num_files_prev):
+                nrc_mat[i][j] = file.readline()[:-1]
+
+    for i in range(0, num_files_prev):
+        for j in range(i, num_files_prev):
+            if min(float(nrc_mat[i][j]), float(nrc_mat[j][i])) < threshold:
+                simil_mat[i][j] = 1
+
+    print(nrc_mat)
+    print(simil_mat)
 
 if filter_nrc:
     threshold = 1.7
