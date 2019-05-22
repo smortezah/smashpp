1a71ebeb72483f71a51ab70200fc71d0cdbed0ae
Morteza Hosseini <s_morteza_h@yahoo.com>
2019-05-22 17:57:50 +0100

    update

--- xp/plot.R
+++ xp/plot.R
@@ -1,10 +1,17 @@
 library(ggplot2)
 library(reshape2)
+library(scales)
 
-data_in <- scan("mit.ent", quiet = TRUE)
-mat <- matrix(data_in, byrow=T, nrow=3327, ncol=3327)
-melted_mat <- melt(mat)
+entropy <- scan("mit.ent", quiet = TRUE)
+ent_mat <- matrix(entropy, byrow=T, nrow=3327, ncol=3327)
+threshold <- 1.2
+ent_mat[ent_mat > threshold] <- threshold
+melted_ent_mat <- melt(ent_mat)
 
-ggplot(melted_mat, aes(x = Var1, y = Var2)) + 
-  geom_raster(aes(fill=value)) +
-  scale_fill_gradient(low="blue", high="red")
+ggplot() +
+  geom_raster(data=melted_ent_mat, aes(x = Var1, y = Var2, fill=value)) +
+  labs(x="Mitochondrion", y="Mitochondrion") +
+  # scale_fill_manual(name = 'fill', values = setNames(c(melted_ent_mat$value,'yellow'),c(T, F))) +
+  scale_fill_gradient(low="yellow", high="dark green") +
+  theme_bw()
+
