a7a1ed7db18be3dadd61d6a2d40a9769219d7e00
Morteza Hosseini <s_morteza_h@yahoo.com>
2019-05-22 19:41:43 +0100

    update
    Former-commit-id: ca6af352271cfb96775a3ff710702e660098f67e [formerly 483c6ac157727c654c88973e6e6136ace1aa448f] [formerly b2103cd6a263eec45388c94359d957a1be2b729c] [formerly 8bd7a186db567ccc849b155e526010bb11a8e898 [formerly 63c2966c98e9199c98930b8ece6db1a290fd3cd0]] [formerly 110d511de36e7df62fbe11772bae634c5d892eb4 [formerly c94dba9c9632958449d9651e0417cba9fdceb7b8]]
    Former-commit-id: ee14d6ecf7f7f438ca2acaed923a48c69eb5b85b
    Former-commit-id: 48e1519199c5ea2ff94ee7b17d05724d91bb0799
    Former-commit-id: a2f59067ab53ae2336b4b9c4dee094b7c4a15e76
    Former-commit-id: eb13d65120753593012741e351cbe97817047457
    Former-commit-id: b93ade98ee210594e553b1d90a2e9142028e3f3f [formerly 70e208efd313777540c7fa8cd9c8f5633af95af6]
    Former-commit-id: add081805ac21b3ac826e68e6ad11da6a9cd0176
    Former-commit-id: 8091114816247001306a74bcc4cab4635b12410f

--- xp/plot.R
+++ xp/plot.R
@@ -1,19 +1,36 @@
 library(ggplot2)
 library(reshape2)
 library(scales)
+library(dplyr)
 
 entropy <- scan("mit.ent", quiet = TRUE)
 ent_mat <- matrix(entropy, byrow=T, nrow=3327, ncol=3327)
-threshold <- 1.2
+threshold <- 1.6
+
+# melted_ent_mat <- melt(ent_mat) %>%
+#   mutate(val_trimmed = case_when(
+#     value <= threshold ~ threshold,
+#     T ~ value
+#   )) 
+# 
+# ggplot(melted_ent_mat, aes(x = Var1, y = Var2, fill = val_trimmed)) +
+#   geom_raster() +
+#   scale_fill_gradient(low="yellow", high="dark green")
+
 ent_mat[ent_mat < threshold] <- threshold
 melted_ent_mat <- melt(ent_mat)
-data_below_thresh <- subset(melted_ent_mat, melted_ent_mat$value<=threshold)
-data_above_thresh <- subset(melted_ent_mat, melted_ent_mat$value>threshold)
+# data_below_thresh <- subset(melted_ent_mat, melted_ent_mat$value<=threshold)
+# data_above_thresh <- subset(melted_ent_mat, melted_ent_mat$value>threshold)
+
+# ggplot() +
+#   geom_raster(data=data_above_thresh, aes(x = Var1, y = Var2, fill=value)) +
+#   geom_raster(data=data_below_thresh, aes(x = Var1, y = Var2), fill='yellow') +
+#   scale_fill_gradient(low="yellow", high="dark green") +
+#   labs(x="Mitochondrion", y="Mitochondrion") +
+#   theme_bw()
 
 ggplot() +
-  geom_raster(data=data_above_thresh, aes(x = Var1, y = Var2, fill=value)) +
-  geom_raster(data=data_below_thresh, aes(x = Var1, y = Var2), fill='yellow') +
+  geom_raster(data=melted_ent_mat, aes(x = Var1, y = Var2, fill=value)) +
+  scale_fill_gradient(low="yellow", high="dark green") +
   labs(x="Mitochondrion", y="Mitochondrion") +
-  # scale_fill_manual(name = 'fill', values = setNames(c(melted_ent_mat$value,'yellow'),c(T, F))) +
-  scale_fill_gradient(low="light green", high="dark green") +
   theme_bw()
