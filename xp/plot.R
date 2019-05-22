1cd62028c280ed3db13410a36b40086d73edbfb5
Morteza Hosseini <s_morteza_h@yahoo.com>
2019-05-22 18:05:27 +0100

    update

--- xp/plot.R
+++ xp/plot.R
@@ -5,7 +5,7 @@ library(scales)
 entropy <- scan("mit.ent", quiet = TRUE)
 ent_mat <- matrix(entropy, byrow=T, nrow=3327, ncol=3327)
 threshold <- 1.2
-ent_mat[ent_mat > threshold] <- threshold
+ent_mat[ent_mat < threshold] <- threshold
 melted_ent_mat <- melt(ent_mat)
 
 ggplot() +
@@ -14,4 +14,3 @@ ggplot() +
   # scale_fill_manual(name = 'fill', values = setNames(c(melted_ent_mat$value,'yellow'),c(T, F))) +
   scale_fill_gradient(low="yellow", high="dark green") +
   theme_bw()
-
