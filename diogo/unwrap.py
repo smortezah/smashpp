b33bc9d124f6b0ef013a03016cc7cda89d395c6d
Morteza Hosseini <s_morteza_h@yahoo.com>
2019-05-29 19:41:05 +0100

    update
    Former-commit-id: 85a815a03e81dc94c27ceb362d6b7bde2553f5f2 [formerly b5cb0a79f9fd2fdcb59666484c1e4c06bce20ccb] [formerly 78a11f07a5a36bcb65276f2fb395f662b2f6cf73] [formerly a2a15d71ffce7a5b1e1b32a99963d671e141c1f4 [formerly 58e26c8536b4ec5fd090627ae320aec9150f6248]] [formerly 08dcce29af2bcc2d6a108a20c2bf7c016e8f5e90 [formerly dbfbe751cfb37c67d0691824d93aa1abedb6729e]]
    Former-commit-id: 41596f3aca7ae5532c30928f7b2012a7de2862ba
    Former-commit-id: 18162f94ba656207ddf01effb68a65a24fcde85e
    Former-commit-id: 15f643ad8cdba352381264bd505fc16b40837501
    Former-commit-id: 62ca81eb1e2e70f6c57e72020a35e777877351da
    Former-commit-id: a4fac5d38e8c22a1150e9122037ccbdf5f41129c [formerly 729ae14d889daf2403d6801c5bcb56a19ac391ce]
    Former-commit-id: b582c0c8531983e38e68f327681a4a9387a094e3
    Former-commit-id: 4de083147f4bae3144bc3d92b2f635b3d4e0925a

--- diogo/unwrap.py
+++ diogo/unwrap.py
@@ -11,12 +11,26 @@ def execute(cmd):
     os.popen(cmd).read()
 
 
-ref = 'MT-CO1.fa'
+ref_name = 'MT-CO1.fa'
 tar_list = ['1']
 
-for tar in tar_list:
+for tar_name in tar_list:
     # execute(smashpp + '-r ' + ref + ' -t ' + tar)
 
-    with open(ref + '.' + tar + '.pos') as pos_file:
-        first_line = pos_file.readline()
-        print(first_line[5])
+    pos_file = open(ref_name + '.' + tar_name + '.pos')
+    pos_file.readline()  # Skip header
+    first_line = pos_file.readline().split('\t')
+    beg_pos = int(first_line[4])
+    end_pos = int(first_line[5])
+
+    tar_bk_name = tar_name + '.bk'
+    if os.path.exists(tar_bk_name):
+        os.remove(tar_bk_name)
+    os.rename(tar_name, tar_bk_name)
+    tar_bk = open(tar_bk_name, 'r')
+    tar_content = tar_bk.read()
+
+    tar = open(tar_name, 'w')
+    tar.write(str(tar_content[beg_pos:]))
+    tar.write(str(tar_content[0:beg_pos]))
+    # os.remove(tar_bk_name)
