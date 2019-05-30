97bbb3dc3092776778f7ac7796d9c08f4e2fdbc3
Morteza Hosseini <s_morteza_h@yahoo.com>
2019-05-30 09:16:30 +0100

    update
    Former-commit-id: e2581927ab8da76e26982f11f780060a349a97e2 [formerly b31d557c3959c362a00581488d4f8001b84c53ed] [formerly 7c8168be72e76ee80b4491b9beff0af6fad3a940] [formerly cdd02677af6b2c73041d0ce19ebf2dd64bd814e7 [formerly 59bfb07cf744d8f09bbb1f5eedee83ea9ed1bd4f]] [formerly 2cc0227e1e5dae7332bafab0e4018cfe0d11ccc1 [formerly 5fa12a2426eb8660668456f0e0e5f00048db245e]]
    Former-commit-id: 33f84de4d8b6aba617d7f2d28611fe3115502422
    Former-commit-id: 8783aa9d05b431aed742fc79aecaa019da867eca
    Former-commit-id: 4f5d266b1bd9bcbb0aaddd142858eb72e2ab83d2
    Former-commit-id: 54e3617138f58d030b3cff11db059a5891d9bafb
    Former-commit-id: e57948d6e6c62bca26c9ca97dbeda14577c4c8fd [formerly 58218318ae3d1e22e4c9223f902d28ec3f1d1e16]
    Former-commit-id: ede626f9b85ea94ba1d44c965d3fe3e3b9b0efb3
    Former-commit-id: 1c898ba5f208fbd7525c9aac986a169465972bb0

---
+++ diogo/xp2/unwrap.py
@@ -0,0 +1,36 @@
+import os
+
+if os.name == 'posix':
+    sep = '/'
+elif os.name == 'nt':
+    sep = '\\'
+smashpp = '..' + sep + 'smashpp '
+
+
+def execute(cmd):
+    os.popen(cmd).read()
+
+
+ref_name = 'MT-CO1.fa'
+tar_list = ['1']
+
+for tar_name in tar_list:
+    # execute(smashpp + '-r ' + ref + ' -t ' + tar)
+
+    pos_file = open(ref_name + '.' + tar_name + '.pos')
+    pos_file.readline()  # Skip header
+    first_line = pos_file.readline().split('\t')
+    beg_pos = int(first_line[4])
+    end_pos = int(first_line[5])
+
+    tar_bk_name = tar_name + '.bk'
+    if os.path.exists(tar_bk_name):
+        os.remove(tar_bk_name)
+    os.rename(tar_name, tar_bk_name)
+    tar_bk = open(tar_bk_name, 'r')
+    tar_content = tar_bk.read()
+
+    tar = open(tar_name, 'w')
+    tar.write(str(tar_content[beg_pos:]))
+    tar.write(str(tar_content[0:beg_pos]))
+    # os.remove(tar_bk_name)
