894bf74a3bf237c7b019508f49a42904da342d1e
Morteza Hosseini <s_morteza_h@yahoo.com>
2019-06-08 16:06:19 +0100

    update

--- diogo/xp2/run.py
+++ diogo/xp2/run.py
@@ -1,16 +1,17 @@
+#%%
 import os
 import shutil
 import matplotlib.pyplot as plt
 import numpy as np
 
 prepare_data = False
-find_simil_seqs = True
+find_simil_seqs = False
 find_simil_regions = False
-plot_simil = False
+plot_simil = True
 
 main_file = 'mtDNA_Chordata_3327_22-03-2019.fasta'
 data_in_path = 'Taxonomy_Chordata_NCBI_Diogo_06-05-2019'
-num_files = 3327
+# num_files = 3327
 ave_ent_file = 'ent'
 threshold = 0.2  # in (0, 1]
 ent_threshold = 8 * threshold
@@ -114,38 +115,17 @@ if find_simil_seqs:
         if os.path.exists(file):
             os.remove(file)
 
-    # first = 1
-    # if os.path.exists(ave_ent_file):
-    #     os.remove(ave_ent_file)
-
-    # out_file = open(ave_ent_file, "w")
-    # for i in range(first, num_files - first + 2):
-    #     for j in range(first, num_files - first + 2):
-    #         execute('./geco -rm 6:1:0:0/0 -rm 10:10:1:0/0 -rm 14:50:1:3/10 ' +
-    #                 '-c 30 -g 0.95 -r ' + str(i) + ' ' + str(j) + ' > log')
-
-    #         with open('log', 'r') as log_file:
-    #             for line in log_file:
-    #                 line_list = line.split()
-    #                 if len(line_list) > 5:
-    #                     out_file.write(str(line_list[5]) + "\t")
-    #     out_file.write("\n")
-
-    # for file in ["log", "*.co"]:
-    #     if os.path.exists(file):
-    #         os.remove(file)
-
 
 def build_simil_matrix(nrc_mat):
     # num = 0
+    num_files = len(nrc_mat)
     simil_mat = [[0 for x in range(num_files)] for y in range(num_files)]
     for i in range(0, num_files):
-        for j in range(i, num_files):
-            if i != j:
-                if min(nrc_mat[i][j], nrc_mat[j][i]) < ent_threshold:
-                    # num += 1
-                    simil_mat[i][j] = 1
-                    # simil_mat[j][i] = 1
+        for j in range(i+1, num_files):
+            if min(nrc_mat[i][j], nrc_mat[j][i]) < ent_threshold:
+                # num += 1
+                simil_mat[i][j] = 1
+                # simil_mat[j][i] = 1
     # print(num)
     return simil_mat
 
@@ -169,20 +149,27 @@ if find_simil_regions:
                 os.popen(cmd).read()
 
 if plot_simil:
-    nrc_mat = np.genfromtxt(ave_ent_file, dtype=float)
+    nrc_mat = np.genfromtxt(
+        ave_ent_file + '_Chondrichthyes.tsv', skip_header=True)
+    plt.matshow(nrc_mat)
+    plt.show()
+
     # for x in nrc_mat:
     #     print(*x, sep="\t")
 
-    simil_mat = build_simil_matrix(nrc_mat)
-    # for x in simil_mat:
-    #     print(*x, sep=" ")
+    # simil_mat = build_simil_matrix(nrc_mat)
+    # # for x in simil_mat:
+    # #     print(*x, sep=" ")
 
     # plt.matshow(nrc_mat)
-    # plt.colorbar()
-    # # plt.show()
-    # plt.savefig('nrc_mat_'+str(threshold)+'.pdf')
+    # # plt.colorbar()
+    # # # plt.show()
+    # # plt.savefig('nrc_mat_'+str(threshold)+'.pdf')
 
-    plt.matshow(simil_mat)
-    plt.colorbar()
-    plt.show()
-    # plt.savefig('simil.'+str(threshold)+'.pdf')
+    # plt.matshow(simil_mat)
+    # plt.colorbar()
+    # plt.show()
+    # # plt.savefig('simil.'+str(threshold)+'.pdf')
+
+
+#%%
