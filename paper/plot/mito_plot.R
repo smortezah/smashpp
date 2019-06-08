503aa65cfb019226509070cbf42a56235c2f6e31
Morteza Hosseini <s_morteza_h@yahoo.com>
2019-06-08 12:44:21 +0100

    update
    Former-commit-id: 4faeeffc480f17c13782d992cf28cd94294c3e3c [formerly f01d2384ff141ed3672ecf00fc06ddd042667446] [formerly 71fcccaced408de84e035edfcf255b3e3c2e647d] [formerly c908d7c739cb072d2b4063c8381288e4305a4eb6 [formerly d2edf392a8e66b380ae97d8457e49471d27330cf]] [formerly aa3ac3e525912938b0f5cc4fc2ed1eaaabf9d4b0 [formerly b6da67fcf88d1d7f3fb03bb4dd1df1165d2aba3c]]
    Former-commit-id: 1f1e47ca86e79ed763bf844d1b79f1d1fbf19441
    Former-commit-id: 94f81e9ac6d9e099c79ae14cef4940de143c0e69
    Former-commit-id: 0721665f162ff92c8acfe10e17c4ba5f8de871e8
    Former-commit-id: f1aad3f98914a004eba0f5e6fd0017d916cb9b6c
    Former-commit-id: 6a08954f6f267a41a75c1125fc9a5254b626b625 [formerly 68488b8b9587b36b3afcacd3f64eacbf5f7fa168]
    Former-commit-id: f9f1aaf67350a8cb2f298707c0f7a35eb5ccf333
    Former-commit-id: b565da623eb3c827c2c33c7973b01ae2db4f1cc9

---
+++ paper/plot/mito_plot.R
@@ -0,0 +1,41 @@
+library(ggplot2)
+library(reshape2)
+library(scales)
+# library(dplyr)
+
+entropy <- scan("mit.ent", quiet = TRUE)
+ent_mat <- matrix(entropy, byrow=T, nrow=3327, ncol=3327)
+threshold <- 1.6
+melted_ent_mat <- melt(ent_mat)
+
+ggplot(melted_ent_mat, aes(x= value)) +
+  geom_density(fill='blue')
+
+
+# melted_ent_mat <- melt(ent_mat) %>%
+#   mutate(val_trimmed = case_when(
+#     value <= threshold ~ threshold,
+#     T ~ value
+#   ))
+#
+# ggplot(melted_ent_mat, aes(x = Var1, y = Var2, fill = val_trimmed)) +
+#   geom_raster() +
+#   scale_fill_gradient(low="yellow", high="dark green")
+
+# ent_mat[ent_mat < threshold] <- threshold
+# melted_ent_mat <- melt(ent_mat)
+# # data_below_thresh <- subset(melted_ent_mat, melted_ent_mat$value<=threshold)
+# # data_above_thresh <- subset(melted_ent_mat, melted_ent_mat$value>threshold)
+# 
+# # ggplot() +
+# #   geom_raster(data=data_above_thresh, aes(x = Var1, y = Var2, fill=value)) +
+# #   geom_raster(data=data_below_thresh, aes(x = Var1, y = Var2), fill='yellow') +
+# #   scale_fill_gradient(low="yellow", high="dark green") +
+# #   labs(x="Mitochondrion", y="Mitochondrion") +
+# #   theme_bw()
+# 
+# ggplot() +
+#   geom_raster(data=melted_ent_mat, aes(x = Var1, y = Var2, fill=value)) +
+#   scale_fill_gradient(low="yellow", high="dark green") +
+#   labs(x="Mitochondrion", y="Mitochondrion") +
+#   theme_bw()
