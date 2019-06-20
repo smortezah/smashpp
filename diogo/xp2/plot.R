8f09fe7775e3f20185bf3e3d19730d1245963b60
Morteza Hosseini <s_morteza_h@yahoo.com>
2019-06-20 22:38:16 +0100

    update
    Former-commit-id: 57dee4d46b3f9f81b9ce2ee98b085937166e7756 [formerly db83b2466af691fe2b5a0ae2f184519c04975e91] [formerly 5cd49f4d879b29b56f4debfa2b107953674ee0b2] [formerly d88a68d7946dcac7faf66ae813dc5b364cce173d [formerly d683e54d7e8f32980f6625d5adde95ad9c5cc536]] [formerly ef2835a8b3f0611a24a392e890f555030eedd2ab [formerly 66442f969f0b4d694803be0fbd979a8169d76778]]
    Former-commit-id: 68a807c1858d9aef99d86601f615f582cb7ac287
    Former-commit-id: 3fd267260b845d2b1ab80d16a32fd8847f311341
    Former-commit-id: c704b3769c4e7831ffe04ed639fae0a189795bf1
    Former-commit-id: d8c3362af9ff44f13784616fec7930d630166048
    Former-commit-id: 0a32c70e7f4f1ac55f04ae49c04381cdc855289a [formerly 940e57a3cfe0efc8ca0bbeec8b353ce3a70a6254]
    Former-commit-id: 2965066e727cf5776033d8d927ff21ee82068f5d
    Former-commit-id: fbdef892a73d3aa6996e0da2fbd96a65b5b9b3d2

--- diogo/xp2/plot.R
+++ diogo/xp2/plot.R
@@ -14,9 +14,10 @@ ggplot(melted_rearrange_mat, aes(x = Var2, y = Var1)) +
   # scale_fill_gradient2(low = "red", mid = "white",
   #                      high = "blue", midpoint = 65, space = "Lab") +
   scale_fill_gradientn(
+    name="Experimental\nCondition",
     colours = hcl.colors(9, palette = "spectral", rev = TRUE),
-    values = rescale(c(0, 40, 80, 125)),
-    limits = c(0, 125)
+    values = rescale(c(0, 20, 30, 50, 80, 125)),
+    # limits = c(0, 125)
   ) +
   theme_bw() +
   ggtitle("Number of rearrangements in Chondrichthyes") +
@@ -25,12 +26,14 @@ ggplot(melted_rearrange_mat, aes(x = Var2, y = Var1)) +
     axis.title.x = element_blank(),
     axis.title.y = element_blank(),
     axis.text.x = element_text(angle = 90, vjust = 0.3),
-    legend.position = c(0.08, 0.6),
+    legend.direction="horizontal",
+    legend.justification=c(0,0),
+    legend.position = c(0, 0.8),
     legend.background = element_rect(fill = "transparent"),
-    legend.title = element_text(color = "white"),
+    # legend.title = element_blank(), # element_text(color = "white"),
     legend.text = element_text(color = "white"),
-    legend.key.size = unit(1.0, "cm"),
-    legend.key.width = unit(0.5, "cm")
+    legend.key.size = unit(0.5, "cm"),
+    legend.key.width = unit(1.25, "cm")
   )
 
 # ggsave("rearrange_count_Chondrichthyes.pdf", scale = 3)
