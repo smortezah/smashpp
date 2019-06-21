0011cb6349572c57a4aef0912b6ce8c826753a23
Morteza Hosseini <s_morteza_h@yahoo.com>
2019-06-21 17:22:53 +0100

    update

--- diogo/xp2/plot.R
+++ diogo/xp2/plot.R
@@ -4,84 +4,123 @@ library(scales)
 # library(dplyr)
 library(ggcorrplot)
 library(corrplot)
+library(factoextra)
 
-plot_rearrange <- FALSE
-plot_nrc <- TRUE
+plot_rearrange <- TRUE
+plot_nrc <- FALSE
 
 if (plot_rearrange) {
   rearrange <-
     read.table("rearrange_count_Chondrichthyes.tsv", header = TRUE)
   rearrange_mat <- as.matrix(rearrange)
   melted_rearrange_mat <- melt(rearrange_mat)
+  # corr <- round(cor(rearrange), 1)
   
+  # res.dist <- get_dist(rearrange, method = "euclidean")
+  # fviz_dist(res.dist, lab_size = 8)
+  
+  # res.hc <- hclust(res.dist, method = "ward.D2")
+  # plot(res.hc, cex = 0.5)
+  
+  # corrplot(
+  #   rearrange_mat[,1:40],
+  #   is.corr = FALSE,
+  #   type = "upper",
+  #   method = "color",
+  #   tl.col = "black",
+  #   outline = TRUE,
+  #   addgrid.col = "white",
+  #   # cl.align = "l",
+  # ) #+
   ggplot(melted_rearrange_mat, aes(x = Var2, y = Var1)) +
-    geom_raster(aes(fill = value)) +
-    # scale_fill_gradient2(low = "red", mid = "white",
-    #                      high = "blue", midpoint = 65, space = "Lab") +
-    ggtitle("Chondrichthyes") +
+    geom_tile(aes(fill = value), colour = "white") +
+    # geom_raster(aes(fill = value)) +
+    coord_fixed() +
+    # scale_fill_gradient2(
+    #   low = "white",
+    #   mid = "lightblue",
+    #   high = "yellow",
+    #   midpoint = 63,
+    #   space = "Lab"
+    # ) +
+    # ggtitle("Chondrichthyes") +
     scale_fill_gradientn(
+      labels = c("1",
+                 "25",
+                 "50",
+                 "75",
+                 "100",
+                 "125"),
+      breaks = c(1,
+                 25,
+                 50,
+                 75,
+                 100,
+                 125),
       guide = guide_colorbar(
-        title = "Number of rearrangements",
+        title = "No.\nrearrangements\n(Chondrichthyes)",
         title.position = "top",
-        title.hjust = 0.5
+        # title.hjust = 0.5
       ),
-      colours = hcl.colors(9, palette = "spectral", rev = TRUE),
+      colours = hcl.colors(5, palette = "spectral", rev = TRUE),
       # values = rescale(c(0, 30, 40, 50, 90, 110, 125)),
-      # limits = c(0, 125)
+      limits = c(1, 126),
+      na.value = "white"
     ) +
     scale_y_discrete(position = "right") +
-    theme_bw() +
+    # theme_bw() +
     theme(
       axis.title.x = element_blank(),
       axis.title.y = element_blank(),
       axis.text.x = element_text(angle = 90, vjust = 0.3),
-      legend.direction = "horizontal",
-      legend.justification = c(0, 0),
-      legend.position = c(0, 0.8),
-      legend.background = element_rect(fill = "transparent"),
-      legend.title = element_text(color = "white"),
-      legend.text = element_text(color = "white"),
-      legend.key.size = unit(0.5, "cm"),
-      legend.key.width = unit(1.25, "cm")
+      # legend.direction = "horizontal",
+      # legend.background = element_rect(fill = "transparent"),
+      # legend.title = element_text(color = "white"),
+      # legend.text = element_text(color = "white"),
+      # legend.key.size = unit(0.5, "cm"),
+      legend.justification = c(0, 1),
+      legend.position = c(0, 1),
+      legend.key.height = unit(1.35, "cm")
+      # legend.key.width = unit(1.25, "cm")
     )
   
   # ggsave("rearrange_count_Chondrichthyes.pdf", scale = 3)
 } else if (plot_nrc) {
   ent <- read.table("ent_Chondrichthyes.tsv", header = TRUE)
-  # ent_mat <- as.matrix(ent)
+  ent_mat <- as.matrix(ent)
   corr <- round(cor(ent), 1)
-  p.mat <- cor_pmat(ent)
+  # p.mat <- cor_pmat(ent)
   # ggcorrplot(corr,
   #            hc.order = TRUE,
   #            type = "lower",
   #            outline.col = "white") #+
-  corrplot(corr, type = "upper", order = "hclust", method = "color", tl.col = "black", outline = TRUE) #+
-    # ggtitle("Chondrichthyes") +
-    # # scale_fill_gradientn(
-    # #   guide = guide_colorbar(
-    # #     title = "Number of rearrangements",
-    # #     title.position = "top",
-    # #     title.hjust = 0.5
-    # #   ),
-    # #   colours = hcl.colors(3, palette = "spectral", rev = TRUE),
-    # #   # values = rescale(c(0, 30, 40, 50, 90, 110, 125)),
-    # #   limits = c(-1, 1)
-    # # ) +
-    # # scale_y_discrete(position = "right") +
-    # theme_bw() +
-    # theme(
-    #   axis.title.x = element_blank(),
-    #   axis.title.y = element_blank(),
-    #   axis.text.x = element_text(angle = 90, vjust = 0.3),
-    #   # legend.direction = "horizontal",
-    #   # legend.justification = c(0, 0),
-    #   # legend.position = c(0, 0.8),
-    #   # legend.background = element_rect(fill = "transparent"),
-    #   # # legend.title = element_text(color = "white"),
-    #   # # legend.text = element_text(color = "white"),
-    #   # legend.key.size = unit(0.5, "cm"),
-    #   # legend.key.width = unit(1.25, "cm")
-    # )
+  # corrplot(corr, type = "upper", order = "hclust", method = "color", tl.col = "black", addrect = 5, col=hcl.colors(9, palette = "spectral", rev = TRUE)) #+
+  # ggtitle("Chondrichthyes") +
+  # # scale_fill_gradientn(
+  # #   guide = guide_colorbar(
+  # #     title = "Number of rearrangements",
+  # #     title.position = "top",
+  # #     title.hjust = 0.5
+  # #   ),
+  # #   colours = hcl.colors(3, palette = "spectral", rev = TRUE),
+  # #   # values = rescale(c(0, 30, 40, 50, 90, 110, 125)),
+  # #   limits = c(-1, 1)
+  # # ) +
+  # # scale_y_discrete(position = "right") +
+  # theme_bw() +
+  # theme(
+  #   axis.title.x = element_blank(),
+  #   axis.title.y = element_blank(),
+  #   axis.text.x = element_text(angle = 90, vjust = 0.3),
+  #   # legend.direction = "horizontal",
+  #   # legend.justification = c(0, 0),
+  #   # legend.position = c(0, 0.8),
+  #   # legend.background = element_rect(fill = "transparent"),
+  #   # # legend.title = element_text(color = "white"),
+  #   # # legend.text = element_text(color = "white"),
+  #   # legend.key.size = unit(0.5, "cm"),
+  #   # legend.key.width = unit(1.25, "cm")
+  # )
 } else {
   # n_row_col <- 113
   # entropy <- scan("ent_Chondrichthyes.tsv", quiet = TRUE)
