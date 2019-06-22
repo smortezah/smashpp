2b27b391fbb0f674035db67b5fd43cdc1a389eaa
Morteza Hosseini <s_morteza_h@yahoo.com>
2019-06-22 12:43:07 +0100

    update

--- diogo/xp2/plot.R
+++ diogo/xp2/plot.R
@@ -13,15 +13,20 @@ if (plot_rearrange) {
   rearrange <-
     read.table("rearrange_count_Chondrichthyes.tsv", header = TRUE)
   rearrange_mat <- as.matrix(rearrange)
+  header <- colnames(rearrange_mat)
   melted_rearrange_mat <- melt(rearrange_mat)
-  # corr <- round(cor(rearrange), 1)
   
+  top_row_mat <- as.matrix(rearrange_mat['NC_028344', ])
+  top_row_mat <- subset(top_row_mat, top_row_mat != 0)
+  top_row_mat <- top_row_mat[order(top_row_mat[,1], decreasing = TRUE)]
+  # colnames(top_row_mat) <- c("NC_028344")
+  # melted_top_row_mat <- melt(top_row_mat)
+
+  # corr <- round(cor(rearrange), 1)
   # res.dist <- get_dist(rearrange, method = "euclidean")
   # fviz_dist(res.dist, lab_size = 8)
-  
   # res.hc <- hclust(res.dist, method = "ward.D2")
   # plot(res.hc, cex = 0.5)
-  
   # corrplot(
   #   rearrange_mat[,1:40],
   #   is.corr = FALSE,
@@ -33,8 +38,19 @@ if (plot_rearrange) {
   #   # cl.align = "l",
   # ) #+
   # ggplot(melted_rearrange_mat, aes(x = Var2, y = Var1)) +
-  ggplot(melted_rearrange_mat, aes(x = Var1, y = Var2)) +
-    geom_tile(aes(fill = value), colour = "white") +
+  #   geom_tile(aes(fill = value), colour = "white") +
+  ggplot() +
+    # geom_tile(
+    #   melted_rearrange_mat,
+    #   mapping = aes(x = Var2, y = Var1, fill = value),
+    #   colour = "white"
+    # ) +
+    geom_tile(
+      melted_top_row_mat,
+      mapping = aes(x = Var1, y = Var2, fill = value),
+      colour = "white",
+      # na.rm = TRUE
+    ) +
     # geom_raster(aes(fill = value)) +
     coord_fixed() +
     # scale_fill_gradient2(
@@ -46,37 +62,10 @@ if (plot_rearrange) {
     # ) +
     # ggtitle("Chondrichthyes") +
     scale_fill_gradientn(
-      # labels = c("1",
-      #            "25",
-      #            "50",
-      #            "75",
-      #            "100",
-      #            "125"),
-      labels = c("1", str(seq(10,130,10))
-                 # "20",
-                 # "40",
-                 # "60",
-                 # "80",
-                 # "100",
-                 # "120"
-                 ),
-      breaks = c(1, seq(10,130,10)
-                 # 20,
-                 # 40,
-                 # 60,
-                 # 80,
-                 # 100,
-                 # 120
-                 ),
-      # breaks = c(1,
-      #            25,
-      #            50,
-      #            75,
-      #            100,
-      #            125),
+      # labels = c("1", str(seq(10,130,10))),
+      # breaks = c(1, seq(10,130,10)),
       guide = guide_colorbar(
         title = "No.\nrearrangements\n(Chondrichthyes)",
-        # title = "No. rearrangements\n(Chondrichthyes)",
         title.position = "top",
         title.vjust = 1,
         # label.position = "left"
@@ -86,7 +75,8 @@ if (plot_rearrange) {
       limits = c(1, 126),
       na.value = "white"
     ) +
-    scale_x_discrete(position = "top") +
+    # scale_x_discrete(position = "top") +
+    # scale_x_discrete(limit = c(rev(header))) +
     # theme_bw() +
     theme(
       axis.title.x = element_blank(),
@@ -97,14 +87,15 @@ if (plot_rearrange) {
       # legend.title = element_text(color = "white"),
       # legend.text = element_text(color = "white"),
       # legend.key.size = unit(0.5, "cm"),
-      legend.justification = c(1, 0),
-      legend.position = c(1, 0),
+      legend.justification = c(1, 1),
+      legend.position = c(1, 1),
       # legend.key.height = unit(1.25, "cm"),
-      legend.key.height = unit(6.5, "cm"), # Makes sense in scale=3
+      legend.key.height = unit(6.5, "cm"),
+      # Makes sense in scale=3
       # legend.text.align = 1
     )
   
-  ggsave("rearrange_count_Chondrichthyes.pdf", scale = 3)
+  # ggsave("rearrange_count_Chondrichthyes.pdf", scale = 3)
 } else if (plot_nrc) {
   ent <- read.table("ent_Chondrichthyes.tsv", header = TRUE)
   ent_mat <- as.matrix(ent)
