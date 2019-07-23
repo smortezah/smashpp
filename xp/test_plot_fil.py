38f2f2035016045ee32c96c57d572caa5b97c08f
Morteza Hosseini <s_morteza_h@yahoo.com>
2019-07-23 15:37:32 +0100

    update
    Former-commit-id: c25baa0b6864372f74a7318795f885b12f4a610b [formerly 279b39748227cf041108d45395867e7d80a60e86] [formerly 8e51f64996d0c197512fd739d00ea1ac73a3b0a3] [formerly 0408572d83a2d3c930bacd2e75972bf19b3238c9 [formerly ddc5612d734db184a25492558fc2e4813736a176]] [formerly 7c916a0cf7d3118d29ca9dcd3dffbdb6a4eceed9 [formerly 2962c3122ed1b62946aee8d082dd7bb326e1cc25]]
    Former-commit-id: 5b843b7bb22e0dde06d990b0780ec382c1c3007f
    Former-commit-id: cd719bf5499e0311e443b546352501b76d842f22
    Former-commit-id: 8fc2ca4698bd23fcb4ef13d34f432df24b88af97
    Former-commit-id: a376f87919ca25fbf1668f5f1bfb9180e774054c
    Former-commit-id: cb35cac04060e2522ba250c9d47dd0d511a67932 [formerly 3eb7f38574d291cfca24ec310f9cd621047ff303]
    Former-commit-id: 335bb0da93c26294171fe51cbccc97e4a8635d07
    Former-commit-id: 43a9f073d1487563a99a74d18b1c2898d7de2326

---
+++ xp/test_plot_fil.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+
+y0, y1 = [], []
+for line0 in open('0.gga28.seq.mga30.seq.fil', 'r'):
+  values0 = [float(s) for s in line0.split()]
+  y0.append(values0[0])
+for line1 in open('1.gga28.seq.mga30.seq.fil', 'r'):
+  values1 = [float(s) for s in line1.split()]
+  y1.append(values1[0])
+
+f, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
+ax1.plot(y0)
+ax1.set_title('ir=0')
+ax2.plot(y1)
+# ax1.set_title('ir=1')
