3f85574a7c6d5bf2a8b159b2ff93a46f9686a061
Morteza Hosseini <s_morteza_h@yahoo.com>
2019-07-30 13:03:40 +0100

    update
    Former-commit-id: cb6713d65407ebc45e0fe1156df056f1b0fb0b9c [formerly 0856f54f7b845eabaa4e6abd1a0c02a328399f74] [formerly 33333852700b2bc66a302ea21ee72e34f54a0376] [formerly 06187986f22e3c87f70f42d6640eb8a660675776 [formerly 4e3b4b0761795c427f0a6712b9874959dcba5ad2]] [formerly 2fe27e2278b4c8786cb6e552b109bcab9eab1a6c [formerly 9cf2fb57b83a015d1b7a6ff7483bcb4cb0a46fd6]]
    Former-commit-id: 82693ffc299d75513a526fe0fd82e676fe8f86a2
    Former-commit-id: 1e0bc4a0f817b01c4fba1c274504e32d0be0b930
    Former-commit-id: 2eff611058e1575de5a41d245974005955bbd0d2
    Former-commit-id: 080e421b0b8dd15fa4ba6262b2871d66b1b400fd
    Former-commit-id: 174b4a4f2d8bccb2720081f9f8692f1c84bde458 [formerly 846602b0dfae17356f6bc4b4743533f8f9cdfa14]
    Former-commit-id: a5363b25e1461ce95287ae0b2e2819be1534e545
    Former-commit-id: 9caaaa7f2851d94bee01a31376da61c21c31bd14

--- xp/test_plot_fil.py
+++ xp/test_plot_fil.py
@@ -3,12 +3,24 @@ import matplotlib.pyplot as plt
 y0, y1 = [], []
 # for line0 in open('0.gga24_1M.26.fil', 'r'):
 for line0 in open('0.24.26.fil', 'r'):
-  values0 = [float(s) for s in line0.split()]
-  y0.append(values0[0])
+# for line0 in open('0.21.seq.23.seq.fil', 'r'):
+# for line0 in open('0.19.seq.21.seq.fil', 'r'):
+# for line0 in open('0.17.seq.19.seq.fil', 'r'):
+# for line0 in open('0.18.seq.20.seq.fil', 'r'):
+# for line0 in open('0.15.seq.17.seq.fil', 'r'):
+# for line0 in open('0.13.seq.15.seq.fil', 'r'):
+    values0 = [float(s) for s in line0.split()]
+    y0.append(values0[0])
 # for line1 in open('1.gga24_1M.26.fil', 'r'):
 for line1 in open('1.24.26.fil', 'r'):
-  values1 = [float(s) for s in line1.split()]
-  y1.append(values1[0])
+# for line1 in open('1.21.seq.23.seq.fil', 'r'):
+# for line1 in open('1.19.seq.21.seq.fil', 'r'):
+# for line1 in open('1.17.seq.19.seq.fil', 'r'):
+# for line1 in open('1.18.seq.20.seq.fil', 'r'):
+# for line1 in open('1.15.seq.17.seq.fil', 'r'):
+# for line1 in open('1.13.seq.15.seq.fil', 'r'):
+    values1 = [float(s) for s in line1.split()]
+    y1.append(values1[0])
 
 f, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
 ax1.plot(y0)
