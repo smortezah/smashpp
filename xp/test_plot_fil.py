298ec2886a0b97ce23f050fc56219021bd555ef8
Morteza Hosseini <s_morteza_h@yahoo.com>
2019-07-30 15:52:03 +0100

    update
    Former-commit-id: 3e8d00be3837fbc2d07b378349bb55cc77e0c70b [formerly c3fc71b44e409e9491e801faf008c465f899711a] [formerly b057aa0d44e7a96d33edfd742fbb539e95db16de] [formerly 17cd3e7824d536f2e0d6bb8ef246d2fa0c6a07c4 [formerly 31d47a170b1b2aedfe2be163cdda9f431782b9d9]] [formerly 954ebe4f2a12fb2c0aed7e4b1de2ad9302c70391 [formerly 159d1c971cc4ab2aeead32a3aca876d8b62dc8c5]]
    Former-commit-id: e0b40ee75aeaa5ac3e87a093e9406c25b8e75ed2
    Former-commit-id: 80c4708f9048a70a7f4429d8fcc58275f249eaf9
    Former-commit-id: f59ebc7999affc5bec248d2af6110b98fa0b8242
    Former-commit-id: 9e7e5870b02eaed8a8c776c69958b3c6c493b968
    Former-commit-id: 3241575b600804bb2bc87b59a6dbd2a80184da85 [formerly 373601e48dd21861fb7a7813a633fc1605e83687]
    Former-commit-id: 2465af296b8f407c5325b07f1146b709877b9ecb
    Former-commit-id: 10f29e6d6011c9e511ab6c094cc33a021249f828

--- xp/test_plot_fil.py
+++ xp/test_plot_fil.py
@@ -2,21 +2,21 @@ import matplotlib.pyplot as plt
 
 y0, y1 = [], []
 # for line0 in open('0.gga24_1M.26.fil', 'r'):
-for line0 in open('0.24.26.fil', 'r'):
+# for line0 in open('0.24.26.fil', 'r'):
 # for line0 in open('0.21.seq.23.seq.fil', 'r'):
 # for line0 in open('0.19.seq.21.seq.fil', 'r'):
 # for line0 in open('0.17.seq.19.seq.fil', 'r'):
-# for line0 in open('0.18.seq.20.seq.fil', 'r'):
+for line0 in open('0.18.seq.20.seq.fil', 'r'):
 # for line0 in open('0.15.seq.17.seq.fil', 'r'):
 # for line0 in open('0.13.seq.15.seq.fil', 'r'):
     values0 = [float(s) for s in line0.split()]
     y0.append(values0[0])
 # for line1 in open('1.gga24_1M.26.fil', 'r'):
-for line1 in open('1.24.26.fil', 'r'):
+# for line1 in open('1.24.26.fil', 'r'):
 # for line1 in open('1.21.seq.23.seq.fil', 'r'):
 # for line1 in open('1.19.seq.21.seq.fil', 'r'):
 # for line1 in open('1.17.seq.19.seq.fil', 'r'):
-# for line1 in open('1.18.seq.20.seq.fil', 'r'):
+for line1 in open('1.18.seq.20.seq.fil', 'r'):
 # for line1 in open('1.15.seq.17.seq.fil', 'r'):
 # for line1 in open('1.13.seq.15.seq.fil', 'r'):
     values1 = [float(s) for s in line1.split()]
