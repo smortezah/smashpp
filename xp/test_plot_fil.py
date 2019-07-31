79290dc8a2ebf10ac9360a972c6e20f6aa83df4d
Morteza Hosseini <s_morteza_h@yahoo.com>
2019-07-31 11:44:28 +0100

    update

--- xp/test_plot_fil.py
+++ xp/test_plot_fil.py
@@ -9,7 +9,9 @@ y0, y1 = [], []
 # for line0 in open('0.18.seq.20.seq.fil', 'r'):
 # for line0 in open('0.15.seq.17.seq.fil', 'r'):
 # for line0 in open('0.13.seq.15.seq.fil', 'r'):
-for line0 in open('0.1.seq.1.seq.fil', 'r'):
+# for line0 in open('0.1.seq.1.seq.fil', 'r'):
+# for line0 in open('0.2.seq.2.seq.fil', 'r'):
+for line0 in open('0.4.seq.4.seq.fil', 'r'):
     values0 = [float(s) for s in line0.split()]
     y0.append(values0[0])
 # for line1 in open('1.gga24_1M.26.fil', 'r'):
@@ -20,7 +22,9 @@ for line0 in open('0.1.seq.1.seq.fil', 'r'):
 # for line1 in open('1.18.seq.20.seq.fil', 'r'):
 # for line1 in open('1.15.seq.17.seq.fil', 'r'):
 # for line1 in open('1.13.seq.15.seq.fil', 'r'):
-for line1 in open('1.1.seq.1.seq.fil', 'r'):
+# for line1 in open('1.1.seq.1.seq.fil', 'r'):
+# for line1 in open('1.2.seq.2.seq.fil', 'r'):
+for line1 in open('1.4.seq.4.seq.fil', 'r'):
     values1 = [float(s) for s in line1.split()]
     y1.append(values1[0])
 
