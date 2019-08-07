96d5215133f6d1f370e3bd6a37e0c01aa3412932
Morteza Hosseini <s_morteza_h@yahoo.com>
2019-08-07 18:57:42 +0100

    update
    Former-commit-id: 896b928d78d155d1dd852515db6d3e53d4d3626d [formerly a8e4f768a8ca83638c6a9eb7a577559a6988a0d9] [formerly 844b4d1fa3403aaa0e5888f00ca577252ea8a6c7] [formerly 150d12ef611f291407cd67b4cf3ef105196c0af3 [formerly 84ff4611aba8c92afea6c97827585b385ec32104]] [formerly 6767018ad0bec61b2e0de8ed43cff5fe571fa037 [formerly d9615f635e8949fa097e8f328583c2b846e3b291]]
    Former-commit-id: 9101e2a1789e81c5ad229e28d298f41c34eaa5c0
    Former-commit-id: 123a354f91657a3f4d7fbc946f911769f3eb9302
    Former-commit-id: e452466f814c1a478ee6119ca1107849bfb5507d
    Former-commit-id: 5800d02cbf2e5a706163083417116687c488387c
    Former-commit-id: db56f28a91379b9984988b1672851aed8aba77ff [formerly 961fc82b151a00ee6b9cb5f3dfbda288541d1a94]
    Former-commit-id: a1d3456c682d0181fb6efa95f4efaa14ba368c7c
    Former-commit-id: e5d867d4be30941ba14b13d5e147b5f098a403e4

--- diogo/xp2/run.py
+++ diogo/xp2/run.py
@@ -5,9 +5,9 @@ import numpy as np
 
 prepare_data = False
 find_simil_seqs = False
-find_simil_regions = False
+find_simil_regions = True
 plot_rearrange_count = False
-make_rearrange_mat_symmetric = True
+make_rearrange_mat_symmetric = False
 plot_simil = False
 
 main_file = 'mtDNA_Chordata_3327_22-03-2019.fasta'
@@ -202,12 +202,12 @@ def count_rearrange(file_name):
 
 if find_simil_regions:
     geco_threshold = 1.5
-    # Class = 'Chondrichthyes'
+    Class = 'Chondrichthyes'
     # Class = 'Mammalia'
-    Class = 'Actinopterygii'
+    # Class = 'Actinopterygii'
     nrc_file = open(ave_ent_file + '_' + Class + '.tsv')
     data_path = dataset_path + sep + Class.lower() + sep
-    exe_param = '-rm 11,0,1,0.95/8,0,1,0.9 -m 20 -f 20 -dp -th 1.8 '
+    exe_param = '-rm 11,0,1,0.95/8,0,1,0.9 -m 20 -f 25 -dp -th 1.8 '
 
     header = nrc_file.readline().split()
     nrc_file.seek(0)
@@ -216,9 +216,11 @@ if find_simil_regions:
     simil_mat = build_simil_matrix(nrc_mat, geco_threshold)
 
     if os.name == 'posix':
-        smashpp_bin = './smashpp '
+        smashpp_bin = '../../smashpp '
+        # smashpp_bin = './smashpp '
     elif os.name == 'nt':
-        smashpp_bin = '.\smashpp.exe '
+        smashpp_bin = '..\..\smashpp.exe '
+        # smashpp_bin = '.\smashpp.exe '
 
     if not os.path.exists(result_path):
         os.mkdir(result_path)
@@ -229,12 +231,13 @@ if find_simil_regions:
     for i in range(0, len(simil_mat)):
         for j in range(i+1, len(simil_mat)):
             if simil_mat[i][j] == 1:
-                execute(smashpp_bin + exe_param +
-                        '-r ' + data_path + header[i] + '.seq ' +
-                        '-t ' + data_path + header[j] + '.seq ')
+                # execute(smashpp_bin + exe_param +
+                #         '-r ' + data_path + header[i] + '.seq ' +
+                #         '-t ' + data_path + header[j] + '.seq ')
 
                 pos_file_name = header[i] + '.seq.' + header[j] + '.seq.pos'
-                rearrange_mat[i][j] = count_rearrange(pos_file_name)
+                # rearrange_mat[i][j] = count_rearrange(pos_file_name)
+                rearrange_mat[i][j] = 1
 
                 if os.path.exists(result_path + sep + pos_file_name):
                     os.remove(result_path + sep + pos_file_name)
