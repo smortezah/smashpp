583aab518c5dd995cded569422f57451e3cd42d4
Morteza Hosseini <s_morteza_h@yahoo.com>
2019-08-07 13:22:12 +0100

    update
    Former-commit-id: 2be85338e2233c53cec000f4c244b94bbb490bb3 [formerly 050697b97234f1d1d2b6fd6eb83a124d3f4b0472] [formerly cf86877d51b45e0a37d26c53614a556ef59e81b4] [formerly a42a586d516bacf744dfc3de1d366fafbb5664d2 [formerly e4bf4e69842558e02b7e3b77425ef8550794d630]] [formerly b47c4763a0b38ca247abc10aba00a77b61d37414 [formerly 416607512b70df01bbb47a870ee2bdc57f9c38b9]]
    Former-commit-id: 9541d540fe5a63e17bfc64af38571188be3b0cff
    Former-commit-id: f6d5a4104a7f22956b07a5df68b7c79003c8e499
    Former-commit-id: f55cbde6285b86da79a2c8ba7da170592ead20c7
    Former-commit-id: ae99d8381bc25ee0e5e87be786944ffa60a7b5f2
    Former-commit-id: 1a656e812fcc7a19082689c7320e79c46858792f [formerly 4e83d56e0f4ae686843be40fa91b84858a1ce82a]
    Former-commit-id: 0875d377554d2b65b3fdf8f70b34772f9348d31a
    Former-commit-id: c2ccfca0e900e8e1b895f7caf08cf13fbe8f3a28

--- src/filter.cpp
+++ src/filter.cpp
@@ -745,73 +745,56 @@ inline void Filter::smooth_seg_non_rect(std::vector<PosRow>& pos_out,
 
 
 
+  // First value
+  for (auto i = (filt_size >> 1u); i--;) seq.push_back(2.0);
+  for (auto i = (filt_size >> 1u) + 1; i-- && (prfF >> entropy); jump_lines())
+    seq.push_back(entropy);
 
-  // // First value
-  // for (auto i = (filt_size >> 1u) + 1; i-- && (prfF >> entropy); jump_lines())
-  //   seq.push_back(entropy);
-
-  // if (seq.size() <= (filt_size >> 1u)) 
-  //   make_window(2 * seq.size() + 1);
-  // else
-  //   make_window(filt_size);
+  make_window(seq.size());
 
   const auto winBeg{std::begin(window)};
   const auto winEnd{std::end(window)};
   const auto half_wsize = (window.size() >> 1u);
   auto sum_weights{std::accumulate(winBeg, winEnd, 0.f)};
 
-  // auto sum = std::inner_product(std::begin(seq), std::end(seq),
-  //                               winBeg + half_wsize, 0.f);
-  // // std::inner_product(winBeg + half_wsize, winEnd, std::begin(seq), 0.f);
-  // auto filtered = sum / sum_weights;
-  // // filtered = sum / sWeight;
-  // // if (SaveFilter) filF << precision(PREC_FIL) << filtered << '\n';
-  // seg->partition(pos_out, filtered);
-  // if (par->verbose) show_progress(++symsNo, seg->totalSize, par->message);
-
-  // Next half_wsize values
-  for (auto i = half_wsize; i-- && (prfF >> entropy); jump_lines()) {
-    seq.push_back(entropy);
-    if (SaveFilter) filF << precision(PREC_FIL) << entropy << '\n';
-    ++seg->pos;
-    seg->partition(pos_out, entropy);
-    if (par->verbose) show_progress(++symsNo, seg->totalSize, par->message);
-  }
+  auto sum = std::inner_product(std::begin(seq), std::end(seq), winBeg, 0.f);
+  auto filtered = sum / sum_weights;
+  if (SaveFilter) filF << precision(PREC_FIL) << filtered << '\n';
+  seg->partition(pos_out, filtered);
+  if (par->verbose) show_progress(++symsNo, seg->totalSize, par->message);
 
   // The rest
   uint32_t idx{0};
-  for (auto seqBeg = std::begin(seq); prfF >> entropy; jump_lines()) {
+  auto seqBeg = std::begin(seq);
+  for (; prfF >> entropy; jump_lines()) {
     seq[idx] = entropy;
     idx = (idx + 1) % filt_size;
-    auto sum = (std::inner_product(winBeg, winEnd - idx, seqBeg + idx, 0.f) +
+    sum = (std::inner_product(winBeg, winEnd - idx, seqBeg + idx, 0.f) +
            std::inner_product(winEnd - idx, winEnd, seqBeg, 0.f));
-    auto filtered = sum / sum_weights;
-    // filtered = sum / sWeight;
+    filtered = sum / sum_weights;
     if (SaveFilter) filF << precision(PREC_FIL) << filtered << '\n';
     ++seg->pos;
     seg->partition(pos_out, filtered);   
     if (par->verbose) show_progress(++symsNo, seg->totalSize, par->message);
   }
   prfF.close();
-
+  
   // Until half of the window goes outside the array
-  const auto offset{idx};
-  for (auto i = 1u; i != half_wsize + 1; ++i) {
-    // auto seqBeg = std::begin(seq), seqEnd = std::end(seq);
-    // if (++idx < filt_size + 1)
-    //   sum = (std::inner_product(seqBeg + idx, seqEnd, winBeg, 0.f) +
-    //          std::inner_product(seqBeg, seqBeg + offset, winEnd - idx, 0.f));
-    // else
-    //   sum = std::inner_product(seqBeg + (idx % filt_size), seqBeg + offset,
-    //                            winBeg, 0.f);
-    // filtered = sum / sum_weights;
-    if (SaveFilter) filF << precision(PREC_FIL) << seq[i-1] << '\n';
+  for (auto i=half_wsize; i--;) {
+    seq[idx] = 2.0;
+    idx = (idx + 1) % filt_size;
+    sum = (std::inner_product(winBeg, winEnd - idx, seqBeg + idx, 0.f) +
+           std::inner_product(winEnd - idx, winEnd, seqBeg, 0.f));
+    filtered = sum / sum_weights;
+    if (SaveFilter) filF << precision(PREC_FIL) << filtered << '\n';
     ++seg->pos;
-    seg->partition(pos_out, seq[i-1]);
+    seg->partition(pos_out, filtered);   
     if (par->verbose) show_progress(++symsNo, seg->totalSize, par->message);
   }
   seg->finalize_partition(pos_out);
   if (par->verbose) show_progress(++symsNo, seg->totalSize, par->message);
+
+
 
 
 
