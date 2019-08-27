69577f6f83da19977c7022f3c91190f390638d8c
Morteza Hosseini <s_morteza_h@yahoo.com>
2019-08-27 15:19:27 +0100

    update
    Former-commit-id: 58a286e7199298ae74456959ba5d3d77e58f6f67 [formerly 52dfea479c8d8f5dab23512a68f64d693273e930] [formerly a12921431ece329ec2c92bfad89b434bdfad2955] [formerly b57cbb3a4d62df1546bc48ae44a42606ef2fd9b3 [formerly e166648675ef1a2c27672ac2606637192250b942]] [formerly c0852ae6bb712182e2eb904427c5d20e7f6d2a04 [formerly c03774b151ec192f124a72bbe31a6949d1484ec9]]
    Former-commit-id: e4317d2aaaf848ec73e61a8602fbb357fe9a1b52
    Former-commit-id: 174754c2131db6aecde77a24408bd778466dc171
    Former-commit-id: 557565f75207c4699a988dd7db92869c77f2b64c
    Former-commit-id: 9a2bf293e19ed6645952bf98df88fae95736dde2
    Former-commit-id: 59d56dba0b5408c8418fae406c4e753ac2530ccd [formerly a013ddccb361f37c8fe2f0134bc5c62a5d63fc8b]
    Former-commit-id: e186f62178156c65ddf2ae1deec1b1553e5e04ef
    Former-commit-id: 0dc0b4df5903388a7f37107c9207453a5108560b

--- xp/run_xp.py
+++ xp/run_xp.py
@@ -218,10 +218,10 @@ if X_oryzae_pv_oryzae_PXO99A_MAFF_311018:
         path = path_data_real + 'fungi' + sep + 'Xanthomonas_oryzae_pv_oryzae' + sep
         ref = 'PXO99A.seq'
         tar = 'MAFF_311018.seq'
-        main_par = ' -rm 13,0,0.005,1 -f 150 -m 10000 -d 1000 -th 1.65 -ar -sf '
+        main_par = ' -rm 13,0,0.005,1 -f 150 -m 10000 -d 1000 -th 1.55 -ar '
         viz_par = ' -viz -l 6 -s 10 -w 8 -p 1 -rt 500000 -rn PXO99A -tn "MAFF 311018" -o PXO99A_MAFF_311018.svg '
-        # execute(smashpp + main_par + ' -r ' + path + ref + ' -t ' +
-        #         path + tar)
+        execute(smashpp + main_par + ' -r ' + path + ref + ' -t ' +
+                path + tar)
         execute(smashpp + viz_par + ref + '.' + tar + '.pos')
 
 if E_gossypii_I_S_cerevisiae_XVI:
