406dbba5056e5cd73c953085b9a2dddf48ccd0e8
Morteza Hosseini <s_morteza_h@yahoo.com>
2019-08-29 16:29:37 +0100

    update
    Former-commit-id: c7e0bb2b0e115ec6cec049f8ea5ac1b266991678 [formerly a26bbd626156426d3f94fd84dcfc596688e943aa] [formerly c6eddf7f4868e27f19847545c9170023f2b470d1] [formerly ed568bfaf5c268f28800b9ba3d45210ceda20bba [formerly 131bbff3507ac5980e32a6f9a79d99828c58946a]] [formerly 788f4ed519170553673353786026ddb263d2b33a [formerly 102fcc1d29f4cccb1752ac0f917ac46fc5ba290f]]
    Former-commit-id: d42a654f7bcf48a85e49fc19c3729108e9ea329a
    Former-commit-id: b69f50aa010146da6ccccfbaf66c23b5295c5e9b
    Former-commit-id: 7b9c060df57ff76eadec9aedfa12f39f4a4f9147
    Former-commit-id: 220fb9ce5c27c3e8ab9bb81752afe3f9d0df3b64
    Former-commit-id: e317e9d05555baeb2d539ee8def8b89f5afa8146 [formerly e0b57db9eb7adfa4a528486dfc1fc3396717f0b5]
    Former-commit-id: 79b86fb321231f6fee35be3cc989e25bdcfee262
    Former-commit-id: 350baca1363a3681d96fd186fa70857640c6c388

--- xp/run_xp.py
+++ xp/run_xp.py
@@ -24,6 +24,8 @@ sim_mutation = False
 X_oryzae_pv_oryzae_PXO99A_MAFF_311018 = False
 gga18_mga20 = False
 
+gga14_mga16 = True
+
 mga25_gga23 = False
 E_gossypii_I_S_cerevisiae_XVI = False
 S_cerevisiae_VIII_C_glabrata_XVI = False
@@ -230,6 +232,17 @@ if gga18_mga20:
         tar = '20.seq'
         main_par = ' -rm 14,0,0.005,0.95/5,0,1,0.95 -f 130 -m 500000 -d 2200 -th 1.9 -dp '
         viz_par = ' -viz -l 1 -p 1 -vv -tc 6 -rn GGA18 -tn MGA20 -o GGA18_MGA20.svg '
+        execute(smashpp + main_par + ' -r ' + path_ref + ref + ' -t ' +
+                path_tar + tar)
+        execute(smashpp + viz_par + ref + '.' + tar + '.pos')
+
+if gga14_mga16:
+        path_ref = path_data_real + 'bird' + sep + 'Gallus_gallus' + sep
+        path_tar = path_data_real + 'bird' + sep + 'Meleagris_gallopavo' + sep
+        ref = '14.seq'
+        tar = '16.seq'
+        main_par = ' -rm 14,0,0.005,0.95/5,0,1,0.95 -f 200 -m 500000 -d 3000 -th 1.85 -nr -sf -ar '
+        viz_par = ' -viz -l 1 -rn GGA14 -tn MGA16 -o GGA14_MGA16.svg '
         execute(smashpp + main_par + ' -r ' + path_ref + ref + ' -t ' +
                 path_tar + tar)
         execute(smashpp + viz_par + ref + '.' + tar + '.pos')
