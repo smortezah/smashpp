211aeea49b94c2cd370c4d6d7ab4220077f198fd
Morteza Hosseini <s_morteza_h@yahoo.com>
2019-08-30 20:38:14 +0100

    update
    Former-commit-id: f26428d70038adc55b42f8eb280ad2e77e4f2be3 [formerly 7cb5e9a9036571b566ebfd7598d9f075923fc97e] [formerly 30b8f7521b3761bfe3f0aad086456ea49447ff8f] [formerly 2197c4ba7b8e6ee99f150e69f010d9cb2f538742 [formerly d69066fbb59dbbdce31cd8fa0756cc6ad2334494]] [formerly 5734e4e6c4f6cfdef050bd1cd7ca64d9c7f09ffa [formerly 9890414eb0698660b9bb60f591d9f687097ccc2e]]
    Former-commit-id: 4c498673a097b598841c352071039f189bb5ba35
    Former-commit-id: fb737617ae8a0c076ba459f34c251d19242835fb
    Former-commit-id: 7faaa49a73ae4fedf807f74fbd56475271c6df52
    Former-commit-id: aa046bc8205686143f49eeeb84cb07cd65547c24
    Former-commit-id: 2f1b741924a8b904d06ae8bceff8d1d457ad073b [formerly 276225ea33c4fd4eb23686bf11e64252fef34d52]
    Former-commit-id: b5b4064ee8ae3993e57568bef58d987e2aec0cf1
    Former-commit-id: 7c990c7be7081b90cbf59762ac72d6795824c62c

--- xp/run_xp.py
+++ xp/run_xp.py
@@ -24,8 +24,9 @@ sim_mutation = False
 X_oryzae_pv_oryzae_PXO99A_MAFF_311018 = False
 gga18_mga20 = False
 
-gga9_mga11 = True
+gga8_mga10 = True
 
+gga9_mga11 = False
 gga11_mga13 = False
 gga13_mga15 = False
 gga14_mga16 = False
@@ -235,6 +236,17 @@ if gga18_mga20:
         tar = '20.seq'
         main_par = ' -rm 14,0,0.005,0.95/5,0,1,0.95 -f 130 -m 500000 -d 2200 -th 1.9 -dp '
         viz_par = ' -viz -l 1 -p 1 -vv -tc 6 -rn GGA18 -tn MGA20 -o GGA18_MGA20.svg '
+        execute(smashpp + main_par + ' -r ' + path_ref + ref + ' -t ' +
+                path_tar + tar)
+        execute(smashpp + viz_par + ref + '.' + tar + '.pos')
+
+if gga8_mga10:
+        path_tar = path_data_real + 'bird' + sep + 'Gallus_gallus' + sep
+        path_ref = path_data_real + 'bird' + sep + 'Meleagris_gallopavo' + sep
+        tar = '8.seq'
+        ref = '10.seq'
+        main_par = ' -rm 14,0,0.005,0.95/5,0,0.5,0.95 -f 150 -m 200000 -d 7000 -th 1.95 -nr -sf '
+        viz_par = ' -viz -l 1 '
         execute(smashpp + main_par + ' -r ' + path_ref + ref + ' -t ' +
                 path_tar + tar)
         execute(smashpp + viz_par + ref + '.' + tar + '.pos')
