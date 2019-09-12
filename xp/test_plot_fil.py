33f789bdd3d1d695b77f488d47213b49db3c753b
Morteza Hosseini <s_morteza_h@yahoo.com>
2019-09-12 17:42:05 +0100

    update

--- xp/test_plot_fil.py
+++ xp/test_plot_fil.py
@@ -2,8 +2,9 @@ import matplotlib.pyplot as plt
 
 # name = '21.seq.21.seq.fil'
 # name = 'RefMut_smash.TarMut_smash.fil'
-name = 'RefS.TarS.fil'
+# name = 'RefS.TarS.fil'
 # name = 'RefM.TarM.fil'
+name = 'RefMut.TarMut.fil'
 
 y0, y1 = [], []
 for line0 in open('0.'+name, 'r'):
