5626a82992dabb5f1a743a3704a568bcb0b83f1e
Morteza Hosseini <s_morteza_h@yahoo.com>
2019-09-12 10:29:42 +0100

    update
    Former-commit-id: 6b55dbc7dac88ab072dbd5c11b77464fbf49c39b [formerly 6a912cab709ae82caba7b93caa0ec96311b1de53] [formerly b8969843741e3e96b059d6686b4433246be4a891] [formerly 72ed9b92f656db446ce648d33ee83b6c1dad87c9 [formerly b1641d0398c5443706c673b631342a8d93298c1f]] [formerly 61aec334c2c3a4d34ed36e17a2633cd2650b9e22 [formerly 0f358393adb1056e152a969b2f4079a0000df6d0]]
    Former-commit-id: 971f91c35bb7fcc50b55373006106841eeae52d1
    Former-commit-id: e7ab1f483bf08abbf23ff1d2894516653fa7f60e
    Former-commit-id: 59863b034b032eee5fa0fd592fde53f18ee0ad63
    Former-commit-id: 38a7353aab5fc02f407e7bc0ebc7189e23dcb377
    Former-commit-id: 856a2e57fa7c7d9d8d77bf14e55027bd98a587b5 [formerly f32a55e60e9572362f55ba3332cc20b5eea13cec]
    Former-commit-id: 57ceb13d73015b6e564ce1f7dfab240c837ce1cb
    Former-commit-id: aeaf0ef2bc5f91d032c6866ac4bd553e79ac1dbd

--- xp/test_plot_fil.py
+++ xp/test_plot_fil.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
 # name = '21.seq.21.seq.fil'
-name = 'RefPerm20.TarPerm.fil'
+name = 'RefMut_smash.TarMut_smash.fil'
 
 y0, y1 = [], []
 for line0 in open('0.'+name, 'r'):
