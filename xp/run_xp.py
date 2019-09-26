99138a16fa25b3bb260c2442dc043d1f09d66533
Morteza Hosseini <s_morteza_h@yahoo.com>
2019-09-26 09:53:10 +0100

    update
    Former-commit-id: e55d1e0cc608a68b043b6a80dbbbdc2e70432334 [formerly 22b7ff3c0578ddb707351060164fc0eef7270411] [formerly 08ac8e5310b93d48521ff09f9ff95fd0e8e27294] [formerly dca6f1a7aad291a1e9bce1d81f1207c391e9781c [formerly 0524ba6db912c1888df831b1321911c5fab039d0]] [formerly 33c6ee88e0637dbd50c68616a41004283b5c1afa [formerly b43ee82bc2404790f15ac8396fc3f04d28d607f9]]
    Former-commit-id: f6c7450f6e33d5020efb7f9d70b5c6b382e95117
    Former-commit-id: 8d6ed31133abff3917452ac73f648b971794f695
    Former-commit-id: 67431a86171237d62da0d1741af3fe2a5b10e3a0
    Former-commit-id: 2dc9f333af4890ac459686796d0d87ad8aaf21fb
    Former-commit-id: 5960045c11d2a4016791041620fbc0b38957ccb5 [formerly 47c9ff2e3d0d61922cebefa34f1543204ecbe90b]
    Former-commit-id: 0f76210836c3ad8700a30b48e57ca0fb5099a33d
    Former-commit-id: 1cdfe836b3a321c1815fa8d2e9a0ff2f6c9b287e

--- xp/run_xp.py
+++ xp/run_xp.py
@@ -363,9 +363,9 @@ if REAL:
     path_tar = path_data_real + 'mammalia' + sep + 'Sus_scrofa' + sep
     # path_tar = path_data_real + 'mammalia' + sep + 'Homo_sapiens' + sep
     # path_tar = path_data_real + 'mammalia' + sep + 'Rattus_norvegicus' + sep
-    ref = '5.seq'
-    tar = '5.seq'
-    main_par = ' -rm 18,0,0.001,0.95/5,0,0.001,0.95 -f 15000 -d 10 -th 0.1 -m 100000 -nr -sf '
+    ref = '2.seq'
+    tar = '15.seq'
+    main_par = ' -rm 18,0,0.001,0.95/5,0,0.001,0.95 -f 20000 -d 10 -th 0.1 -m 100000 -nr -sf '
     viz_par = ' -viz '
     execute(smashpp + main_par + ' -r ' + path_ref + ref + ' -t ' +
             path_tar + tar)
