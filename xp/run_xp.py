1e00f5b47e2dc92fe9b2d20d945c2ea82aa172b6
Morteza Hosseini <s_morteza_h@yahoo.com>
2019-09-30 17:59:12 +0100

    update
    Former-commit-id: 591fe418db1fab50c46b955b070f839f00e1eb95 [formerly bff22d66c34ebe8790b14494b61f7c84e6ae836d] [formerly f1cb41412e358e8f9e7909f5c884644a6149b95b] [formerly db8bb512d9ceeb1840e0392e788a96f3e36e3b12 [formerly 3618c7ec750063d5ef3bb81c70619bacf245e4dd]] [formerly 1282a67e6984e465f6577953e5f77efac5a0c740 [formerly 6cd9634f45f614340449d03e39564acd97f8314a]]
    Former-commit-id: 40844a9f5e1c530cca17d42feff4b9c7970010ad
    Former-commit-id: 18dac86647c771c48bb026a1688847ea5a1970e9
    Former-commit-id: 06711081a51d0ded169598a51342a7f5e60dc963
    Former-commit-id: 5a208f291bb07bfeb835ce17c9c90cb5fc5016e7
    Former-commit-id: 853e507b1af3c03afb27a5907459a08d48f55700 [formerly b2fa8fc02a5f2bc5fa25dc3d429bdca8f004db18]
    Former-commit-id: ab02d004bba003af4badf3c3aaba8d48006828c4
    Former-commit-id: 99244c23960d21c4a6008c06cacb8c0f88a233f7

--- xp/run_xp.py
+++ xp/run_xp.py
@@ -395,11 +395,11 @@ if bench:
     bench_result = []  # name, size, time, memory
     
     # Synthetic
-    bench_synth_small = True
-    bench_synth_medium = True
-    bench_synth_large = True
-    bench_synth_xlarge = True
-    bench_synth_mutate = False
+    bench_synth_small = False
+    bench_synth_medium = False
+    bench_synth_large = False
+    bench_synth_xlarge = False
+    bench_synth_mutate = True
     bench_synth_comp_smash = False
 
     # Real
