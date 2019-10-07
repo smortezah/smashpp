f371ed422094f70c3ba54249fbf15c5ee898578e
Morteza Hosseini <s_morteza_h@yahoo.com>
2019-10-07 12:23:21 +0200

    update
    Former-commit-id: 4c9dc74567c474902cba5b3bd44de83836558846 [formerly 3d495c4562256d10d2525d168ac3241b2827dadc] [formerly 892233ed3411f282ce0d6f145cdf5226cfb495b4] [formerly 431453cf927de41189dc8ee6d3b165ccc8b1e496 [formerly 1157e037fa44e19e675b99926df22e6ecb2e8cb3]] [formerly 0167646142380806812326a58fb2586bcc0ff2ad [formerly 5cfb92495cf3a0088b2a93d0f86ff268646cb09f]]
    Former-commit-id: 84ebe4ae2fcec6d5961d26b8fd9b46d239889798
    Former-commit-id: c9ec6debf9a326e5c588057e4240ccc891dc36e9
    Former-commit-id: d5c6605fafe0909c1acd3edf2f7e379e65579264
    Former-commit-id: cabe962c52564be1b344451b04b928a682ac7d28
    Former-commit-id: 1ef08a2e643dac5f4e6f0650703c239e0a42957d [formerly 54db3572d48eb29ff942e1e8a313aa8836be2501]
    Former-commit-id: dc2bb19eec4f93729b722c867366fd7c92e74c63
    Former-commit-id: 6d3289368e1477304ab5d6699e405622922c4369

--- xp/run_xp.py
+++ xp/run_xp.py
@@ -379,20 +379,20 @@ def run_real_gga14_mga16():
 
 
 if REAL:
-    # path_ref = path_data_real + 'mammalia' + sep + 'Homo_sapiens' + sep
+    path_ref = path_data_real + 'mammalia' + sep + 'Homo_sapiens' + sep
     # path_ref = path_data_real + 'mammalia' + sep + 'Mus_musculus' + sep
     # path_ref = path_data_real + 'mammalia' + sep + \
     #     'Monodelphis_domestica' + sep
     # path_ref = path_data_real + 'mammalia' + sep + 'Bos_taurus' + sep
     # path_ref = path_data_real + 'mammalia' + sep + 'Sus_scrofa' + sep
-    path_ref = path_data_real + 'mammalia' + sep + 'Ovis_aries' + sep
+    # path_ref = path_data_real + 'mammalia' + sep + 'Ovis_aries' + sep
     path_tar = path_data_real + 'mammalia' + sep + 'Bos_taurus' + sep
     # path_tar = path_data_real + 'mammalia' + sep + 'Sus_scrofa' + sep
     # path_tar = path_data_real + 'mammalia' + sep + 'Homo_sapiens' + sep
     # path_tar = path_data_real + 'mammalia' + sep + 'Rattus_norvegicus' + sep
-    ref = '2.seq'
-    tar = '8.seq'
-    main_par = ' -rm 18,0,0.001,0.95/5,0,0.001,0.95 -f 3000 -d 100 ' + \
+    ref = 'X.seq'
+    tar = 'X.seq'
+    main_par = ' -rm 18,0,0.001,0.95/5,0,0.001,0.95 -f 3000 -d 1000 ' + \
         '-th 0.1 -m 100000 -nr -sf '
     viz_par = ' -viz '
     execute(smashpp + main_par + ' -r ' + path_ref + ref + ' -t ' +
