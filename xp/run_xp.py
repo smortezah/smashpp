df08ff80028b6ac447790288dc8bec84f913cf33
Morteza Hosseini <s_morteza_h@yahoo.com>
2019-10-21 17:39:38 +0200

    update
    Former-commit-id: 8bb8780b447213d74699d9116f00ceab8286e31e [formerly bd1ed645aa798fb49a900c8938b60cc6784744fc] [formerly 32b0ac191f5854236365aa60e019a6eec0e22bb7] [formerly 7cf6b8019ebc220a7d2825781a275304a844f1c3 [formerly 7edbc9548b483b53c5bfb4513a4a5afad3c85c35]] [formerly 6ff676df854ac3e767e42310aa73f670ff08ec13 [formerly 617327d8e50d757f6d851c516c6936be8ea0f2b3]]
    Former-commit-id: 641d26d149636fb3c2be0b65199621bf9440dbab
    Former-commit-id: 08e64e220c267fc9fc487349fc36b6ae71d90db6
    Former-commit-id: e6d01b567bb6bad20a636c0c4642692d331a76a3
    Former-commit-id: d5f0ea549ca784f69c610e9af5fce4f748fb11f1
    Former-commit-id: 3dbee35e03e5218500e7c63c7b9920bdb65fff9d [formerly 91a7509ab3bb11ecd2e99a3da2681b3cbfd29a0a]
    Former-commit-id: eb8281190f9ba8a90d79d79211aa1beb8fa4e776
    Former-commit-id: 48fa87b8102b614db384952d5fb3b638b603b838

--- xp/run_xp.py
+++ xp/run_xp.py
@@ -373,9 +373,9 @@ def run_real_PXO99A_MAFF():
 
 if run:
     # # Synthetic
-    # run_synth_small()
+    run_synth_small()
     # run_synth_medium()
-    run_synth_large()
+    # run_synth_large()
     # run_synth_xlarge()
     # run_synth_mutate()
 
