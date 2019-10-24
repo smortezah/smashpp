75b4f12aa6b770d7de518cce76ad517992de38e3
Morteza Hosseini <s_morteza_h@yahoo.com>
2019-10-24 13:16:17 +0200

    update

--- AFproject/src/run.py
+++ AFproject/src/run.py
@@ -12,16 +12,20 @@ compress_param = ' -rm 4:1000:1:30:0.85/0:1000:0.8 ' + \
     ' -rm 8:1000:0:30:0.85/1:1000:0.8 -rm 12:1000:0:30:0.9/2:1000:0.8 ' + \
     ' -rm 14:1000:0:30:0.9/2:1000:0.8 -rm 16:1000:0:30:0.95/3:1000:0.8 ' + \
     ' -rm 18:1000:0:30:0.95/4:1000:0.8 '
-# compress_param = ' -rm 4:1000:1:0/0 -rm 8:1000:1:0/0 -rm 14:1000:1:3/10 -rm 18:1000:1:5/10 -c 30 - g 0.95 '
+# compress_param = ' -rm 4:1000:1:0/0 -rm 8:1000:1:0/0 -rm 14:1000:1:3/10 ' + \
+#     ' -rm 18:1000:1:5/10 -c 30 - g 0.95 '
 
+sep = ''
 if os.name == 'posix':
     sep = '/'
 elif os.name == 'nt':
     sep = '\\'
-dataset_path = '..' + sep + 'dataset' + sep
-result_path = '..' + sep + 'result' + sep
-bin_path = '..' + sep + 'bin' + sep
-geco2 = bin_path + 'GeCo2 '
+path_dataset = '..' + sep + 'dataset' + sep
+path_result = '..' + sep + 'result' + sep
+path_bin = '..' + sep + 'bin' + sep
+prog = path_bin + 'GeCo2 '
+nrc_file_name = 'nrc.csv'
+nrc_ave_file_name = 'nrc_ave.csv'
 
 
 def execute(cmd):
@@ -33,50 +37,81 @@ def remove_file_if_exist(file):
         os.remove(file)
 
 
+def remove_files_end_with(path, extension):
+    if path[-1] != sep:
+        path += sep
+    for file in os.listdir(path):
+        if file.endswith(extension):
+            os.remove(path + file)
+
+
 def create_dir_if_not_exit(path):
     if not os.path.exists(path):
         os.mkdir(path)
 
 
+def grant_permission(path):
+    execute('sudo chmod -R 777 ' + path)
+
+
+def extension_removed(file_with_extension):
+    return os.path.splitext(file_with_extension)[0]
+
+
+def ave_matrix(mat):
+    num_col = len(mat)
+    ave_mat = [[0 for x in range(num_col)] for y in range(num_col)]
+    for i in range(0, num_col):
+        for j in range(i, num_col):
+            if i == j:
+                ave_mat[i][j] = mat[i][j]
+            else:
+                ave_mat[i][j] = ave_mat[j][i] = (mat[i][j] + mat[j][i]) / 2
+    return ave_mat
+
+
+def nrc_from_log_file(log_file_name):
+    NRC = 0.0
+    with open(log_file_name) as log_file:
+        for line in log_file:
+            line_list = line.split()
+            if len(line_list) > 5:
+                NRC = float(line_list[15])
+                if NRC > 1.0:
+                    NRC = 1.0
+    return NRC
+
+
 if find_simil_seqs:
     remove_file_if_exist('log')
-    for file in os.listdir(dataset_path):
-        if file.endswith('.co'):
-            os.remove(dataset_path + file)
-    create_dir_if_not_exit(result_path)
-    execute('sudo chmod -R 777 ' + bin_path)
-
-    in_file_path = dataset_path
-    in_file_list = os.listdir(in_file_path)
-    out_file_name = 'nrc.csv'
+    remove_files_end_with(path_dataset, '.co')
+    create_dir_if_not_exit(path_result)
+    grant_permission(path_bin)
 
     print('Finding similar sequences ...')
-    remove_file_if_exist(result_path + out_file_name)
-    with open(result_path + out_file_name, "w") as out_file:
+    in_file_path = path_dataset
+    in_file_list = os.listdir(in_file_path)
+    out_file_name = nrc_file_name
+    remove_file_if_exist(path_result + out_file_name)
+
+    with open(path_result + out_file_name, "w") as out_file:
         for file in in_file_list:
-            file_name = os.path.splitext(file)[0]
+            file_name = extension_removed(file)
             out_file.write(',' + file_name)
         out_file.write('\n')
 
         for i in range(len(in_file_list)):
-            file_name = os.path.splitext(in_file_list[i])[0]
+            file_name = extension_removed(in_file_list[i])
             out_file.write(file_name)
             for j in range(len(in_file_list)):
                 ref = in_file_path + in_file_list[i]
                 tar = in_file_path + in_file_list[j]
+                nrc = 0.0
                 if i != j:
-                    execute(geco2 + compress_param + ' -r ' + ref + ' ' +
+                    execute(prog + compress_param + ' -r ' + ref + ' ' +
                             tar + ' > log')
-                    with open('log', 'r') as log_file:
-                        for line in log_file:
-                            line_list = line.split()
-                            if len(line_list) > 5:
-                                NRC = float(line_list[15])
-                                if NRC > 1:
-                                    NRC = 1
-                                out_file.write(',' + str(NRC))
-                else:
-                    out_file.write(',0')
+                    nrc = nrc_from_log_file('log')
+                out_file.write(',' + str(nrc))
                 remove_file_if_exist(tar + '.co')
             out_file.write('\n')
     remove_file_if_exist('log')
@@ -84,25 +119,14 @@ if find_simil_seqs:
 
 if make_nrc_ave:
     print('Making NRC average ...')
-    with open(result_path + 'nrc.csv') as nrc_file:
+    with open(path_result + nrc_file_name) as nrc_file:
         header = nrc_file.readline()[:-1].split(",")[1:]
         nrc_file.seek(0)
         nrc_mat = np.genfromtxt(nrc_file, skip_header=True, delimiter=",",
                                 usecols=range(1, len(header) + 1))
-    nrc_ave_name = 'nrc_ave.csv'
-    remove_file_if_exist(result_path + nrc_ave_name)
-
-    num_files = len(nrc_mat)
-    nrc_ave = [[0 for x in range(num_files)] for y in range(num_files)]
-    for i in range(0, num_files):
-        for j in range(i, num_files):
-            if i == j:
-                nrc_ave[i][j] = nrc_mat[i][j]
-            else:
-                nrc_ave[i][j] = nrc_ave[j][i] = (
-                    nrc_mat[i][j] + nrc_mat[j][i]) / 2
-
-    with open(result_path + nrc_ave_name, "w") as nrc_ave_file:
+    remove_file_if_exist(path_result + nrc_ave_file_name)
+    nrc_ave = ave_matrix(nrc_mat)
+    with open(path_result + nrc_ave_file_name, "w") as nrc_ave_file:
         for i in range(len(header)):
             nrc_ave_file.write(',' + header[i])
         nrc_ave_file.write('\n')
@@ -115,8 +139,8 @@ if make_nrc_ave:
 
 if make_Phylip_distance_matrix:
     print('Making Phylip distance matrix ...')
-    with open(result_path + 'nrc_ave.csv') as nrc_ave, \
-            open(result_path + 'dist.phy', 'w') as dist:
+    with open(path_result + 'nrc_ave.csv') as nrc_ave, \
+            open(path_result + 'dist.phy', 'w') as dist:
         reader = csv.reader(nrc_ave)
         num_seqs = len(reader.__next__()) - 1
         data = []
