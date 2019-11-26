aef5c0121c69215afd5fd4211d9367c4a65353eb
Morteza Hosseini <s_morteza_h@yahoo.com>
2019-11-26 10:44:36 +0100

    up
    Former-commit-id: a92e7193478c2bbafd19260d4f88a8c3a1c2c00f [formerly 9461514f4bb2bc647007448ac66685d63a4b81e2] [formerly a450c8bdbe1d683c688fe0f12ac92fbfa24f4b68] [formerly 5e791df01982d1513ffb0a975207da418fcdaa2d [formerly 8788619cb98c27990d4a1bdaa81748fbbec62fb7]] [formerly d98c921f043d044432d439255f2e4b872035d7b2 [formerly 4ed7a9adb4454c87528b4e3bd28037ab9bafc52e]]
    Former-commit-id: b3d3a40af6691b2a7c034cf9317b3b1d113353ac
    Former-commit-id: 41092299624301dd25f0f78b103680fc59a9221a
    Former-commit-id: 75631351c54c68b286f11d62dc3b28be60796e77
    Former-commit-id: 268a62579cf02d27e3cdc16740132887cf760719
    Former-commit-id: c77ccdff1f8d573df17d7b454a81bb994e6c2ddf [formerly dc4cd5b769851a0cb833f0d41237bf283950a85a]
    Former-commit-id: de94ad4fd6ca472ccf76c802ca64b5b7f015c648
    Former-commit-id: 36b1b5908c66a9723242d4a3c50c7258a15db08f

--- diogo/xp2/top.R
+++
@@ -1,8 +0,0 @@
-# rearrange <- read.table('rearrange_count_Chondrichthyes.tsv', sep = '\t', header = TRUE)
-# rearrange <- read.table('rearrange_count_Mammalia.tsv', sep = '\t', header = TRUE)
-# rearrange <- read.table('rearrange_count_Actinopterygii.tsv', sep = '\t', header = TRUE)
-# molten.rearrange <- melt(rearrange)
-
-names <- read.table('COX1_pos.tsv', sep='\t', header = TRUE)
-
-print(names$Organism[names$Name=='NC_012737'])
