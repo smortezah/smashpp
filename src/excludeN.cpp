05345bfb12448611d957dca5b25d4df73a110170
smortezah <s_morteza_h@yahoo.com>
2020-04-06 16:01:23 +0200

    minor
    Former-commit-id: 016eba508435f03ae053a98d6566339cfaa198a5 [formerly a8a87e3feb49079e8f5c213c1cfa5e9bf5d7bf11]
    Former-commit-id: 3779222727cd8a332b08c9a8267134445a0e4778
    Former-commit-id: 0a1900c0bdbd5ca5e2df336e6cacc98b5886423f
    Former-commit-id: aafa6c6398bf8ff13d34e82dbf982e13daad783b
    Former-commit-id: 407671ee3523ee3045160c2b8b9649a543a4b574
    Former-commit-id: 5ac7718ab9f62d9466e9d6696a8d23c84bb0635f [formerly ba07ffde5d4889847a18205da8d3f907cfd0d70c]
    Former-commit-id: 05ee426a1a0ae3d63e23bf6081a1788b08c3de80
    Former-commit-id: ef545a302e918e93ef66ec1457ab276f226998fe

--- src/excludeN.cpp
+++ src/excludeN.cpp
@@ -2,18 +2,21 @@
 // Morteza Hosseini    seyedmorteza@ua.pt
 // Copyright (C) 2018-2020, IEETA, University of Aveiro, Portugal.
 
-#include <iostream>
 #include <fstream>
-#include "def.hpp"
+#include <iostream>
+
+#include "par.hpp"
+using namespace smashpp;
 
 int main(int argc, char* argv[]) {
-  for (std::vector<char> buffer(FILE_BUF, 0); cin;) {
-    cin.read(buffer.data(), FILE_BUF);
+  for (std::vector<char> buffer(FILE_READ_BUF, 0); std::cin;) {
+    std::cin.read(buffer.data(), FILE_READ_BUF);
     std::string out;
-    for (auto it = begin(buffer); it != begin(buffer) + cin.gcount(); ++it) {
+    for (auto it = begin(buffer); it != begin(buffer) + std::cin.gcount();
+         ++it) {
       if (*it != 'N') out += *it;
     }
-    cout.write(out.data(), out.size());
+    std::cout.write(out.data(), out.size());
   }
 
   return 0;
